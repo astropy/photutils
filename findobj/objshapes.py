df219d22fd5ba5145a3ba83d81c1a9b372b8a8ef
Larry Bradley <larry.bradley@gmail.com>
2014-03-14 14:08:56 -0400

    added centroiding from 1D and 2D Gaussian fits

--- findobj/objshapes.py
+++ findobj/objshapes.py
@@ -2,7 +2,7 @@
 import findobj
 import numpy as np
 from astropy.convolution import Kernel2D
-from astropy.modeling import models
+from astropy.modeling import models, fitting
 
 
 def centroid_com(data, data_mask=None):
@@ -27,7 +27,7 @@ def centroid_com(data, data_mask=None):
 
     if data_mask is not None:
         if data.shape != data_mask.shape:
-            raise ValueError('data and data_mask must have the same shap')
+            raise ValueError('data and data_mask must have the same shape')
         data[data_mask] = 0.
     m = findobj.moments(data, 1)
     xcen = m[1, 0] / m[0, 0]
@@ -35,18 +35,26 @@ def centroid_com(data, data_mask=None):
     return xcen, ycen
 
 
-def centroid_2dg(data, fwhm, data_err=None, data_mask=None):
+def gaussian1d_moments(data):
     """
-    Calculate the centroid of an array from fitting a symmetrical
-    2D Gaussian to the data.
+    Estimate 1D Gaussian parameters from moments.
+    """
+    x = np.arange(data.size)
+    xc = np.sum(x * data) / np.sum(data)
+    stddev = np.sqrt(abs(np.sum(data * (x - xc)**2) / np.sum(data)))
+    amplitude = np.max(data) - np.median(data)
+    return amplitude, xc, stddev
+
+
+def centroid_1dg(data, data_err=None, data_mask=None):
+    """
+    Calculate the centroid of an array from fitting 1D Gaussians
+    to the marginal x and y distributions of the data.
 
     Parameters
     ----------
     data : array_like
-        The image data.
-
-    fwhm : float
-        The full width at half maximum of the 2D Gaussian.
+        The image data.  (should be background subtracted)
 
     data_err : array_like, optional
         The 1-sigma errors for `data`.
@@ -60,29 +68,74 @@ def centroid_2dg(data, fwhm, data_err=None, data_mask=None):
     centroid : tuple
         (x, y) coordinates of the centroid.
     """
-    size = 5.      # all that is needed for accurate centroiding
-    stddev = fwhm / 2. * np.sqrt(2. * np.log(2.))
-    gauss = Gaussian2DImg(size, size, x_stddev=stddev, y_stddev=stddev)
-    return None
+    gaussian_x = data.sum(axis=0)
+    gaussian_y = data.sum(axis=1)
+    marginal_dists = [gaussian_x, gaussian_y]
+    centroid = []
+    for data in marginal_dists:
+        params_init = findobj.gaussian1d_moments(data)
+        g_init = models.Gaussian1D(*params_init)
+        f = fitting.NonLinearLSQFitter()
+        x = np.arange(data.size)
+        weights = None
+        g_fit = f(g_init, x, data, weights=weights)
+        centroid.append(g_fit.mean.value)
+    return centroid
 
 
-def shape_params(data):
+def centroid_2dg(data, data_err=None, data_mask=None):
     """
-    Calculate the centroid and basic shape parameters for an object.
+    Calculate the centroid of an array from fitting a
+    2D Gaussian to the data.
 
     Parameters
     ----------
     data : array_like
-        The image data.
+        The image data.  (should be background subtracted)
+
+    data_err : array_like, optional
+        The 1-sigma errors for `data`.
+
+    data_mask : array_like, boolean, optional
+        A boolean mask with the same shape as `data`, where a `True`
+        value indicates the corresponding element of data in invalid.
+
+    Returns
+    -------
+    centroid : tuple
+        (x, y) coordinates of the centroid.
+    """
+
+    gparams = shape_params(data)
+    amplitude = np.max(data) - np.median(data)
+    g_init = models.Gaussian2D(amplitude, gparams['xcen'], gparams['ycen'],
+                               gparams['major_axis'], gparams['minor_axis'],
+                               theta=gparams['pa'])
+    f = fitting.NonLinearLSQFitter()
+    weights = None
+    y, x = np.indices(data.shape)
+    gfit = f(g_init, x, y, data, weights=weights)
+    return gfit.x_mean.value, gfit.y_mean.value
+
+
+def shape_params(data):
+    """
+    Calculate the centroid and basic shape parameters for an object
+    using image moments.
+
+    Parameters
+    ----------
+    data : array_like
+        The 2D image data.
 
     linear_eccen : is the distance between the object center and either of
                    its two ellipse foci.
     """
 
     result = {}
-    xcen, ycen = centroid(data)
+    xcen, ycen = centroid_com(data)
     m = findobj.moments(data, 1)
-    mu = findobj.moments_central(data, xcen, ycen, 2) / m[0, 0]
+    mu = findobj.moments_central(data, ycen, xcen, 2) / m[0, 0]
     result['xcen'] = xcen
     result['ycen'] = ycen
     musum = mu[2, 0] + mu[0, 2]
@@ -91,38 +144,40 @@ def shape_params(data):
     if pa < 0.0:
         pa += 180.0
     result['pa'] = pa
-    covar = np.array([[mu[2,0], mu[1,1]], [mu[1,1], mu[0,2]]])
+    covar = np.array([[mu[2, 0], mu[1, 1]], [mu[1, 1], mu[0, 2]]])
     result['covar'] = covar
     eigvals, eigvecs = np.linalg.eigh(covar)
     majsq = np.max(eigvals)
     minsq = np.min(eigvals)
     result['major_axis'] = np.sqrt(majsq)
     result['minor_axis'] = np.sqrt(minsq)
-    #tmp = np.sqrt(4.0*mu[1,1]**2 + mudiff**2)
-    #majsq = 0.5 * (musum + tmp)
-    #minsq = 0.5 * (musum - tmp)
-    #result['major_axis2'] = np.sqrt(majsq)
-    #result['minor_axis2'] = np.sqrt(minsq)
+    #if True:   # equivalent calculation
+    #   tmp = np.sqrt(4.0*mu[1,1]**2 + mudiff**2)
+    #   majsq = 0.5 * (musum + tmp)
+    #   minsq = 0.5 * (musum - tmp)
+    #   result['major_axis2'] = np.sqrt(majsq)
+    #   result['minor_axis2'] = np.sqrt(minsq)
     result['eccen'] = np.sqrt(1.0 - (minsq / majsq))
     result['linear_eccen'] = np.sqrt(majsq - minsq)
     return result
 
 
-class Gaussian2DImg(Kernel2D):
-    _separable = True
-    _is_bool = False
+def moments_to_2DGaussian(amplitude, x_mean, y_mean, mu):
+    """
+    mu:  normalized second-order central moments matrix [units of pixels**2]
+        mu = findobj.moments_central(data, ycen, xcen, 2) / m[0, 0]
+    """
 
-    def __init__(self, xsize, ysize, x_stddev=None, y_stddev=None,
-                 theta=0.0, cov_matrix=None, **kwargs):
-        #amplitude = 1. / (2 * np.pi * width ** 2)
-        amplitude = 1.0
-        self._model = models.Gaussian2D(amplitude, 0, 0, x_stddev=x_stddev,
-                                        y_stddev=y_stddev, theta=theta,
-                                        cov_matrix=cov_matrix)
+    cov_matrix = np.array([[mu[2, 0], mu[1, 1]], [mu[1, 1], mu[0, 2]]])
+    return models.Gaussian2D(amplitude, x_mean, y_mean, cov_matrix=cov_matrix)
 
-        #self._default_size = _round_up_to_odd_integer(8 * width)
-        self._default_size = 101
-        super(Gaussian2DImg, self).__init__(**kwargs)
-        self._truncation = np.abs(1. - 1 / self._normalization)
+
+def moments_to_2DGaussian2(amplitude, x_mean, y_mean, Ixx, Ixy, Iyy):
+    """
+    Ixx, Ixy, Iyy:  second-order central moments [units of pixels**2]
+    """
+
+    cov_matrix = np.array([[Ixx, Ixy], [Ixy, Iyy]])
+    return models.Gaussian2D(amplitude, x_mean, y_mean, cov_matrix=cov_matrix)
 
 
