8ad14f0a82b6b9947d1c89c6e46f6fcc74f33562
Larry Bradley <larry.bradley@gmail.com>
2014-05-16 16:06:14 -0400

    Use pytest bundled with astropy

--- photutils/detection/tests/test_findstars.py
+++ photutils/detection/tests/test_findstars.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-import pytest
+from astropy.tests.helper import pytest
 import os.path as op
 import itertools
 import numpy as np
