3ba5866af76f1754b82b8b8b797b7c2d7f25b58e
Larry Bradley <larry.bradley@gmail.com>
2014-05-19 16:10:20 -0400

    Fixed photutils/detection/__init__.py docstring

--- photutils/detection/__init__.py
+++ photutils/detection/__init__.py
@@ -1,5 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
-This subpackage contains modules and packages for interpreting data storage
-formats used by and in astropy.
+This subpackage contains modules and packages for identifying sources in
+an astronomical image and estimating their morphological parameters
+(e.g. centroid and shape parameters).
 """
