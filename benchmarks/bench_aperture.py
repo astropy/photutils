Fixing after aperture broadcasting removal in benchmark for multiobject multi apertures

benchmarks/bench_aperture.py
@@ -61,6 +61,7 @@ c[name]['elli']     = (5., 2., 0.5)
 c[name]['elli_ann'] = (2., 5., 4., 0.5)
 c[name]['iter']     = 1000
 c[name]['multiap']  = False
+c[name]['multipos'] = False
 
 name = "Big data, single small aperture"
 c[name] = {}
@@ -72,6 +73,7 @@ c[name]['elli']     = (5., 2., 0.5)
 c[name]['elli_ann'] = (2., 5., 4., 0.5)
 c[name]['iter']     = 1000
 c[name]['multiap']  = False
+c[name]['multipos'] = False
 
 name = "Big data, single big aperture"
 c[name] = {}
@@ -83,6 +85,7 @@ c[name]['elli']     = (50., 20., 0.5)
 c[name]['elli_ann'] = (20., 50., 40., 0.5)
 c[name]['iter']     = 10
 c[name]['multiap']  = False
+c[name]['multipos'] = False
 
 name = "Small data, multiple small apertures"
 c[name] = {}
@@ -93,7 +96,8 @@ c[name]['circ_ann'] = (5., 6.)
 c[name]['elli']     = (5., 2., 0.5)
 c[name]['elli_ann'] = (2., 5., 4., 0.5)
 c[name]['iter']     = 1
-c[name]['multiap']  = True
+c[name]['multiap']  = False
+c[name]['multipos'] = True
 
 name = "Big data, multiple small apertures"
 c[name] = {}
@@ -104,9 +108,9 @@ c[name]['circ_ann'] = (5., 6.)
 c[name]['elli']     = (5., 2., 0.5)
 c[name]['elli_ann'] = (2., 5., 4., 0.5)
 c[name]['iter']     = 1
-c[name]['multiap']  = True
+c[name]['multiap']  = False
+c[name]['multipos'] = True
 
-# TODO: multiple apertures per object are not supported by the Aperture objects
 name = "Big data, multiple small apertures, multiple per object"
 c[name] = {}
 c[name]['dims']     = (1000, 1000)
@@ -114,6 +118,7 @@ c[name]['pos']      = (np.random.uniform(250., 750., 1000), np.random.uniform(25
 c[name]['circ']     = (np.linspace(1., 10., 10).reshape((10, 1)),)
 c[name]['iter']     = 1
 c[name]['multiap']  = True
+c[name]['multipos'] = True
 
 name = "Big data, multiple big apertures"
 c[name] = {}
@@ -124,7 +129,8 @@ c[name]['circ_ann'] = (50., 60.)
 c[name]['elli']     = (50., 20., 0.5)
 c[name]['elli_ann'] = (20., 50., 40., 0.5)
 c[name]['iter']     = 1
-c[name]['multiap']  = True
+c[name]['multiap']  = False
+c[name]['multipos'] = True
 
 f = {}
 f['circ'] = photutils.CircularAperture
@@ -134,17 +140,13 @@ f['elli_ann'] = photutils.EllipticalAnnulus
 
 
 # Select subset of defined tests and functions to run, to save time.
-
-# Removed as not yet supported:
-#               "Big data, multiple small apertures, multiple per object",
-
-
 names_to_run = ["Small data, single small aperture",
                 "Big data, single small aperture",
                 "Big data, single big aperture",
                 "Small data, multiple small apertures",
                 "Big data, multiple small apertures",
-                "Big data, multiple big apertures"]
+                "Big data, multiple big apertures",
+                "Big data, multiple small apertures, multiple per object"]
 
 functions_to_run = ['circ']
 
@@ -184,14 +186,30 @@ if not args.show:
                 time1 = time.time()
                 for i in range(c[name]['iter']):
                     # Check whether it is single or multiple apertures
-                    if c[name]['multiap']:
-                        photutils.aperture_photometry(data, x, y,
-                                                      [f[t](*c[name][t])] * len(x),
-                                                      subpixels=subpixels)
-                    else:
+                    if not c[name]['multipos'] and not c[name]['multiap']:
                         photutils.aperture_photometry(data, x, y,
                                                       f[t](*c[name][t]),
                                                       subpixels=subpixels)
+
+                    elif c[name]['multipos'] and not c[name]['multiap']:
+                        photutils.aperture_photometry(data, x, y,
+                                                      [f[t](*c[name][t])] * len(x),
+                                                      subpixels=subpixels)
+
+                    elif c[name]['multipos'] and c[name]['multiap']:
+                        apertures = []
+                        for index in range(len(c[name][t][0])):
+                            apertures.append([f[t](*c[name][t][0][index])] * len(x))
+                        photutils.aperture_photometry(data, x, y, apertures,
+                                                      subpixels=subpixels)
+
+                    else:
+                        apertures = []
+                        for index in range(len(c[name][t][0])):
+                            apertures.append(f[t](*c[name][t][0][index]))
+                        photutils.aperture_photometry(data, x, y, apertures,
+                                                      subpixels=subpixels)
+
                 time2 = time.time()
                 time_sec = (time2 - time1) / c[name]['iter']
                 print("%10.5f " % (time_sec * 1000.),)