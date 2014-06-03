6e1c5e543803e21ca8494056794bc1fcba2cca47
Larry Bradley <larry.bradley@gmail.com>
2014-06-03 15:29:57 -0400

    Added upsample cython function

--- photutils/utils/upsample.pyx
+++ photutils/utils/upsample.pyx
@@ -1,12 +1,14 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+#cython: boundscheck=False
+#cython: wraparound=False
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import numpy as np
-cimport numpy as np
 
+cimport cython
+cimport numpy as np
 DTYPE = np.float
 ctypedef np.float_t DTYPE_t
-cimport cython
 
 
 def upsample(np.ndarray[DTYPE_t, ndim=2] array, int factor):
@@ -15,9 +17,9 @@ def upsample(np.ndarray[DTYPE_t, ndim=2] array, int factor):
     cdef int nx_new = nx * factor
     cdef int ny_new = ny * factor
     cdef unsigned int i, j
-    cdef np.ndarray[DTYPE_t, ndim=2] result = np.zeros((ny_new, nx_new), dtype=DTYPE)
+    cdef np.ndarray[DTYPE_t, ndim=2] result = np.zeros((ny_new, nx_new),
+                                                       dtype=DTYPE)
     cdef float factor_sq = factor * factor
-
     for i in range(nx_new):
         for j in range(ny_new):
             result[j, i] += array[int(j / 2), int(i / 2)] / factor_sq
