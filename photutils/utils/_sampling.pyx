8d05197441b61775bcb8970c63cd61fbe4fcb61e
Larry Bradley <larry.bradley@gmail.com>
2014-06-11 19:10:39 -0400

    Remove docstrings from _sampling.pyx

--- photutils/utils/_sampling.pyx
+++ photutils/utils/_sampling.pyx
@@ -14,34 +14,7 @@ __all__ = ['_downsample', '_upsample']
 
 
 def _downsample(np.ndarray[DTYPE_t, ndim=2] array, int factor):
-    """
-    Downsample an image by combining image pixels.  This process
-    conserves image flux.  If the dimensions of `image` are
-    not a whole-multiple of `factor`, the extra rows/columns will
-    not be included in the output downsampled image.
-
-    Parameters
-    ----------
-    image : array_like, float
-        The 2D array of the image.
-
-    factor : int
-        Downsampling integer factor along each axis.
-
-    Returns
-    -------
-    output : array_like
-        The 2D array of the resampled image.
-
-    Examples
-    --------
-    >>> from photutils import utils
-    >>> img = np.arange(16.).reshape((4, 4))
-    >>> utils.downsample(img, 2)
-    array([[ 10.,  18.],
-           [ 42.,  50.]])
-    """
-
+    """See docstring of wrapper in sampling.py."""
     cdef int nx = array.shape[1]
     cdef int ny = array.shape[0]
     cdef int nx_new = nx // factor
@@ -58,34 +31,7 @@ def _downsample(np.ndarray[DTYPE_t, ndim=2] array, int factor):
 
 
 def _upsample(np.ndarray[DTYPE_t, ndim=2] array, int factor):
-    """
-    Upsample an image by replicating image pixels.  This process
-    conserves image flux.
-
-    Parameters
-    ----------
-    image : array_like, float
-        The 2D array of the image.
-
-    factor : int
-        Upsampling integer factor along each axis.
-
-    Returns
-    -------
-    output : array_like
-        The 2D array of the resampled image.
-
-    Examples
-    --------
-    >>> from photutils import utils
-    >>> img = np.array([[0., 1.], [2., 3.]])
-    >>> utils.upsample(img, 2)
-    array([[ 0.  ,  0.  ,  0.25,  0.25],
-           [ 0.  ,  0.  ,  0.25,  0.25],
-           [ 0.5 ,  0.5 ,  0.75,  0.75],
-           [ 0.5 ,  0.5 ,  0.75,  0.75]])
-    """
-
+    """See docstring of wrapper in sampling.py."""
     cdef int nx = array.shape[1]
     cdef int ny = array.shape[0]
     cdef int nx_new = nx * factor
