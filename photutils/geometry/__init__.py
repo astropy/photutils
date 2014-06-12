2ce1b8b5dc80d99e92e3ef9eff84ff82cadc6ef3
Brigitta Sipocz <bsipocz@gmail.com>
2014-06-12 13:10:09 +0100

    Adding __init__.py for geometry subpackage

---
+++ photutils/geometry/__init__.py
@@ -0,0 +1,8 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+Geometry subpackge for low-level geometry functions.
+"""
+
+from .circular_overlap import *
+from .elliptical_exact import *
