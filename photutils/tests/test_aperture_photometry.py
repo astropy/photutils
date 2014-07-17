267bf81f19e77eba088fd0773fa7b6be80d558a5
Brigitta Sipocz <bsipocz@gmail.com>
2014-07-17 16:21:17 +0100

    adding errors back to tests

--- photutils/tests/test_aperture_photometry.py
+++ photutils/tests/test_aperture_photometry.py
@@ -54,3 +54,55 @@ def test_inside_array_simple(aperture_cl, aperture, radius):
     assert_allclose(table3['aperture_sum'], true_flux)
     assert table1['aperture_sum'] < table3['aperture_sum']
     assert_allclose(table2['aperture_sum'], table3['aperture_sum'], atol=0.1)
+
+
+class BaseTestAperturePhotometry(object):
+
+    def test_scalar_error_no_gain(self):
+
+        # Scalar error, no gain.
+        error = 1.
+        if not hasattr(self, 'mask'):
+            mask = None
+        else:
+            mask = self.mask
+
+        fluxtable = aperture_photometry(self.data, self.position,
+                                        self.aperture,
+                                        mask=mask, error=error)
+        assert_allclose(fluxtable['aperture_sum'], self.true_flux)
+
+        true_error = error * np.sqrt(self.area)
+        assert_allclose(fluxtable['aperture_sum_err'], true_error)
+
+    def test_array_error_no_gain(self):
+
+        # Array error, no gain.
+        error = np.ones(self.data.shape, dtype=np.float)
+        if not hasattr(self, 'mask'):
+            mask = None
+        else:
+            mask = self.mask
+
+        fluxtable = aperture_photometry(self.data, self.position,
+                                        self.aperture,
+                                        mask=mask, error=error)
+        assert_allclose(fluxtable['aperture_sum'], self.true_flux)
+
+        if hasattr(self, 'true_variance'):
+            true_error = np.sqrt(self.true_variance - self.true_flux)
+        else:
+            true_error = np.sqrt(self.area)
+
+        assert_allclose(fluxtable['aperture_sum_err'], true_error)
+
+
+class TestCircular(BaseTestAperturePhotometry):
+
+    def setup_class(self):
+        self.data = np.ones((40, 40), dtype=np.float)
+        self.position = (20., 20.)
+        r = 10.
+        self.aperture = ('circular', r)
+        self.area = np.pi * r * r
+        self.true_flux = self.area
