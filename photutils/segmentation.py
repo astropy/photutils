8c57e5876e6e541d1954da3bcc10704cb4fb2f3f
Larry Bradley <larry.bradley@gmail.com>
2014-08-26 22:09:08 -0400

    More work on segment_props

--- photutils/segmentation.py
+++ photutils/segmentation.py
@@ -7,10 +7,10 @@ from astropy.table import Table, Column
 from skimage.measure._regionprops import _cached_property, _RegionProperties
 
 
-__all__ = ['segment_props', 'segment_photometry']
+__all__ = ['SegmentProperties', 'segment_props', 'segment_photometry']
 
 
-class _SegmentProperties(object):
+class SegmentProperties(object):
     def __init__(self, image, segment_image, label, slice):
         self.label = label
         self._slice = slice
@@ -52,51 +52,166 @@ class _SegmentProperties(object):
     #radii = np.sqrt(npix / np.pi)
 
 
-def segment_props(image, segment_image, mask_image=None,
-                  mask_method='exclude', labels=None, out_table=False):
+def segment_props(image, segment_image, mask=None, mask_method='exclude',
+                  background=None, labels=None, output_table=False):
     """
+
+
     Parameters
     ----------
+    data : array_like
+        The 2D array on which to perform photometry.
+
+    segment_image : array_like
+        A 2D segmentation image where sources are marked by different
+        positive integer values.  A value of zero is reserved for the
+        background.
+
+    mask : array_like, bool, optional
+        A boolean mask with the same shape as ``data``, where a `True`
+        value indicates the corresponding element of ``image`` is
+        masked.  Use the ``mask_method`` keyword to select the method
+        used to treat masked pixels.
+
+    mask_method : {'exclude', 'interpolate'}, optional
+        Method used to treat masked pixels.  The currently supported
+        methods are:
+
+        'exclude'
+            Exclude masked pixels from all calculations.  This is the
+            default.
+
+        'interpolate'
+            The value of masked pixels are replaced by the mean value of
+            the neighboring non-masked pixels.
+
+    background : float or array_like, optional
+        The background level of the input ``data``.  ``background`` may
+        either be a scalar value or a 2D image with the same shape as
+        the input ``data``.  If the input ``data`` has been
+        background-subtracted, then set ``background`` to `None` (which
+        is the default).
+
+    labels : int, sequence of ints or None
+        Subset of ``segment_image`` labels for which to perform the
+        photometry.  If `None`, then photometry will be performed for
+        all source segments.
+
+    output_table : bool, optional
+        If `True` then return an astropy `astropy.table.Table`,
+        otherwise return a list of `SegmentProperties`.
 
     Returns
     -------
+    output : `astropy.table.Table` or list of `SegmentProperties`.
 
-    xcen, ycen: centroids
-    area
-    radius_equivalent
-    major/minor axis
-    eccen
-    theta
-    2nd order central moments
+        * If ``output_table = True``: `astropy.table.Table`
+              A table of the photometry of the segmented sources
+              containing the columns listed below.
 
+        * If ``output_table = False``: list
+              A list of `SegmentProperties`, one for each source
+              segment.
 
-        * ``'xcen', 'ycen'``: object centroid (zero-based origin).
-        * ``'area'``: the number pixels in the source segment.
-        * ``'radius_equiv'``: the equivalent circular radius derived
-          from the source ``area``.
+    Notes
+    -----
+    The following properties can be accessed either as columns in an
+    `astropy.table.Table` or as attributes or keys of
+    `SegmentProperties`:
 
+    **id** : int
+        The source identification number corresponding to the object
+        label in the ``segment_image``.
+
+    **xcen**, **ycen** : float
+        The ``x`` and ``y`` coordinates of the centroid within the
+        source segment.
+
+    **max_value**, **min_value** : float
+        The minimum and maximum pixel values within the source segment.
+
+    **area** : float
+        The number pixels in the source segment.
+
+    **equivalent_radius** : float
+        The radius of a circle with the same ``area`` as the source
+        segment.
+
+    **perimeter** : float
+        The perimeter of source segment, approximated using a line
+        through the centers of the border pixels using a 4-connectivity.
+
+    **major_axis_length** : float
+        The length of the major axis of the ellipse that has the same
+        normalized second central moments as the region.
+
+    **minor_axis_length** : float
+        The length of the minor axis of the ellipse that has the same
+        normalized second central moments as the region.
+
+    **eccentricity** : float
+        The cccentricity of the ellipse that has the same second-moments
+        as the region. The eccentricity is the ratio of the distance
+        between its minor and major axis length. The value is between 0
+        and 1.
+
+    **orientation** : float
+        The ngle between the X-axis and the major axis of the ellipse
+        that has the same second-moments as the region. Ranging from
+        -pi/2 to pi/2 in counter-clockwise direction.
+
+    The following properties can be accessed only as columns in an
+    `astropy.table.Table`:
+
+    **xmin**, **xmax**, **ymin**, **ymax** : float
+        The bounding box of the source segment.
+
+    The following properties can be accessed only as attributes or keys
+    of `SegmentProperties`:
+
+    **min_position**, **max_position** : 2-tuple
+        The image coordinates ``(y, x)`` of the minimum and maximum
+        pixel values.
+
+    **bbox** : 4-tuple
+        The bounding box ``(min_row, min_col, max_row, max_col)`` of the
+        source segment.
+
+    **coords** : (N, 2) `numpy.ndarray`
+        The coordinate list ``(row, col)`` of the source segment.
+
+    **moments** :
+        2nd order central moments
+
+    **cutout_image** : `numpy.ndarray`
+        A 2D cutout image based on the bounding box (``bbox``) of the
+        source segment.
+
+    **masked_cutout_image** : `numpy.ndarray`
+        A 2D cutout image based on the bounding box (``bbox``) of the
+        source segment, but including *only* the segmented pixels of the
+        object.
     """
 
     from scipy import ndimage
-    objects = ndimage.find_objects(segment_image)
+    objslicess = ndimage.find_objects(segment_image)
     objpropslist = []
-    for i, sl in enumerate(objects):
-        if sl is None:
+    for i, objslice in enumerate(objslices):
+        if objslice is None:
             continue
         label = i + 1
-        cache = True
-        #objprops = _RegionProperties(sl, label, segment_image, image, cache)
-        objprops = _SegmentProperties(image, segment_image, label, sl)
+        objprops = SegmentProperties(image, segment_image, label, objslice)
         objpropslist.append(objprops)
 
-    if not out_table:
+    if not output_table:
         return objpropslist
     else:
         props_table = Table()
         data = [getattr(objprops, 'centroid') for objprops in objpropslist]
         xcen, ycen = np.transpose(data)
 
-        props_table['xmin'] = Column(xmin)
+        props_table['xcen'] = Column(xcen)
+        props_table['ycen'] = Column(ycen)
         data = [getattr(objprops, 'bbox') for objprops in objpropslist]
         xmin, ymin, xmax, ymax = np.transpose(data)
         props_table['xmin'] = Column(xmin)
