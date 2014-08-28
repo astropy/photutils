0b1df107137ee2dc3608715e3d1d9c2455d868a1
Larry Bradley <larry.bradley@gmail.com>
2014-08-28 15:33:21 -0400

    Add check_random_state from scikit-learn

---
+++ photutils/utils/check_random_state.py
@@ -0,0 +1,39 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+This subpackage contains developer-oriented utilities.
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+import numpy as np
+import numbers
+
+
+__all__ = ['check_random_state']
+
+
+def check_random_state(seed):
+    """
+    Turn seed into a `numpy.random.RandomState` instance.
+
+    Parameters
+    ----------
+    seed : `None`, int, or `numpy.random.RandomState`
+        If ``seed`` is `None`, return the `~numpy.random.RandomState`
+        singleton used by ``numpy.random``.  If ``seed`` is an `int`,
+        return a new `~numpy.random.RandomState` instance seeded with
+        ``seed``.  If ``seed`` is already a `~numpy.random.RandomState`,
+        return it.  Otherwise raise ``ValueError``.
+
+    Notes
+    -----
+    This routine is from scikit-learn.  See http://scikit-learn.org/stable/developers/utilities.html#validation-tools.
+    """
+
+    if seed is None or seed is np.random:
+        return np.random.mtrand._rand
+    if isinstance(seed, (numbers.Integral, np.integer)):
+        return np.random.RandomState(seed)
+    if isinstance(seed, np.random.RandomState):
+        return seed
+    raise ValueError('%r cannot be used to seed a numpy.random.RandomState'
+                     ' instance' % seed)
