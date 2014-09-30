17542cf9ee3e7edb8443706c3ddd9ba645c2a436
Larry Bradley <larry.bradley@gmail.com>
2014-09-30 15:44:28 -0400

    Move morphology from detection

---
+++ photutils/morphology.py
@@ -0,0 +1,281 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+import numpy as np
+from astropy.modeling import models
+from astropy.modeling.fitting import LevMarLSQFitter
+
+
+__all__ = ['centroid_com', 'gaussian1d_moments', 'centroid_1dg',
+           'centroid_2dg', 'fit_2dgaussian', 'data_properties']
+
+
+def _convert_image(data, mask=None):
+    """
+    Convert the input data to a float64 (double) `numpy.ndarray`,
+    required for input to `skimage.measure.moments` and
+    `skimage.measure.moments_central`.
+
+    The input ``data`` is copied unless it already has that
+    `numpy.dtype`.
+
+    If ``mask`` is input, then masked pixels are set to zero in the
+    output ``data``.
+
+    Parameters
+    ----------
+    data : array_like
+        The 2D array of the image.
+
+    mask : array_like (bool), optional
+        A boolean mask, with the same shape as ``data``, where a `True`
+        value indicates the corresponding element of ``data`` is masked.
+        Masked pixels are set to zero in the output ``data``.
+
+    Returns
+    -------
+    image : `numpy.ndarray`, float64
+        The converted 2D array of the image, where masked pixels have
+        been set to zero.
+    """
+
+    try:
+        if mask is None:
+            copy = False
+        else:
+            copy = True
+        image = np.asarray(data).astype(np.float, copy=copy)
+    except TypeError:
+        image = np.asarray(data).astype(np.float)    # for numpy <= 1.6
+    if mask is not None:
+        mask = np.asarray(mask)
+        if data.shape != mask.shape:
+            raise ValueError('data and mask must have the same shape')
+        image[mask] = 0.0
+    return image
+
+
+def centroid_com(data, mask=None):
+    """
+    Calculate the centroid of a 2D array as its center of mass
+    determined from image moments.
+
+    Parameters
+    ----------
+    data : array_like
+        The 2D array of the image.
+
+    mask : array_like (bool), optional
+        A boolean mask, with the same shape as ``data``, where a `True`
+        value indicates the corresponding element of ``data`` is masked.
+
+    Returns
+    -------
+    xcen, ycen : float
+        (x, y) coordinates of the centroid.
+    """
+
+    from skimage.measure import moments
+    data = _convert_image(data, mask=mask)
+    m = moments(data, 1)
+    xcen = m[1, 0] / m[0, 0]
+    ycen = m[0, 1] / m[0, 0]
+    return xcen, ycen
+
+
+def gaussian1d_moments(data):
+    """
+    Estimate the 1D Gaussian parameters from the moments of 1D data.
+    This function can be useful for providing initial parameter values
+    when fitting a 1D Gaussian to the ``data``.
+
+    Parameters
+    ----------
+    data : array_like
+        The 1D array of the image.
+
+    Returns
+    -------
+    amplitude, mean, stddev : float
+        The estimated parameters of a 1D Gaussian.
+    """
+
+    x = np.arange(data.size)
+    xc = np.sum(x * data) / np.sum(data)
+    stddev = np.sqrt(abs(np.sum(data * (x - xc)**2) / np.sum(data)))
+    amplitude = np.ptp(data)
+    return amplitude, xc, stddev
+
+
+def centroid_1dg(data, uncertainty=None, mask=None):
+    """
+    Calculate the centroid of a 2D array by fitting 1D Gaussians to the
+    marginal x and y distributions of the image.
+
+    Parameters
+    ----------
+    data : array_like or `~astropy.nddata.NDData`
+        The 2D array of the image.
+
+    uncertainty : array_like, optional
+        The 2D array of the 1-sigma errors of the input ``data``.  If
+        ``uncertainty`` is input it will override ``data.uncertainty``
+        for `~astropy.nddata.NDData` inputs.
+
+    mask : array_like, bool, optional
+        (Not yet implemented).
+        A boolean mask with the same shape as ``data``, where a `True`
+        value indicates the corresponding element of ``data`` is
+        invalid.  If ``mask`` is input it will override ``data.mask``
+        for `~astropy.nddata.NDData` inputs.
+
+    Returns
+    -------
+    xcen, ycen : tuple of floats
+        (x, y) coordinates of the centroid.
+    """
+
+    data_x = data.sum(axis=0)
+    data_y = data.sum(axis=1)
+    if uncertainty is None:
+        weights_x = None
+        weights_y = None
+    else:
+        image_err_x = np.sqrt(np.sum(uncertainty**2, axis=0))
+        image_err_y = np.sqrt(np.sum(uncertainty**2, axis=1))
+        weights_x = 1.0 / image_err_x
+        weights_y = 1.0 / image_err_y
+
+    marginal_data = [data_x, data_y]
+    marginal_weights = [weights_x, weights_y]
+    centroid = []
+    for (data, weights) in zip(marginal_data, marginal_weights):
+        params_init = gaussian1d_moments(data)
+        g_init = models.Gaussian1D(*params_init)
+        fitter = LevMarLSQFitter()
+        x = np.arange(data.size)
+        g_fit = fitter(g_init, x, data, weights=weights)
+        centroid.append(g_fit.mean.value)
+    return centroid
+
+
+def centroid_2dg(data, uncertainty=None, mask=None):
+    """
+    Calculate the centroid of a 2D array by fitting a 2D Gaussian to the
+    image.
+
+    Parameters
+    ----------
+    data : array_like or `~astropy.nddata.NDData`
+        The 2D array of the image.
+
+    uncertainty : array_like, optional
+        The 2D array of the 1-sigma errors of the input ``data``.
+
+    mask : array_like, bool, optional
+        (Not yet implemented).
+        A boolean mask with the same shape as ``data``, where a `True`
+        value indicates the corresponding element of ``data`` is
+        invalid.  If ``mask`` is input it will override ``data.mask``
+        for `~astropy.nddata.NDData` inputs.
+
+    Returns
+    -------
+    centroid : tuple
+        (x, y) coordinates of the centroid.
+    """
+
+    gfit = fit_2dgaussian(data, uncertainty=uncertainty, mask=mask)
+    return gfit.x_mean.value, gfit.y_mean.value
+
+
+def fit_2dgaussian(data, uncertainty=None, mask=None):
+    """
+    Fit a 2D Gaussian to a 2D image.
+
+    Parameters
+    ----------
+    data : array_like or `~astropy.nddata.NDData`
+        The 2D array of the image.
+
+    uncertainty : array_like, optional
+        The 2D array of the 1-sigma errors of the input ``data``.
+
+    mask : array_like, bool, optional
+        (Not yet implemented).
+        A boolean mask with the same shape as ``data``, where a `True`
+        value indicates the corresponding element of ``data`` is
+        invalid.  If ``mask`` is input it will override ``data.mask``
+        for `~astropy.nddata.NDData` inputs.
+
+    Returns
+    -------
+    centroid : tuple
+        (x, y) coordinates of the centroid.
+    """
+
+    if uncertainty is None:
+        weights = None
+    else:
+        weights = 1.0 / uncertainty
+    init_param = shape_params(data, mask=mask)
+    init_amplitude = np.ptp(data)
+    g_init = models.Gaussian2D(init_amplitude, init_param['xcen'],
+                               init_param['ycen'], init_param['major_axis'],
+                               init_param['minor_axis'],
+                               theta=init_param['angle'])
+    fitter = LevMarLSQFitter()
+    y, x = np.indices(data.shape)
+    gfit = fitter(g_init, x, y, data, weights=weights)
+    return gfit
+
+
+def data_properties(data, mask=None, background=None):
+    """
+    Calculate the centroid and morphological properties of a 2D array,
+    e.g., an image cutout of an object.
+
+    Parameters
+    ----------
+    data : array_like or `~astropy.units.Quantity`
+        The 2D array of the image.
+
+    mask : array_like (bool), optional
+        A boolean mask, with the same shape as ``data``, where a
+        `True` value indicates the corresponding element of ``data``
+        is masked.  Masked data are excluded/ignored from all
+        calculations.
+
+    background : float, array_like, or `~astropy.units.Quantity`, optional
+        The background level of the input ``data``.  ``background``
+        may either be a scalar value or a 2D image with the same
+        shape as the input ``data``.  If the input ``data`` has been
+        background-subtracted, then set ``background`` to `None`
+        (the default) or ``0.``.
+
+    Returns
+    -------
+    """
+
+    segment_image = np.ones(data.shape)
+    return SegmentProperties(data, segment_image, 1, mask=mask,
+                             background=background)
+
+
+def _moments_to_2DGaussian(amplitude, x_mean, y_mean, mu):
+    """
+    mu:  normalized second-order central moments matrix [units of pixels**2]
+    mu = moments_central(image, ycen, xcen, 2) / m[0, 0]
+    """
+
+    cov_matrix = np.array([[mu[2, 0], mu[1, 1]], [mu[1, 1], mu[0, 2]]])
+    return models.Gaussian2D(amplitude, x_mean, y_mean, cov_matrix=cov_matrix)
+
+
+def _moments_to_2DGaussian2(amplitude, x_mean, y_mean, Ixx, Ixy, Iyy):
+    """
+    Ixx, Ixy, Iyy:  second-order central moments [units of pixels**2]
+    """
+
+    cov_matrix = np.array([[Ixx, Ixy], [Ixy, Iyy]])
+    return models.Gaussian2D(amplitude, x_mean, y_mean, cov_matrix=cov_matrix)
---
+++ photutils/tests/test_morphology.py
@@ -0,0 +1,83 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from astropy.tests.helper import pytest
+import numpy as np
+from numpy.testing import assert_allclose
+import itertools
+from ..morphology import (centroid_com, centroid_1dg, centroid_2dg,
+                          gaussian1d_moments, shape_params)
+from astropy.modeling import models
+try:
+    import skimage
+    HAS_SKIMAGE = True
+except ImportError:
+    HAS_SKIMAGE = False
+
+
+XCS = [25.0, 25.7, 26.1]
+YCS = [25.0, 26.2, 26.9]
+XSTDDEVS = [2.0, 3.2]
+YSTDDEVS = [2.0, 5.7]
+THETAS = np.array([30., 45.]) * np.pi / 180.
+
+
+@pytest.mark.parametrize(('xc_ref', 'yc_ref', 'x_stddev', 'y_stddev', 'theta'),
+                         list(itertools.product(XCS, YCS, XSTDDEVS, YSTDDEVS,
+                                                THETAS)))
+@pytest.mark.skipif('not HAS_SKIMAGE')
+def test_centroids(xc_ref, yc_ref, x_stddev, y_stddev, theta):
+    model = models.Gaussian2D(2.4, xc_ref, yc_ref, x_stddev=x_stddev,
+                              y_stddev=y_stddev, theta=theta)
+    y, x = np.mgrid[0:50, 0:50]
+    data = model(x, y)
+    xc, yc = centroid_com(data)
+    assert_allclose([xc_ref, yc_ref], [xc, yc], rtol=0, atol=1.e-3)
+    xc2, yc2 = centroid_1dg(data)
+    assert_allclose([xc_ref, yc_ref], [xc2, yc2], rtol=0, atol=1.e-3)
+    xc3, yc3 = centroid_2dg(data)
+    assert_allclose([xc_ref, yc_ref], [xc3, yc3], rtol=0, atol=1.e-3)
+
+
+@pytest.mark.skipif('not HAS_SKIMAGE')
+def test_centroid_com_mask():
+    """Test centroid_com with and without an image_mask."""
+    data = np.ones((2, 2)).astype(np.float)
+    mask = [[False, False], [True, True]]
+    centroid = centroid_com(data, mask=None)
+    centroid_mask = centroid_com(data, mask=mask)
+    assert_allclose([0.5, 0.5], centroid, rtol=0, atol=1.e-6)
+    assert_allclose([0.5, 0.0], centroid_mask, rtol=0, atol=1.e-6)
+
+
+@pytest.mark.skipif('not HAS_SKIMAGE')
+def test_centroid_com_mask_shape():
+    """
+    Test if ValueError raises if mask shape doesn't match data
+    shape.
+    """
+    with pytest.raises(ValueError):
+        mask = np.zeros((2, 2), dtype=bool)
+        centroid_com(np.zeros((4, 4)), mask=mask)
+
+
+@pytest.mark.skipif('not HAS_SKIMAGE')
+def test_shape_params_mask():
+    data = np.ones((2, 2)).astype(np.float)
+    mask = [[False, False], [True, True]]
+    params = shape_params(data, mask=None)
+    params_mask = shape_params(data, mask=mask)
+    result = [params['xcen'], params['ycen'], params['eccen']]
+    result_mask = [params_mask['xcen'], params_mask['ycen'],
+                   params_mask['eccen']]
+    assert_allclose([0.5, 0.5, 0.0], result, rtol=0, atol=1.e-6)
+    assert_allclose([0.5, 0.0, 1.0], result_mask, rtol=0, atol=1.e-6)
+
+
+def test_gaussian1d_moments():
+    x = np.arange(100)
+    ref = (75, 50, 5)
+    g = models.Gaussian1D(*ref)
+    data = g(x)
+    result = gaussian1d_moments(data)
+    assert_allclose(ref, result, rtol=0, atol=1.e-6)
