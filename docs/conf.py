a9bba0216b086fc6893060fd1d064ad2fe6f7fb1
Larry Bradley <larry.bradley@gmail.com>
2014-11-21 16:44:37 -0500

    Customize HTML theme

--- docs/conf.py
+++ docs/conf.py
@@ -104,6 +104,12 @@ release = package.__version__
 # variables set in the global configuration. The variables set in the
 # global configuration are listed below, commented out.
 
+html_theme_options = {
+    'logotext1': 'phot',   # white, semi-bold
+    'logotext2': 'utils',  # orange, light
+    'logotext3': ':docs'   # white, light
+}
+
 # Add any paths that contain custom themes here, relative to this directory.
 # To use a different custom theme, add the directory containing the theme.
 #html_theme_path = []
