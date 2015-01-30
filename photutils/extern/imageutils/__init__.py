7f68842a2052d2cba695dc67296e4357d1f6f310
Christoph Deil <Deil.Christoph@gmail.com>
2015-01-30 13:04:13 +0100

    Remove photutils/extern/imageutils

--- photutils/extern/imageutils/__init__.py
+++
@@ -1,6 +0,0 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-"""
-Image processing utilities for Astropy.
-"""
-
-from .nddata import *
--- photutils/psf.py
+++ photutils/psf.py
@@ -8,8 +8,7 @@ from astropy.modeling.parameters import Parameter
 from astropy.utils.exceptions import AstropyUserWarning
 from astropy.modeling.fitting import LevMarLSQFitter
 from astropy.modeling import Fittable2DModel
-from .extern.imageutils.nddata.utils import (extract_array, add_array,
-                                             subpixel_indices)
+from astropy.nddata.utils import extract_array, add_array, subpixel_indices
 from .utils import mask_to_mirrored_num
 
 
