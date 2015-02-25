085f45c2214c055377e27243fd3e47f271431a59
Brigitta Sipocz <bsipocz@gmail.com>
2015-02-25 19:01:28 +0000

    Adding photutils version number to testing header

--- photutils/conftest.py
+++ photutils/conftest.py
@@ -2,7 +2,17 @@
 # by importing them here in conftest.py they are discoverable by py.test
 # no matter how it is invoked within the source tree.
 
+import os
 from astropy.tests.pytest_plugins import *
+
+# This is to figure out the photutil version, rather than using Astropy's
+from . import version
+
+try:
+    packagename = os.path.basename(os.path.dirname(__file__))
+    TESTED_VERSIONS[packagename] = version.version
+except NameError:
+    pass
 
 # Uncomment the following line to treat all DeprecationWarnings as
 # exceptions
@@ -10,5 +20,5 @@ enable_deprecations_as_exceptions()
 
 # Add scikit-image to test header information
 PYTEST_HEADER_MODULES['scikit-image'] = 'skimage'
-PYTEST_HEADER_MODULES['astropy'] = 'astropy'
+PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
 del PYTEST_HEADER_MODULES['h5py']
