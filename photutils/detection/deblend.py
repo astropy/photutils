307aa6177d743e1fcb3cddd8a32e5beb6e97f686
Larry Bradley <larry.bradley@gmail.com>
2015-11-12 22:10:26 -0500

    Working version of source deblending

--- photutils/detection/deblend.py
+++ photutils/detection/deblend.py
@@ -5,13 +5,8 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from copy import deepcopy
 import numpy as np
-from astropy.table import Column, Table
-from astropy.convolution import Kernel2D
-from astropy.stats import sigma_clipped_stats
 from .core import _convolve_data, detect_sources
-from ..morphology import cutout_footprint, fit_2dgaussian
 from ..segmentation import remove_segments
-from ..utils.wcs_helpers import pixel_to_icrs_coords
 
 
 __all__ = ['deblend_source']
@@ -26,6 +21,8 @@ def deblend_source(data, segm, label, threshold, filter_kernel=None,
     data : `~numpy.ndarray`
         data should be smoothed by the same filter used in ``detect_sources``.
 
+    threshold : float or 2D array
+
     mode : {'exponential', 'linear'}
     """
 
@@ -38,9 +35,11 @@ def deblend_source(data, segm, label, threshold, filter_kernel=None,
 
     steps = np.arange(1., nlevels+1)
     if mode == 'exponential':
-        thresholds = threshold * (source_max / threshold)**(steps / nlevels)
+        thresholds = threshold * ((source_max / threshold) **
+                                  (steps / (nlevels + 1)))
     elif mode == 'linear':
-        thresholds = threshold + ((source_max - threshold) / nlevels) * steps
+        thresholds = threshold + ((source_max - threshold) /
+                                  (nlevels + 1)) * steps
     else:
         raise ValueError('"{0}" is an invalid mode; mode must be '
                          '"exponential" or "linear"')
@@ -67,26 +66,21 @@ def deblend_source(data, segm, label, threshold, filter_kernel=None,
     segm_tree_orig = deepcopy(segm_tree)
     nbranch = len(segm_tree)
     if nbranch == 0:
-        return
-    elif nbranch == 1:
-        return
+        return segm, None, None, None
     else:
-        for j in np.arange(nbranch-1):
-            intersection = (segm_tree[j] * segm_tree[j + 1]).astype(bool)
-            labels = np.unique(segm_tree[j + 1][intersection])
-            print(labels, len(labels), np.max(segm_tree[j + 1]))
+        for j in np.arange(1, nbranch):
+            intersection = (segm_tree[j] * segm_tree[j - 1]).astype(bool)
+            labels = np.unique(segm_tree[j][intersection])
 
-            if np.max(segm_tree[j]) > len(labels):
-                # remove intersecting labels in lower level, if higher level
-                # has more peaks.  Note that segm_tree will not be
-                # labeled properly until the end.
-                segm_tmp = remove_segments(segm_tree[j+1], labels,
+            if np.max(segm_tree[j-1]) > len(labels):
+                # If higher tree level has more peaks, then remove the
+                # intersecting labels in the lower level, add to the higher
+                # level, and relabel.
+                segm_tmp = remove_segments(segm_tree[j], labels,
                                            relabel=False)
-                segm_tree[j] = segm_tree[j] + segm_tmp
+                segm_tree[j] = segm_tree[j-1] + segm_tmp
+                segm_tree[j] = detect_sources(segm_tree[j], 0, npixels=1,
+                                              connectivity=connectivity)
 
-                print(j, 'need to remove labeled image in intersections')
-            else:
-                print(j, 'no change')
-
-        semg_deblend = watershed(-data, segm_tree[0], mask=segm)
+        segm_deblend = watershed(-data, segm_tree[-1], mask=segm)
         return segm_deblend, all_segm, segm_tree_orig, segm_tree
