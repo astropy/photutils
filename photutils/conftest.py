de6dd81a3adf39be1067a6f5b8befbc9f83135e1
Larry Bradley <larry.bradley@gmail.com>
2015-12-31 12:15:43 -0500

    Update conftest

--- photutils/conftest.py
+++ photutils/conftest.py
@@ -2,23 +2,33 @@
 # by importing them here in conftest.py they are discoverable by py.test
 # no matter how it is invoked within the source tree.
 
-import os
 from astropy.tests.pytest_plugins import *
-
-# This is to figure out photutils version, rather than using Astropy's
-from . import version
-
-try:
-    packagename = os.path.basename(os.path.dirname(__file__))
-    TESTED_VERSIONS[packagename] = version.version
-except NameError:
-    pass
 
 # Uncomment the following line to treat all DeprecationWarnings as
 # exceptions
 enable_deprecations_as_exceptions()
 
-# Add scikit-image to test header information
-PYTEST_HEADER_MODULES['scikit-image'] = 'skimage'
-PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
-del PYTEST_HEADER_MODULES['h5py']
+# Uncomment and customize the following lines to add/remove entries
+# from the list of packages for which version numbers are displayed
+# when running the tests
+try:
+    PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
+    PYTEST_HEADER_MODULES['scikit-image'] = 'skimage'
+    del PYTEST_HEADER_MODULES['h5py']
+except NameError:  # needed to support Astropy < 1.0
+    pass
+
+# Uncomment the following lines to display the version number of the
+# package rather than the version number of Astropy in the top line when
+# running the tests.
+import os
+
+# This is to figure out the affiliated package version, rather than
+# using Astropy's
+from . import version
+
+try:
+    packagename = os.path.basename(os.path.dirname(__file__))
+    TESTED_VERSIONS[packagename] = version.version
+except NameError:   # Needed to support Astropy <= 1.0.0
+    pass
