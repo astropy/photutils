de03dbf9b5d6d22b62c118fb365b47d963360c31
hamogu <hgunther@mit.edu>
2016-02-17 17:11:26 -0500

    start fixing PSF/PRF tests

--- photutils/tests/test_psf_photometry.py
+++ photutils/tests/test_psf_photometry.py
@@ -9,7 +9,7 @@ from astropy.modeling.models import Gaussian2D
 from astropy.convolution.utils import discretize_model
 from astropy.table import Table
 
-from ..psf import (create_prf, DiscretePRF, psf_photometry,
+from ..psf import (DiscretePRF, psf_photometry,
                    IntegratedGaussianPSF, subtract_psf)
 
 try:
@@ -24,8 +24,9 @@ GAUSSIAN_WIDTH = 1.
 IMAGE_SIZE = 101
 
 # Position and FLUXES of test sources
-POSITIONS = [(50, 50), (23, 83), (12, 80), (86, 84)]
-FLUXES = [np.pi * 10, 3.654, 20., 80 / np.sqrt(3)]
+INTAB = Table([[50., 23, 12, 86], [50., 83, 80, 84],
+               [np.pi * 10, 3.654, 20., 80 / np.sqrt(3)]],
+              names=['x_0', 'y_0', 'flux_0'])
 
 # Create test psf
 psf_model = Gaussian2D(1. / (2 * np.pi * GAUSSIAN_WIDTH ** 2), PSF_SIZE // 2,
@@ -37,8 +38,7 @@ test_psf = discretize_model(psf_model, (0, PSF_SIZE), (0, PSF_SIZE),
 image = np.zeros((IMAGE_SIZE, IMAGE_SIZE))
 
 # Add sources to test image
-for flux, position in zip(FLUXES, POSITIONS):
-    x, y = position
+for x, y, flux in INTAB:
     model = Gaussian2D(flux / (2 * np.pi * GAUSSIAN_WIDTH ** 2),
                        x, y, GAUSSIAN_WIDTH, GAUSSIAN_WIDTH)
     image += discretize_model(model, (0, IMAGE_SIZE), (0, IMAGE_SIZE),
@@ -61,16 +61,20 @@ for x, y, flux in WIDE_INTAB:
 def test_create_prf_mean():
     """
     Check if create_prf works correctly on simulated data.
+    Position input format: list
     """
-    prf = create_prf(image, POSITIONS, PSF_SIZE, subsampling=1, mode='mean')
+    prf = DiscretePRF.create_from_image(image, list(INTAB['x_0', 'y_0'].as_array()),
+                                        PSF_SIZE, subsampling=1, mode='mean')
     assert_allclose(prf._prf_array[0, 0], test_psf, atol=1E-8)
 
 
 def test_create_prf_median():
     """
     Check if create_prf works correctly on simulated data.
+    Position input format: astropy.table.Table
     """
-    prf = create_prf(image, POSITIONS, PSF_SIZE, subsampling=1, mode='median')
+    prf = DiscretePRF.create_from_image(image, np.array(INTAB['x_0', 'y_0']),
+                                        PSF_SIZE, subsampling=1, mode='median')
     assert_allclose(prf._prf_array[0, 0], test_psf, atol=1E-8)
 
 
@@ -81,8 +85,8 @@ def test_create_prf_nan():
     image_nan = image.copy()
     image_nan[52, 52] = np.nan
     image_nan[52, 48] = np.nan
-    prf = create_prf(image_nan, POSITIONS, PSF_SIZE, subsampling=1,
-                     fix_nan=True)
+    prf = DiscretePRF.create_from_image(image, np.array(INTAB['x_0', 'y_0']),
+                                        PSF_SIZE, subsampling=1, fix_nan=True)
     assert not np.isnan(prf._prf_array[0, 0]).any()
 
 
@@ -90,24 +94,11 @@ def test_create_prf_flux():
     """
     Check if create_prf works correctly when FLUXES are specified.
     """
-    prf = create_prf(image, POSITIONS, PSF_SIZE, fluxes=FLUXES, subsampling=1)
+    prf = DiscretePRF.create_from_image(image, np.array(INTAB['x_0', 'y_0']),
+                                        PSF_SIZE, subsampling=1, mode='median',
+                                        fluxes=INTAB['flux_0'])
     assert_allclose(prf._prf_array[0, 0].sum(), 1)
     assert_allclose(prf._prf_array[0, 0], test_psf, atol=1E-8)
-
-
-@pytest.mark.skipif('not HAS_SCIPY')
-def test_discrete_prf_fit():
-    """
-    Check if fitting of discrete PSF model works.
-    """
-    prf = DiscretePRF(test_psf, subsampling=1)
-    prf.x_0 = 50
-    prf.y_0 = 50
-
-    # test_psf is normalized to unity
-    indices = np.indices(image.shape)
-    flux = prf.fit(image, indices)
-    assert_allclose(flux, FLUXES[0], rtol=1E-5)
 
 
 @pytest.mark.skipif('not HAS_SCIPY')
@@ -116,20 +107,26 @@ def test_psf_photometry_discrete():
     Test psf_photometry with discrete PRF model.
     """
     prf = DiscretePRF(test_psf, subsampling=1)
-    f = psf_photometry(image, POSITIONS, prf)
-    assert_allclose(f, FLUXES, rtol=1E-6)
+    f = psf_photometry(image, INTAB, prf)
+    for n in ['x', 'y', 'flux']:
+        assert_allclose(f[n + '_0'], f[n + '_fit'], rtol=1e-6)
 
 
 @pytest.mark.skipif('not HAS_SCIPY')
 def test_tune_coordinates():
     """
-    Test psf_photometry with discrete PRF model and tune_coordinates=True.
+    Test psf_photometry with discrete PRF model and coordinates that need
+    to be adjusted in the fit.
     """
     prf = DiscretePRF(test_psf, subsampling=1)
+    prf.x_0.fixed = False
+    prf.y_0.fixed = False
     # Shift all sources by 0.3 pixels
-    positions = [(_[0] + 0.3, _[1] + 0.3) for _ in POSITIONS]
-    f = psf_photometry(image, positions, prf, tune_coordinates=True)
-    assert_allclose(f, FLUXES, rtol=1E-6)
+    intab = INTAB.copy()
+    intab['x_0'] += 0.3
+    f = psf_photometry(image, intab, prf)
+    for n in ['x', 'y', 'flux']:
+        assert_allclose(f[n + '_0'], f[n + '_fit'], rtol=1e-3)
 
 
 @pytest.mark.skipif('not HAS_SCIPY')
@@ -138,20 +135,20 @@ def test_psf_boundary():
     Test psf_photometry with discrete PRF model at the boundary of the data.
     """
     prf = DiscretePRF(test_psf, subsampling=1)
-    # Shift all sources by 0.3 pixels
-    f = psf_photometry(image, [(1, 1)], prf)
-    assert f == 0
+    f = psf_photometry(image, np.ones((2, 1)), prf)
+    assert_allclose(f['flux_fit'], 0, atol=1e-8)
 
 
 @pytest.mark.skipif('not HAS_SCIPY')
 def test_psf_boundary_gaussian():
     """
-    Test psf_photometry with discrete PRF model at the boundary of the data.
+    Test psf_photometry at the boundary of the data where no source is found.
+    This also tests the case were input positions is an array instead
+    of a table.
     """
     psf = IntegratedGaussianPSF(GAUSSIAN_WIDTH)
-    # Shift all sources by 0.3 pixels
-    f = psf_photometry(image, [(1, 1)], psf)
-    assert f == 0
+    f = psf_photometry(image, np.ones((2, 1)), psf)
+    assert_allclose(f['flux_fit'], 0, atol=1e-8)
 
 
 @pytest.mark.skipif('not HAS_SCIPY')
@@ -159,9 +156,10 @@ def test_psf_photometry_gaussian():
     """
     Test psf_photometry with Gaussian PSF model.
     """
-    prf = IntegratedGaussianPSF(GAUSSIAN_WIDTH)
-    f = psf_photometry(image, POSITIONS, prf)
-    assert_allclose(f, FLUXES, rtol=1E-3)
+    psf = IntegratedGaussianPSF(sigma=GAUSSIAN_WIDTH)
+    f = psf_photometry(image, INTAB, psf)
+    for n in ['x', 'y', 'flux']:
+        assert_allclose(f[n + '_0'], f[n + '_fit'], rtol=1e-3)
 
 
 @pytest.mark.skipif('not HAS_SCIPY')
@@ -170,7 +168,10 @@ def test_subtract_psf():
     Test subtract_psf
     """
     prf = DiscretePRF(test_psf, subsampling=1)
-    residuals = subtract_psf(image, prf, POSITIONS, FLUXES)
+    posflux = INTAB.copy()
+    for n in posflux.colnames:
+        posflux.rename_column(n, n.split('_')[0] + '_fit')
+    residuals = subtract_psf(image, prf, posflux)
     assert_allclose(residuals, np.zeros_like(image), atol=1E-6)
 
 
