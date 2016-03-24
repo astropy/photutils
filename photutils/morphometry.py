149e0abd45d95ad3b6af25c280b73d6c5f2cee70
Leonardo Ferreira <leonardo.ferreira.furg@gmail.com>
2016-03-24 10:03:33 -0700

    Add base file for non-parametric morphology

---
+++ photutils/morphometry.py
@@ -0,0 +1,6 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+Functions for measuring non-parametric morphology.
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
