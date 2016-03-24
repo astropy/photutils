670a1bdc0d91db0d807c44dcfdb245292f53448c
Leonardo Ferreira <leonardo.ferreira.furg@gmail.com>
2016-03-24 15:19:42 -0700

    fix typos

--- photutils/morphometry.py
+++ photutils/morphometry.py
@@ -8,36 +8,31 @@ import numpy as np
 
 
 def gini(data):
-    """Measures the Gini coefficient on a 2-D array based on perscription
-    from [lotz2004]_.
+    """Measures the Gini coefficient on a 2-D array based on
+    prescription from [Lotz2004]_.
 
     .. math::
+        G = \frac{1}{\left | \bar{f} \right | N (N - 1)}
+        \sum^{N}_{i} (2i - N - 1) \left | f_i \right |
 
-        G = \\frac{1}{\\left | \\bar{f} \\right | N (N - 1)}
-        \\sum^N_i (2i - N - 1) \\left | f_i \\right |
-
-
-    It is a way of measuring the inaquality in a
-    given set of values. In the context of galaxy morphology, it
-    measures how the light of a galaxy image is distributed among its
-    pixels. A Gini value of 0 corresponds to a galaxy image with the
-    light equally distributed over all pixels while a Gini value of 1
-    represents a galaxy image with all its light concentrated in
-    just one pixel.
+    It is a way of measuring the inequality in a given set of
+    values. In the context of galaxy morphology, it measures how
+    the light of a galaxy image is distributed among its pixels.
+    A Gini value of 0 corresponds to a galaxy image with the
+    light equally distributed over all pixels while a Gini value
+    of 1 represents a galaxy image with all its light concentrated
+    in just one pixel.
 
     Usually Gini's measurement needs some sort of pre-processing
     based on the quality of the input data. As there is not a general
     standard for doing this, this is left for the user.
 
-    .. [lotz2004] Lotz et al. 2004,
-    A new nonparametric approach to galaxy morphological classification,
-    http://arxiv.org/abs/astro-ph/0311352
+    .. [Lotz2004] Lotz et al. 2004, A new nonparametric approach to galaxy morphological classification, http://arxiv.org/abs/astro-ph/0311352
 
     Parameters
     ----------
     data : array_like
         The 2-d array with the values for measuring the Gini Coefficient.
-
     Returns
     -------
     gini : `float`
