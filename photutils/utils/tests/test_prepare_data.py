7f83c6b54b755782c635d7b2856000315bc101fb
Larry Bradley <larry.bradley@gmail.com>
2016-06-08 12:28:38 -0400

    Update calc_total_error tests

--- photutils/utils/tests/test_prepare_data.py
+++ photutils/utils/tests/test_prepare_data.py
@@ -11,7 +11,7 @@ DATAVAL = 2.
 DATA = np.ones(SHAPE) * DATAVAL
 MASK = np.zeros_like(DATA, dtype=bool)
 MASK[2, 2] = True
-ERROR = np.ones(SHAPE)
+BKG_ERROR = np.ones(SHAPE)
 EFFGAIN = np.ones(SHAPE) * DATAVAL
 BACKGROUND = np.ones(SHAPE)
 WRONG_SHAPE = np.ones((2, 2))
@@ -20,22 +20,21 @@ WRONG_SHAPE = np.ones((2, 2))
 class TestCalculateTotalError(object):
     def test_error_shape(self):
         with pytest.raises(ValueError):
-            calc_total_error(DATA, error=WRONG_SHAPE, effective_gain=EFFGAIN)
+            calc_total_error(DATA, WRONG_SHAPE, EFFGAIN)
 
     def test_gain_shape(self):
         with pytest.raises(ValueError):
-            calc_total_error(DATA, error=ERROR, effective_gain=WRONG_SHAPE)
+            calc_total_error(DATA, BKG_ERROR, WRONG_SHAPE)
 
     @pytest.mark.parametrize('effective_gain', (0, -1))
     def test_gain_le_zero(self, effective_gain):
         with pytest.raises(ValueError):
-            calc_total_error(DATA, error=ERROR, effective_gain=effective_gain)
+            calc_total_error(DATA, BKG_ERROR, effective_gain)
 
     def test_gain_scalar(self):
-        error_tot = calc_total_error(DATA, error=ERROR, effective_gain=2.)
-        assert_allclose(error_tot, np.sqrt(2.) * ERROR)
+        error_tot = calc_total_error(DATA, BKG_ERROR, 2.)
+        assert_allclose(error_tot, np.sqrt(2.) * BKG_ERROR)
 
     def test_gain_array(self):
-        error_tot = calc_total_error(DATA, error=ERROR,
-                                     effective_gain=EFFGAIN)
-        assert_allclose(error_tot, np.sqrt(2.) * ERROR)
+        error_tot = calc_total_error(DATA, BKG_ERROR, EFFGAIN)
+        assert_allclose(error_tot, np.sqrt(2.) * BKG_ERROR)
