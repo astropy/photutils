0b9703bd2bd6b51adc6bbbc0d57c7616c9829e6c
Mihai Cara <mihail.cara@gmail.com>
2016-09-16 16:22:42 -0400

    fix tests

--- photutils/psf/tests/test_models.py
+++ photutils/psf/tests/test_models.py
@@ -4,7 +4,7 @@ from __future__ import division
 import numpy as np
 from astropy.modeling.models import Gaussian2D
 
-from .. import FittableImageModel2D
+from .. import FittableImageModel
 
 try:
     import scipy
@@ -17,14 +17,14 @@ def test_image_model():
     gm = Gaussian2D(x_stddev=3, y_stddev=3)
     xg, yg = np.mgrid[-2:3, -2:3]
 
-    imod_nonorm = FittableImageModel2D(gm(xg, yg), flux=1)
+    imod_nonorm = FittableImageModel(gm(xg, yg), flux=1)
     assert np.allclose(imod_nonorm(0, 0), gm(0, 0))
 
-    imod_norm = FittableImageModel2D(gm(xg, yg), normalize=True, flux=1)
+    imod_norm = FittableImageModel(gm(xg, yg), normalize=True, flux=1)
     assert not np.allclose(imod_norm(0, 0), gm(0, 0))
     assert np.allclose(np.sum(imod_norm(xg, yg)), 1)
 
-    imod_norm2 = FittableImageModel2D(gm(xg, yg), normalize=True, normalization_correction=2, flux=1)
+    imod_norm2 = FittableImageModel(gm(xg, yg), normalize=True, normalization_correction=2, flux=1)
     assert not np.allclose(imod_norm2(0, 0), gm(0, 0))
     assert np.allclose(imod_norm(0, 0), imod_norm2(0, 0)*2)
     assert np.allclose(np.sum(imod_norm2(xg, yg)), 0.5)
