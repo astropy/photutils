66fd0768fe2a203ebe5460c78753906a5d09eadf
Larry Bradley <larry.bradley@gmail.com>
2016-12-06 21:17:06 -0500

    Merge pull request #476 from larrybradley/circleci
    Add 32-bit CircleCI configuration

---
+++ .run_docker_tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+docker info
+
+cat << EOF | docker run -i \
+                        -v ${PWD}:/photutils_src \
+                        -a stdin -a stdout -a stderr \
+                        astropy/affiliated-32bit-test-env:1.6 \
+                        bash || exit $?
+
+cd /photutils_src
+
+echo "Output of uname -m:"
+uname -m
+
+echo "Output of sys.maxsize in Python:"
+python -c 'import sys; print(sys.maxsize)'
+
+python setup.py test -V -a "-s"
+
+EOF
