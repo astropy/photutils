91cd11333c53bdf41aca5d022d1604427a138a32
Larry Bradley <larry.bradley@gmail.com>
2017-09-27 15:00:42 -0400

    Remove tests for deprecated functions

--- photutils/detection/tests/test_findstars.py
+++ photutils/detection/tests/test_findstars.py
@@ -14,7 +14,6 @@ from astropy.table import Table
 from astropy.utils.exceptions import AstropyUserWarning
 from astropy.utils.exceptions import AstropyDeprecationWarning
 
-from ..findstars import daofind, irafstarfind
 from ..findstars import DAOStarFinder, IRAFStarFinder
 from ...datasets import make_100gaussians_image
 
@@ -52,30 +51,10 @@ class TestDAOStarFinder(object):
         assert_allclose(np.array(t).astype(np.float),
                         np.array(t_ref).astype(np.float))
 
-    # test for the deprecated daofind
-    @pytest.mark.parametrize(('threshold', 'fwhm'),
-                             list(itertools.product(THRESHOLDS, FWHMS)))
-    def test_dep_daofind(self, threshold, fwhm):
-        with catch_warnings(AstropyDeprecationWarning):
-            t = daofind(DATA, threshold, fwhm, sigma_radius=1.5)
-        datafn = ('daofind_test_thresh{0:04.1f}_fwhm{1:04.1f}'
-                  '.txt'.format(threshold, fwhm))
-        datafn = op.join(op.dirname(op.abspath(__file__)), 'data', datafn)
-        t_ref = Table.read(datafn, format='ascii')
-        assert_allclose(np.array(t).astype(np.float),
-                        np.array(t_ref).astype(np.float))
-
     def test_daofind_include_border(self):
         starfinder = DAOStarFinder(threshold=10, fwhm=2, sigma_radius=1.5,
                                    exclude_border=False)
         t = starfinder(DATA)
-        assert len(t) == 20
-
-    # test for the deprecated daofind
-    def test_dep_daofind_include_border(self):
-        with catch_warnings(AstropyDeprecationWarning):
-            t = daofind(DATA, threshold=10, fwhm=2, sigma_radius=1.5,
-                        exclude_border=False)
         assert len(t) == 20
 
     def test_daofind_exclude_border(self):
@@ -84,24 +63,10 @@ class TestDAOStarFinder(object):
         t = starfinder(DATA)
         assert len(t) == 19
 
-    # test for the deprecated daofind
-    def test_dep_daofind_exclude_border(self):
-        with catch_warnings(AstropyDeprecationWarning):
-            t = daofind(DATA, threshold=10, fwhm=2, sigma_radius=1.5,
-                        exclude_border=True)
-        assert len(t) == 19
-
     def test_daofind_nosources(self):
         data = np.ones((3, 3))
         starfinder = DAOStarFinder(threshold=10, fwhm=1)
         t = starfinder(data)
-        assert len(t) == 0
-
-    # test for the deprecated daofind
-    def test_dep_daofind_nosources(self):
-        data = np.ones((3, 3))
-        with catch_warnings(AstropyDeprecationWarning):
-            t = daofind(data, threshold=10, fwhm=1)
         assert len(t) == 0
 
     def test_daofind_sharpness(self):
@@ -110,24 +75,10 @@ class TestDAOStarFinder(object):
         t = starfinder(DATA)
         assert len(t) == 0
 
-    # test for the deprecated daofind
-    def test_dep_daofind_sharpness(self):
-        """Sources found, but none pass the sharpness criteria."""
-        with catch_warnings(AstropyDeprecationWarning):
-            t = daofind(DATA, threshold=50, fwhm=1.0, sharplo=1.)
-        assert len(t) == 0
-
     def test_daofind_roundness(self):
         """Sources found, but none pass the roundness criteria."""
         starfinder = DAOStarFinder(threshold=50, fwhm=1.0, roundlo=1.)
         t = starfinder(DATA)
-        assert len(t) == 0
-
-    # test for the deprecated daofind
-    def test_dep_daofind_roundness(self):
-        """Sources found, but none pass the roundness criteria."""
-        with catch_warnings(AstropyDeprecationWarning):
-            t = daofind(DATA, threshold=50, fwhm=1.0, roundlo=1.)
         assert len(t) == 0
 
     def test_daofind_flux_negative(self):
@@ -136,15 +87,6 @@ class TestDAOStarFinder(object):
         data[2, 2] = 10.
         starfinder = DAOStarFinder(threshold=0.1, fwhm=1.0, sky=10)
         t = starfinder(data)
-        assert not np.isfinite(t['mag'])
-
-    # test for the deprecated daofind
-    def test_dep_daofind_flux_negative(self):
-        """Test handling of negative flux (here created by large sky)."""
-        data = np.ones((5, 5))
-        data[2, 2] = 10.
-        with catch_warnings(AstropyDeprecationWarning):
-            t = daofind(data, threshold=0.1, fwhm=1.0, sky=10)
         assert not np.isfinite(t['mag'])
 
 
@@ -163,30 +105,10 @@ class TestIRAFStarFinder(object):
         assert_allclose(np.array(t).astype(np.float),
                         np.array(t_ref).astype(np.float))
 
-    # test for the deprecated irafstarfind
-    @pytest.mark.parametrize(('threshold', 'fwhm'),
-                             list(itertools.product(THRESHOLDS, FWHMS)))
-    def test_dep_irafstarfind(self, threshold, fwhm):
-        with catch_warnings(AstropyDeprecationWarning):
-            t = irafstarfind(DATA, threshold, fwhm, sigma_radius=1.5)
-        datafn = ('irafstarfind_test_thresh{0:04.1f}_fwhm{1:04.1f}'
-                  '.txt'.format(threshold, fwhm))
-        datafn = op.join(op.dirname(op.abspath(__file__)), 'data', datafn)
-        t_ref = Table.read(datafn, format='ascii')
-        assert_allclose(np.array(t).astype(np.float),
-                        np.array(t_ref).astype(np.float))
-
     def test_irafstarfind_nosources(self):
         data = np.ones((3, 3))
         starfinder = IRAFStarFinder(threshold=10, fwhm=1)
         t = starfinder(data)
-        assert len(t) == 0
-
-    # test for the deprecated irafstarfind
-    def test_dep_irafstarfind_nosources(self):
-        data = np.ones((3, 3))
-        with catch_warnings(AstropyDeprecationWarning):
-            t = irafstarfind(data, threshold=10, fwhm=1)
         assert len(t) == 0
 
     def test_irafstarfind_sharpness(self):
@@ -195,24 +117,10 @@ class TestIRAFStarFinder(object):
         t = starfinder(DATA)
         assert len(t) == 0
 
-    # test for the deprecated irafstarfind
-    def test_dep_irafstarfind_sharpness(self):
-        """Sources found, but none pass the sharpness criteria."""
-        with catch_warnings(AstropyDeprecationWarning):
-            t = irafstarfind(DATA, threshold=50, fwhm=1.0, sharplo=2.)
-        assert len(t) == 0
-
     def test_irafstarfind_roundness(self):
         """Sources found, but none pass the roundness criteria."""
         starfinder = IRAFStarFinder(threshold=50, fwhm=1.0, roundlo=1.)
         t = starfinder(DATA)
-        assert len(t) == 0
-
-    # test for the deprecated irafstarfind
-    def test_dep_irafstarfind_roundness(self):
-        """Sources found, but none pass the roundness criteria."""
-        with catch_warnings(AstropyDeprecationWarning):
-            t = irafstarfind(DATA, threshold=50, fwhm=1.0, roundlo=1.)
         assert len(t) == 0
 
     def test_irafstarfind_sky(self):
@@ -220,19 +128,7 @@ class TestIRAFStarFinder(object):
         t = starfinder(DATA)
         assert len(t) == 4
 
-    # test for the deprecated irafstarfind
-    def test_dep_irafstarfind_sky(self):
-        with catch_warnings(AstropyDeprecationWarning):
-            t = irafstarfind(DATA, threshold=25.0, fwhm=2.0, sky=10.)
-        assert len(t) == 4
-
     def test_irafstarfind_largesky(self):
         starfinder = IRAFStarFinder(threshold=25.0, fwhm=2.0, sky=100.)
         t = starfinder(DATA)
-        assert len(t) == 0
-
-    # test for the deprecated irafstarfind
-    def test_dep_irafstarfind_largesky(self):
-        with catch_warnings(AstropyDeprecationWarning):
-            t = irafstarfind(DATA, threshold=25.0, fwhm=2.0, sky=100.)
         assert len(t) == 0
