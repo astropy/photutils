25ae3e88a96cecd786a06b25c497e71d3f802d02
Larry Bradley <larry.bradley@gmail.com>
2017-10-02 15:35:37 -0400

    Refactor some isophote tests

--- photutils/isophote/tests/test_harmonics.py
+++ photutils/isophote/tests/test_harmonics.py
@@ -19,10 +19,8 @@ except ImportError:
 
 @pytest.mark.skipif('not HAS_SCIPY')
 def test_harmonics_1():
-
     # this is an almost as-is example taken from stackoverflow
-
-    N = 100 # number of data points
+    N = 100  # number of data points
     t = np.linspace(0, 4*np.pi, N)
 
     # create artificial data with noise:
@@ -31,16 +29,20 @@ def test_harmonics_1():
 
     # first guesses for harmonic parameters
     guess_mean = np.mean(data)
-    guess_std = 3 * np.std(data)/(2**0.5)
+    guess_std = 3 * np.std(data) / 2**0.5
     guess_phase = 0
 
-    # Minimize the difference between the actual data and our "guessed" parameters
-    optimize_func = lambda x: x[0] * np.sin(t+x[1]) + x[2] - data
+    # Minimize the difference between the actual data and our "guessed"
+    # parameters
+    # optimize_func = lambda x: x[0] * np.sin(t + x[1]) + x[2] - data
+    def optimize_func(x):
+        return x[0] * np.sin(t + x[1]) + x[2] - data
 
-    est_std, est_phase, est_mean = leastsq(optimize_func, [guess_std, guess_phase, guess_mean])[0]
+    est_std, est_phase, est_mean = leastsq(
+        optimize_func, [guess_std, guess_phase, guess_mean])[0]
 
     # recreate the fitted curve using the optimized parameters
-    data_fit = est_std * np.sin(t+est_phase) + est_mean
+    data_fit = est_std * np.sin(t + est_phase) + est_mean
     residual = data - data_fit
 
     assert np.mean(residual) == pytest.approx(0.00, abs=0.001)
@@ -49,9 +51,7 @@ def test_harmonics_1():
 
 @pytest.mark.skipif('not HAS_SCIPY')
 def test_harmonics_2():
-
     # this uses the actual functional form used for fitting ellipses
-
     N = 100
     E = np.linspace(0, 4*np.pi, N)
 
@@ -60,11 +60,13 @@ def test_harmonics_2():
     b1_0 = 5.
     a2_0 = 8.
     b2_0 = 2.
-    data = y0_0 + a1_0 * np.sin(E) + b1_0 * np.cos(E) + a2_0 * np.sin(2*E) + b2_0 * np.cos(2*E) + 0.01 * np.random.randn(N)
+    data = (y0_0 + a1_0*np.sin(E) + b1_0*np.cos(E) + a2_0*np.sin(2*E) +
+            b2_0*np.cos(2*E) + 0.01*np.random.randn(N))
 
     harmonics = fit_1st_and_2nd_harmonics(E, data)
     y0, a1, b1, a2, b2 = harmonics[0]
-    data_fit = y0 + a1*np.sin(E) + b1*np.cos(E) + a2*np.sin(2*E) + b2* np.cos(2*E) + 0.01 * np.random.randn(N)
+    data_fit = (y0 + a1*np.sin(E) + b1*np.cos(E) + a2*np.sin(2*E) +
+                b2*np.cos(2*E) + 0.01*np.random.randn(N))
     residual = data - data_fit
 
     assert np.mean(residual) == pytest.approx(0.00, abs=0.01)
@@ -73,21 +75,21 @@ def test_harmonics_2():
 
 @pytest.mark.skipif('not HAS_SCIPY')
 def test_harmonics_3():
-
-    # tests an upper harmonic fit
+    """Tests an upper harmonic fit."""
 
     N = 100
     E = np.linspace(0, 4*np.pi, N)
-
     y0_0 = 100.
     a1_0 = 10.
     b1_0 = 5.
     order = 3
-    data = y0_0 + a1_0 * np.sin(order*E) + b1_0 * np.cos(order*E) + 0.01 * np.random.randn(N)
+    data = (y0_0 + a1_0*np.sin(order*E) + b1_0*np.cos(order*E) +
+            0.01*np.random.randn(N))
 
     harmonic = fit_upper_harmonic(E, data, order)
     y0, a1, b1 = harmonic[0]
-    data_fit = y0 + a1*np.sin(order*E) + b1*np.cos(order*E) + 0.01 * np.random.randn(N)
+    data_fit = (y0 + a1*np.sin(order*E) + b1*np.cos(order*E) +
+                0.01*np.random.randn(N))
     residual = data - data_fit
 
     assert np.mean(residual) == pytest.approx(0.00, abs=0.01)
@@ -95,90 +97,72 @@ def test_harmonics_3():
 
 
 @pytest.mark.skipif('not HAS_SCIPY')
-def test_fit_sample_1():
+class TestFitSamples(object):
+    def setup_class(self):
+        # major axis parallel to X image axis
+        self.data1 = make_test_image(random_state=123)
 
-    # major axis parallel to X image axis
-    data = make_test_image(random_state=123)
+        # major axis tilted 45 deg wrt X image axis
+        self.data2 = make_test_image(pa=np.pi/4, random_state=123)
 
-    sample = Sample(data, 40.)
-    s = sample.extract()
+    def test_fit_sample_1(self):
+        sample = Sample(self.data1, 40.)
+        s = sample.extract()
 
-    harmonics = fit_1st_and_2nd_harmonics(s[0], s[2])
-    y0, a1, b1, a2, b2 = harmonics[0]
+        harmonics = fit_1st_and_2nd_harmonics(s[0], s[2])
+        y0, a1, b1, a2, b2 = harmonics[0]
 
-    assert np.mean(y0) == pytest.approx(200.019,   abs=0.001)
-    assert np.mean(a1) == pytest.approx(-0.000138, abs=0.001)
-    assert np.mean(b1) == pytest.approx(0.000254,  abs=0.001)
-    assert np.mean(a2) == pytest.approx(-5.658e-05,abs=0.001)
-    assert np.mean(b2) == pytest.approx(-0.00911,  abs=0.001)
+        assert np.mean(y0) == pytest.approx(200.019, abs=0.001)
+        assert np.mean(a1) == pytest.approx(-0.000138, abs=0.001)
+        assert np.mean(b1) == pytest.approx(0.000254, abs=0.001)
+        assert np.mean(a2) == pytest.approx(-5.658e-05, abs=0.001)
+        assert np.mean(b2) == pytest.approx(-0.00911, abs=0.001)
 
-    # self.assertAlmostEqual(y0, 200.019, 3)
-    # self.assertAlmostEqual(a1, -0.000138, 3)
-    # self.assertAlmostEqual(b1, 0.000254, 3)
-    # self.assertAlmostEqual(a2, -5.658e-05, 3)
-    # self.assertAlmostEqual(b2, -0.00911, 3)
+        # check that harmonics subtract nicely
+        model = first_and_2nd_harmonic_function(
+            s[0], np.array([y0, a1, b1, a2, b2]))
+        residual = s[2] - model
 
-    # check that harmonics subtract nicely
-    model = first_and_2nd_harmonic_function(s[0], np.array([y0, a1, b1, a2, b2]))
-    residual = s[2] - model
+        assert np.mean(residual) == pytest.approx(0.00, abs=0.001)
+        assert np.std(residual) == pytest.approx(0.015, abs=0.01)
 
-    assert np.mean(residual) == pytest.approx(0.00, abs=0.001)
-    assert np.std(residual) == pytest.approx(0.015, abs=0.01)
+    def test_fit_sample_2(self):
+        # initial guess is rounder than actual image
+        sample = Sample(self.data1, 40., eps=0.1)
+        s = sample.extract()
 
+        harmonics = fit_1st_and_2nd_harmonics(s[0], s[2])
+        y0, a1, b1, a2, b2 = harmonics[0]
 
-@pytest.mark.skipif('not HAS_SCIPY')
-def test_fit_sample_2():
+        assert np.mean(y0) == pytest.approx(188.686, abs=0.001)
+        assert np.mean(a1) == pytest.approx(0.000283, abs=0.001)
+        assert np.mean(b1) == pytest.approx(0.00692, abs=0.001)
+        assert np.mean(a2) == pytest.approx(-0.000215, abs=0.001)
+        assert np.mean(b2) == pytest.approx(10.153, abs=0.001)
 
-    # major axis tilted 45 deg wrt X image axis
-    data = make_test_image(pa=np.pi/4, random_state=123)
+    def test_fit_sample_3(self):
+        # initial guess for center is offset
+        sample = Sample(self.data1, x0=220., y0=210., sma=40.)
+        s = sample.extract()
 
-    sample = Sample(data, 40., eps=0.4)
-    s = sample.extract()
+        harmonics = fit_1st_and_2nd_harmonics(s[0], s[2])
+        y0, a1, b1, a2, b2 = harmonics[0]
 
-    harmonics = fit_1st_and_2nd_harmonics(s[0], s[2])
-    y0, a1, b1, a2, b2 = harmonics[0]
+        assert np.mean(y0) == pytest.approx(152.660, abs=0.001)
+        assert np.mean(a1) == pytest.approx(55.338, abs=0.001)
+        assert np.mean(b1) == pytest.approx(33.091, abs=0.001)
+        assert np.mean(a2) == pytest.approx(33.036, abs=0.001)
+        assert np.mean(b2) == pytest.approx(-14.306, abs=0.001)
 
-    assert np.mean(y0) == pytest.approx(245.102,  abs=0.001)
-    assert np.mean(a1) == pytest.approx(-0.003108,abs=0.001)
-    assert np.mean(b1) == pytest.approx(-0.0578,  abs=0.001)
-    assert np.mean(a2) == pytest.approx(28.781,   abs=0.001)
-    assert np.mean(b2) == pytest.approx(-63.184,  abs=0.001)
+    def test_fit_sample_4(self):
+        sample = Sample(self.data2, 40., eps=0.4)
+        s = sample.extract()
 
+        harmonics = fit_1st_and_2nd_harmonics(s[0], s[2])
+        y0, a1, b1, a2, b2 = harmonics[0]
 
-@pytest.mark.skipif('not HAS_SCIPY')
-def test_fit_sample_3():
-
-    data = make_test_image(random_state=123)
-
-    # initial guess is rounder than actual image
-    sample = Sample(data, 40., eps=0.1)
-    s = sample.extract()
-
-    harmonics = fit_1st_and_2nd_harmonics(s[0], s[2])
-    y0, a1, b1, a2, b2 = harmonics[0]
-
-    assert np.mean(y0) == pytest.approx(188.686,  abs=0.001)
-    assert np.mean(a1) == pytest.approx(0.000283, abs=0.001)
-    assert np.mean(b1) == pytest.approx(0.00692,  abs=0.001)
-    assert np.mean(a2) == pytest.approx(-0.000215,abs=0.001)
-    assert np.mean(b2) == pytest.approx(10.153,   abs=0.001)
-
-
-@pytest.mark.skipif('not HAS_SCIPY')
-def test_fit_sample_4():
-
-    data = make_test_image(random_state=123)
-
-    # initial guess for center is offset
-    sample = Sample(data, x0=220., y0=210., sma=40.)
-    s = sample.extract()
-
-    harmonics = fit_1st_and_2nd_harmonics(s[0], s[2])
-    y0, a1, b1, a2, b2 = harmonics[0]
-
-    assert np.mean(y0) == pytest.approx(152.660,abs=0.001)
-    assert np.mean(a1) == pytest.approx(55.338, abs=0.001)
-    assert np.mean(b1) == pytest.approx(33.091, abs=0.001)
-    assert np.mean(a2) == pytest.approx(33.036, abs=0.001)
-    assert np.mean(b2) == pytest.approx(-14.306,abs=0.001)
-
+        assert np.mean(y0) == pytest.approx(245.102, abs=0.001)
+        assert np.mean(a1) == pytest.approx(-0.003108, abs=0.001)
+        assert np.mean(b1) == pytest.approx(-0.0578, abs=0.001)
+        assert np.mean(a2) == pytest.approx(28.781, abs=0.001)
+        assert np.mean(b2) == pytest.approx(-63.184, abs=0.001)
--- photutils/isophote/tests/test_integrator.py
+++ photutils/isophote/tests/test_integrator.py
@@ -24,14 +24,12 @@ class TestData(object):
         self.data = hdu[0].data
         hdu.close()
 
-
-@remote_data
-class TestUnmasked(TestData):
-
-    def _init_test(self, sma=40., integrmode=BI_LINEAR):
-
-        sample = Sample(self.data, sma, integrmode=integrmode)
-
+    def make_sample(self, masked=False, sma=40., integrmode=BI_LINEAR):
+        if masked:
+            data = ma.masked_values(self.data, 200., atol=10.0, rtol=0.)
+        else:
+            data = self.data
+        sample = Sample(data, sma, integrmode=integrmode)
         s = sample.extract()
 
         assert len(s) == 3
@@ -40,145 +38,126 @@ class TestUnmasked(TestData):
 
         return s, sample
 
-    def test_bilinear(self):
 
-        s, sample = self._init_test()
+@remote_data
+class TestUnmasked(TestData):
+
+    def test_bilinear(self):
+        s, sample = self.make_sample()
 
         assert len(s[0]) == 225
         # intensities
         assert np.mean(s[2]) == pytest.approx(200.76, abs=0.01)
-        assert np.std(s[2])  == pytest.approx(21.55,  abs=0.01)
-        # radii
-        assert np.max(s[1]) == pytest.approx(40.0, abs=0.01)
-        assert np.min(s[1]) == pytest.approx(32.0,  abs=0.01)
-
-        assert sample.total_points  == 225
-        assert sample.actual_points == 225
-
-    def test_bilinear_small(self):
-
-        # small radius forces sub-pixel sampling
-        s, sample = self._init_test(sma=10.)
-
-        # intensities
-        assert np.mean(s[2]) == pytest.approx(1045.4, abs=0.1)
-        assert np.std(s[2])  == pytest.approx(143.0,  abs=0.1)
-        # radii
-        assert np.max(s[1]) == pytest.approx(10.0, abs=0.1)
-        assert np.min(s[1]) == pytest.approx(8.0,  abs=0.1)
-
-        assert sample.total_points  == 57
-        assert sample.actual_points == 57
-
-    def test_nearest_neighbor(self):
-
-        s, sample = self._init_test(integrmode=NEAREST_NEIGHBOR)
-
-        assert len(s[0]) == 225
-        # intensities
-        assert np.mean(s[2]) == pytest.approx(201.1, abs=0.1)
-        assert np.std(s[2])  == pytest.approx(21.8,  abs=0.1)
+        assert np.std(s[2]) == pytest.approx(21.55, abs=0.01)
         # radii
         assert np.max(s[1]) == pytest.approx(40.0, abs=0.01)
         assert np.min(s[1]) == pytest.approx(32.0, abs=0.01)
 
-        assert sample.total_points  == 225
+        assert sample.total_points == 225
+        assert sample.actual_points == 225
+
+    def test_bilinear_small(self):
+        # small radius forces sub-pixel sampling
+        s, sample = self.make_sample(sma=10.)
+
+        # intensities
+        assert np.mean(s[2]) == pytest.approx(1045.4, abs=0.1)
+        assert np.std(s[2]) == pytest.approx(143.0, abs=0.1)
+        # radii
+        assert np.max(s[1]) == pytest.approx(10.0, abs=0.1)
+        assert np.min(s[1]) == pytest.approx(8.0, abs=0.1)
+
+        assert sample.total_points == 57
+        assert sample.actual_points == 57
+
+    def test_nearest_neighbor(self):
+        s, sample = self.make_sample(integrmode=NEAREST_NEIGHBOR)
+
+        assert len(s[0]) == 225
+        # intensities
+        assert np.mean(s[2]) == pytest.approx(201.1, abs=0.1)
+        assert np.std(s[2]) == pytest.approx(21.8, abs=0.1)
+        # radii
+        assert np.max(s[1]) == pytest.approx(40.0, abs=0.01)
+        assert np.min(s[1]) == pytest.approx(32.0, abs=0.01)
+
+        assert sample.total_points == 225
         assert sample.actual_points == 225
 
     def test_mean(self):
-
-        s, sample = self._init_test(integrmode=MEAN)
+        s, sample = self.make_sample(integrmode=MEAN)
 
         assert len(s[0]) == 64
         # intensities
         assert np.mean(s[2]) == pytest.approx(199.9, abs=0.1)
-        assert np.std(s[2])  == pytest.approx(21.3,  abs=0.1)
+        assert np.std(s[2]) == pytest.approx(21.3, abs=0.1)
         # radii
         assert np.max(s[1]) == pytest.approx(40.0, abs=0.01)
         assert np.min(s[1]) == pytest.approx(32.0, abs=0.01)
 
         assert sample.sector_area == pytest.approx(12.4, abs=0.1)
-        assert sample.total_points  == 64
+        assert sample.total_points == 64
         assert sample.actual_points == 64
 
     def test_mean_small(self):
-
-        s, sample = self._init_test(sma=5., integrmode=MEAN)
+        s, sample = self.make_sample(sma=5., integrmode=MEAN)
 
         assert len(s[0]) == 29
         # intensities
         assert np.mean(s[2]) == pytest.approx(2339.0, abs=0.1)
-        assert np.std(s[2])  == pytest.approx(284.7,  abs=0.1)
+        assert np.std(s[2]) == pytest.approx(284.7, abs=0.1)
         # radii
         assert np.max(s[1]) == pytest.approx(5.0, abs=0.01)
         assert np.min(s[1]) == pytest.approx(4.0, abs=0.01)
 
         assert sample.sector_area == pytest.approx(2.0, abs=0.1)
-        assert sample.total_points  == 29
+        assert sample.total_points == 29
         assert sample.actual_points == 29
 
     def test_median(self):
-
-        s, sample = self._init_test(integrmode=MEDIAN)
+        s, sample = self.make_sample(integrmode=MEDIAN)
 
         assert len(s[0]) == 64
         # intensities
         assert np.mean(s[2]) == pytest.approx(199.9, abs=0.1)
-        assert np.std(s[2])  == pytest.approx(21.3,  abs=0.1)
+        assert np.std(s[2]) == pytest.approx(21.3, abs=0.1)
         # radii
-        assert np.max(s[1]) == pytest.approx(40.0,  abs=0.01)
+        assert np.max(s[1]) == pytest.approx(40.0, abs=0.01)
         assert np.min(s[1]) == pytest.approx(32.01, abs=0.01)
 
         assert sample.sector_area == pytest.approx(12.4, abs=0.1)
-        assert sample.total_points  == 64
+        assert sample.total_points == 64
         assert sample.actual_points == 64
 
 
 @remote_data
 class TestMasked(TestData):
 
-    def _init_test(self, sma=40., integrmode=BI_LINEAR):
-
-        data = ma.masked_values(self.data, 200., atol=10.0, rtol=0.)
-
-        sample = Sample(data, sma, integrmode=integrmode)
-
-        s = sample.extract()
-
-        assert len(s) == 3
-        assert len(s[0]) == len(s[1])
-        assert len(s[0]) == len(s[2])
-
-        return s, sample
-
     def test_bilinear(self):
-
-        s, sample = self._init_test()
+        s, sample = self.make_sample(masked=True, integrmode=BI_LINEAR)
 
         assert len(s[0]) == 157
         # intensities
         assert np.mean(s[2]) == pytest.approx(201.52, abs=0.01)
-        assert np.std(s[2])  == pytest.approx(25.21,  abs=0.01)
+        assert np.std(s[2]) == pytest.approx(25.21, abs=0.01)
         # radii
         assert np.max(s[1]) == pytest.approx(40.0, abs=0.01)
-        assert np.min(s[1]) == pytest.approx(32.0,  abs=0.01)
+        assert np.min(s[1]) == pytest.approx(32.0, abs=0.01)
 
-        assert sample.total_points  == 225
+        assert sample.total_points == 225
         assert sample.actual_points == 157
 
     def test_mean(self):
-
-        s, sample = self._init_test(integrmode=MEAN)
+        s, sample = self.make_sample(masked=True, integrmode=MEAN)
 
         assert len(s[0]) == 51
         # intensities
         assert np.mean(s[2]) == pytest.approx(199.9, abs=0.1)
-        assert np.std(s[2])  == pytest.approx(24.12,  abs=0.1)
+        assert np.std(s[2]) == pytest.approx(24.12, abs=0.1)
         # radii
         assert np.max(s[1]) == pytest.approx(40.0, abs=0.01)
         assert np.min(s[1]) == pytest.approx(32.0, abs=0.01)
 
         assert sample.sector_area == pytest.approx(12.4, abs=0.1)
-        assert sample.total_points  == 64
+        assert sample.total_points == 64
         assert sample.actual_points == 51
-
