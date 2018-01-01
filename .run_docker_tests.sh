833260ac384ae6f7c26d60f18ad40a3077747ed9
Brigitta Sipocz <b.sipocz@gmail.com>
2018-01-01 15:56:54 +0000

    Merge pull request #635 from mirca/patch-1
    Update .run_docker_tests.sh

--- .run_docker_tests.sh
+++ .run_docker_tests.sh
@@ -16,10 +16,6 @@ uname -m
 echo "Output of sys.maxsize in Python:"
 python -c 'import sys; print(sys.maxsize)'
 
-# temporary: remove when https://github.com/astropy/astropy/issues/6418
-# is resolved
-easy_install "pytest<3.2"
-
 pip install six
 
 python setup.py test -V -a "-s"
