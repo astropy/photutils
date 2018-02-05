4b87407ef7e8de41926f2bb0359c0f25cce985bb
Larry Bradley <larry.bradley@gmail.com>
2018-02-05 17:33:12 -0500

    Add unit tests for aperture mask

--- photutils/aperture/tests/test_mask.py
+++ photutils/aperture/tests/test_mask.py
@@ -3,9 +3,12 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import numpy as np
+from numpy.testing import assert_allclose
 import pytest
 
+from ..bounding_box import BoundingBox
 from ..circle import CircularAperture
+from ..mask import ApertureMask
 
 try:
     import matplotlib    # noqa
@@ -14,7 +17,46 @@ except ImportError:
     HAS_MATPLOTLIB = False
 
 
-POSITIONS = [(-20, -20), (-20, 20), (20, -20)]
+POSITIONS = [(-20, -20), (-20, 20), (20, -20), (60, 60)]
+
+
+def test_mask_input_shapes():
+    with pytest.raises(ValueError):
+        mask_data = np.ones((10, 10))
+        bbox = BoundingBox(5, 10, 5, 10)
+        ApertureMask(mask_data, bbox)
+
+
+def test_mask_array():
+    mask_data = np.ones((10, 10))
+    bbox = BoundingBox(5, 15, 5, 15)
+    mask = ApertureMask(mask_data, bbox)
+    data = np.array(mask)
+    assert_allclose(data, mask.data)
+
+
+def test_mask_cutout_shape():
+    mask_data = np.ones((10, 10))
+    bbox = BoundingBox(5, 15, 5, 15)
+    mask = ApertureMask(mask_data, bbox)
+
+    with pytest.raises(ValueError):
+        mask.cutout(np.arange(10))
+
+    with pytest.raises(ValueError):
+        mask._overlap_slices((10,))
+
+    with pytest.raises(ValueError):
+        mask.to_image((10,))
+
+
+def test_mask_cutout_copy():
+    data = np.ones((50, 50))
+    aper = CircularAperture((25, 25), r=10.)
+    mask = aper.to_mask()[0]
+    cutout = mask.cutout(data, copy=True)
+    data[25, 25] = 100.
+    assert cutout[10, 10] == 1.
 
 
 @pytest.mark.parametrize('position', POSITIONS)
