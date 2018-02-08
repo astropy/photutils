f6842fced018ab4182edc5522c55bab89d890df1
Larry Bradley <larry.bradley@gmail.com>
2018-02-08 14:37:37 -0500

    Add psf building imports and test directory

--- photutils/psf/building/__init__.py
+++ photutils/psf/building/__init__.py
@@ -3,3 +3,9 @@
 This subpackage contains modules and packages to build point spread
 functions.
 """
+
+from .catalogs import *
+from .centroid import *
+from .models import *
+from .psf import *
+from .utils import *
