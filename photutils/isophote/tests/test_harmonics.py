e96ee6d993f9c561fe1e1f71d78ec9a2b372c24c
Larry Bradley <larry.bradley@gmail.com>
2018-04-02 11:44:02 -0400

    Use approx_allclose in test_harmonics

--- photutils/isophote/tests/test_harmonics.py
+++ photutils/isophote/tests/test_harmonics.py
@@ -3,6 +3,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import numpy as np
+from numpy.testing import assert_allclose
 import pytest
 
 from .make_test_data import make_test_image
@@ -45,8 +46,8 @@ def test_harmonics_1():
     data_fit = est_std * np.sin(t + est_phase) + est_mean
     residual = data - data_fit
 
-    assert np.mean(residual) == pytest.approx(0.00, abs=0.001)
-    assert np.std(residual) == pytest.approx(0.01, abs=0.01)
+    assert_allclose(np.mean(residual), 0., atol=0.001)
+    assert_allclose(np.std(residual), 0.01, atol=0.01)
 
 
 @pytest.mark.skipif('not HAS_SCIPY')
@@ -69,8 +70,8 @@ def test_harmonics_2():
                 b2*np.cos(2*E) + 0.01*np.random.randn(N))
     residual = data - data_fit
 
-    assert np.mean(residual) == pytest.approx(0.00, abs=0.01)
-    assert np.std(residual) == pytest.approx(0.015, abs=0.01)
+    assert_allclose(np.mean(residual), 0., atol=0.01)
+    assert_allclose(np.std(residual), 0.015, atol=0.01)
 
 
 @pytest.mark.skipif('not HAS_SCIPY')
@@ -92,8 +93,8 @@ def test_harmonics_3():
                 0.01*np.random.randn(N))
     residual = data - data_fit
 
-    assert np.mean(residual) == pytest.approx(0.00, abs=0.01)
-    assert np.std(residual) == pytest.approx(0.015, abs=0.01)
+    assert_allclose(np.mean(residual), 0., atol=0.01)
+    assert_allclose(np.std(residual), 0.015, atol=0.01)
 
 
 @pytest.mark.skipif('not HAS_SCIPY')
@@ -112,19 +113,19 @@ class TestFitEllipseSamples(object):
         harmonics = fit_first_and_second_harmonics(s[0], s[2])
         y0, a1, b1, a2, b2 = harmonics[0]
 
-        assert np.mean(y0) == pytest.approx(200.019, abs=0.001)
-        assert np.mean(a1) == pytest.approx(-0.000138, abs=0.001)
-        assert np.mean(b1) == pytest.approx(0.000254, abs=0.001)
-        assert np.mean(a2) == pytest.approx(-5.658e-05, abs=0.001)
-        assert np.mean(b2) == pytest.approx(-0.00911, abs=0.001)
+        assert_allclose(np.mean(y0), 200.019, atol=0.001)
+        assert_allclose(np.mean(a1), -0.000138, atol=0.001)
+        assert_allclose(np.mean(b1), 0.000254, atol=0.001)
+        assert_allclose(np.mean(a2), -5.658e-05, atol=0.001)
+        assert_allclose(np.mean(b2), -0.00911, atol=0.001)
 
         # check that harmonics subtract nicely
         model = first_and_second_harmonic_function(
             s[0], np.array([y0, a1, b1, a2, b2]))
         residual = s[2] - model
 
-        assert np.mean(residual) == pytest.approx(0.00, abs=0.001)
-        assert np.std(residual) == pytest.approx(0.015, abs=0.01)
+        assert_allclose(np.mean(residual), 0., atol=0.001)
+        assert_allclose(np.std(residual), 0.015, atol=0.01)
 
     def test_fit_ellipsesample_2(self):
         # initial guess is rounder than actual image
@@ -134,11 +135,11 @@ class TestFitEllipseSamples(object):
         harmonics = fit_first_and_second_harmonics(s[0], s[2])
         y0, a1, b1, a2, b2 = harmonics[0]
 
-        assert np.mean(y0) == pytest.approx(188.686, abs=0.001)
-        assert np.mean(a1) == pytest.approx(0.000283, abs=0.001)
-        assert np.mean(b1) == pytest.approx(0.00692, abs=0.001)
-        assert np.mean(a2) == pytest.approx(-0.000215, abs=0.001)
-        assert np.mean(b2) == pytest.approx(10.153, abs=0.001)
+        assert_allclose(np.mean(y0), 188.686, atol=0.001)
+        assert_allclose(np.mean(a1), 0.000283, atol=0.001)
+        assert_allclose(np.mean(b1), 0.00692, atol=0.001)
+        assert_allclose(np.mean(a2), -0.000215, atol=0.001)
+        assert_allclose(np.mean(b2), 10.153, atol=0.001)
 
     def test_fit_ellipsesample_3(self):
         # initial guess for center is offset
@@ -148,11 +149,11 @@ class TestFitEllipseSamples(object):
         harmonics = fit_first_and_second_harmonics(s[0], s[2])
         y0, a1, b1, a2, b2 = harmonics[0]
 
-        assert np.mean(y0) == pytest.approx(152.660, abs=0.001)
-        assert np.mean(a1) == pytest.approx(55.338, abs=0.001)
-        assert np.mean(b1) == pytest.approx(33.091, abs=0.001)
-        assert np.mean(a2) == pytest.approx(33.036, abs=0.001)
-        assert np.mean(b2) == pytest.approx(-14.306, abs=0.001)
+        assert_allclose(np.mean(y0), 152.660, atol=0.001)
+        assert_allclose(np.mean(a1), 55.338, atol=0.001)
+        assert_allclose(np.mean(b1), 33.091, atol=0.001)
+        assert_allclose(np.mean(a2), 33.036, atol=0.001)
+        assert_allclose(np.mean(b2), -14.306, atol=0.001)
 
     def test_fit_ellipsesample_4(self):
         sample = EllipseSample(self.data2, 40., eps=0.4)
@@ -161,8 +162,8 @@ class TestFitEllipseSamples(object):
         harmonics = fit_first_and_second_harmonics(s[0], s[2])
         y0, a1, b1, a2, b2 = harmonics[0]
 
-        assert np.mean(y0) == pytest.approx(245.102, abs=0.001)
-        assert np.mean(a1) == pytest.approx(-0.003108, abs=0.001)
-        assert np.mean(b1) == pytest.approx(-0.0578, abs=0.001)
-        assert np.mean(a2) == pytest.approx(28.781, abs=0.001)
-        assert np.mean(b2) == pytest.approx(-63.184, abs=0.001)
+        assert_allclose(np.mean(y0), 245.102, atol=0.001)
+        assert_allclose(np.mean(a1), -0.003108, atol=0.001)
+        assert_allclose(np.mean(b1), -0.0578, atol=0.001)
+        assert_allclose(np.mean(a2), 28.781, atol=0.001)
+        assert_allclose(np.mean(b2), -63.184, atol=0.001)
