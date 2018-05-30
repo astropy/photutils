f2f834a0c3bf6e0bc600056ac6dd5a5c4a524389
Larry Bradley <larry.bradley@gmail.com>
2018-05-30 16:56:14 -0400

    Update subpackage imports

--- photutils/psf/building/__init__.py
+++ photutils/psf/building/__init__.py
@@ -4,12 +4,7 @@ This subpackage contains modules and packages to build point spread
 functions.
 """
 
-from .catalogs import *    # noqa
-from .centroid import *
-from .core import *
 from .epsf import *
 from .epsf_fitter import *
 from .models import *
-from .psf import *
 from .psfstars import *
-from .utils import *
