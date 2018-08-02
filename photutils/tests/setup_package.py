d420956d5ecc1e188ec3cfcff8fc09ab1a14e060
Larry Bradley <larry.bradley@gmail.com>
2018-08-02 14:36:54 -0400

    Update package init

--- photutils/tests/setup_package.py
+++ photutils/tests/setup_package.py
@@ -1,3 +1,11 @@
+# import os
+
+# If this package has test data in the tests/data directory, add them to
+# the paths here - see commented example
+paths = ['coveragerc',
+#         os.path.join('data', '*fits')
+         ]
+
 def get_package_data():
     return {
-        _ASTROPY_PACKAGE_NAME_ + '.tests': ['coveragerc']}    # noqa
+        _ASTROPY_PACKAGE_NAME_ + '.tests': paths}  # noqa
