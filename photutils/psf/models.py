123d22ba12f9156d78b966843d3004dfc3d18cff
Tom J. Wilson <towilson@stsci.edu>
2018-09-13 13:45:25 -0400

    Initial pixel_scale removal and redefinition of EPSFModel

--- photutils/psf/models.py
+++ photutils/psf/models.py
@@ -11,7 +11,7 @@ from astropy.modeling import models, Parameter, Fittable2DModel
 from astropy.utils.exceptions import AstropyWarning
 
 
-__all__ = ['NonNormalizable', 'FittableImageModel', 'EPSFModel',
+__all__ = ['NonNormalizable', 'FittableImageModel', 'EPSFModel', 'EPSFModel2',
            'IntegratedGaussianPRF', 'PRFAdapter', 'prepare_psf_model',
            'get_grouped_psf_model']
 
@@ -475,77 +475,419 @@ class FittableImageModel(Fittable2DModel):
 
         return evaluated_model
 
-
-class EPSFModel(FittableImageModel):
+class EPSFModel(Fittable2DModel):
     """
-    A subclass of `FittableImageModel` that adds a ``pixel_scale``
-    attribute.
-
-    The ``pixel_scale`` can be used in conjunction with the
-    `~photutils.psf.Star` pixel scale when fitting (and building) the
-    PSF.  This allows fitting (and building) a PSF using images of stars
-    with different pixel scales.
-
-    `EPSFModel` has the same parameters as `FittableImageModel` with the
-    addition of a single parameter listed below.
+    A fittable ePSF model.
 
     Parameters
     ----------
-    pixel_scale : float, tuple of two floats or `None`, optional
-        The pixel scale (in arbitrary units) of the ePSF.  The
-        ``pixel_scale`` can either be a single float or tuple of two
-        floats of the form ``(x_pixscale, y_pixscale)``.  If
-        ``pixel_scale`` is a scalar then the pixel scale will be the
-        same for both the x and y axes.  The default is `None`, which
-        means it will be set to the inverse of the ``oversampling``
-        factor.
+    data : numpy.ndarray
+        Array containing oePSF pixel values on a regularly oversampled pixel
+        grid.
 
-        The ePSF ``pixel_scale`` is used only when building the ePSF and
-        when fitting the ePSF to `Star` objects with `EPSFFitter`.  In
-        those cases, the ``pixel_scale`` is used in conjunction with the
-        `Star` pixel scale when building and fitting the ePSF.  This
-        allows for building (and fitting) a ePSF using images of stars
-        with different pixel scales (e.g. velocity aberrations).  The
-        ``oversampling`` factor is ignored in these cases.
+    origin : tuple, None, optional
+        A reference point in the input image ``data`` array. When origin is
+        `None`, origin will be set at the middle of the image array.
 
-        If you are not using `EPSFBuilder` or `EPSFFitter`, then you
-        must set the ``oversampling`` factor.  The ``pixel_scale`` will
-        be ignored.
+
+    normalize : bool, optional
+        Indicates whether or not the model should be build on normalized
+        input image data. If true, then the normalization constant (*N*) is
+        computed so that
+
+        .. math::
+            N \\cdot C \\cdot \\Sigma_{i,j}D_{i,j} = 1,
+
+        where *N* is the normalization constant, *C* is correction factor
+        given by the parameter ``normalization_correction``, and
+        :math:`D_{i,j}` are the elements of the input image ``data`` array.
+
+    normalization_correction : float, optional
+        A strictly positive number that represents correction that needs to
+        be applied to model's data normalization (see *C* in the equation
+        in the comments to ``normalize`` for more details).
+
+        A possible application for this parameter is to account for aperture
+        correction. Assuming model's data represent a PSF to be fitted to
+        some target star, we set ``normalization_correction`` to the aperture
+        correction that needs to be applied to the model. That is,
+        ``normalization_correction`` in this case should be set to the
+        ratio between the total flux of the PSF (including flux outside model's
+        data) to the flux of model's data.
+        Then, best fitted value of the `flux` model
+        parameter will represent an aperture-corrected flux of the target star.
+
+    fill_value : float, optional
+        The value to be returned by the `evaluate` or
+        ``astropy.modeling.Model.__call__`` methods
+        when evaluation is performed outside the definition domain of the
+        model.
+
+    ikwargs : dict, optional
+
+        Additional optional keyword arguments to be passed directly to the
+        `compute_interpolator` method. See `compute_interpolator` for more
+        details.
+
     """
+    x_0 = Parameter(description='X-position of a feature in the image in '
+                    'the output coordinate grid on which the model is '
+                    'evaluated.', default=0.0)
+    y_0 = Parameter(description='Y-position of a feature in the image in '
+                    'the output coordinate grid on which the model is '
+                    'evaluated.', default=0.0)
 
-    def __init__(self, data, flux=1.0, x_0=0, y_0=0, normalize=True,
-                 normalization_correction=1.0, origin=None, oversampling=1.,
-                 pixel_scale=None, fill_value=0., ikwargs={}):
+    def __init__(self, data, x_0=x_0.default, y_0=y_0.default,
+                 normalize=False, normalization_correction=1.0,
+                 origin=None, oversampling=1, fill_value=0.0, ikwargs={}):
+        self._fill_value = fill_value
+        self._img_norm = None
+        self._normalization_status = 0 if normalize else 2
+        self._store_interpolator_kwargs(ikwargs)
+        self._set_oversampling(oversampling)
 
-        if pixel_scale is None:
-            pixel_scale = 1. / oversampling
+        if normalization_correction <= 0:
+            raise ValueError("'normalization_correction' must be strictly "
+                             "positive.")
+        self._normalization_correction = normalization_correction
 
-        super().__init__(
-            data=data, flux=flux, x_0=x_0, y_0=y_0, normalize=normalize,
-            normalization_correction=normalization_correction, origin=origin,
-            oversampling=oversampling, fill_value=fill_value, ikwargs=ikwargs)
+        self._data = np.array(data, copy=True, dtype=np.float64)
 
-        self._pixel_scale = pixel_scale
+        if not np.all(np.isfinite(self._data)):
+            raise ValueError("All elements of input 'data' must be finite.")
+
+        # set input image related parameters:
+        self._ny, self._nx = self._data.shape
+        self._shape = self._data.shape
+        if self._data.size < 1:
+            raise ValueError("Image data array cannot be zero-sized.")
+
+        # set the origin of the coordinate system in image's pixel grid:
+        self.origin = origin
+
+        self._compute_normalization(normalize)
+
+        super().__init__(x_0, y_0)
+
+        # initialize interpolator:
+        self.compute_interpolator(ikwargs)
+
+    def _compute_raw_image_norm(self, data):
+        """
+        Helper function that computes the uncorrected inverse normalization
+        factor of input image data. This quantity is computed as the
+        *sum of all pixel values*.
+
+        .. note::
+            This function is intended to be overriden in a subclass if one
+            desires to change the way the normalization factor is computed.
+
+        """
+        return np.sum(self._data, dtype=np.float64)
+
+    def _compute_normalization(self, normalize):
+        """
+        Helper function that computes (corrected) normalization factor
+        of the original image data. This quantity is computed as the
+        inverse "raw image norm" (or total "flux" of model's image)
+        corrected by the ``normalization_correction``:
+
+        .. math::
+            N = 1/(\\Phi * C),
+
+        where :math:`\\Phi` is the "total flux" of model's image as
+        computed by `_compute_raw_image_norm` and *C* is the
+        normalization correction factor. :math:`\\Phi` is computed only
+        once if it has not been previously computed. Otherwise, the
+        existing (stored) value of :math:`\\Phi` is not modified as
+        :py:class:`FittableImageModel` does not allow image data to be
+        modified after the object is created.
+
+        .. note::
+            Normally, this function should not be called by the
+            end-user. It is intended to be overriden in a subclass if
+            one desires to change the way the normalization factor is
+            computed.
+        """
+
+        self._normalization_constant = 1.0 / self._normalization_correction
+
+        if normalize:
+            # compute normalization constant so that
+            # N*C*sum(data) = 1:
+            if self._img_norm is None:
+                self._img_norm = self._compute_raw_image_norm(self._data)
+
+            if self._img_norm != 0.0 and np.isfinite(self._img_norm):
+                self._normalization_constant /= self._img_norm
+                self._normalization_status = 0
+
+            else:
+                self._normalization_constant = 1.0
+                self._normalization_status = 1
+                warnings.warn("Overflow encountered while computing "
+                              "normalization constant. Normalization "
+                              "constant will be set to 1.", NonNormalizable)
+
+        else:
+            self._normalization_status = 2
 
     @property
-    def pixel_scale(self):
+    def oversampling(self):
         """
-        The ``(x, y)`` pixel scale (in arbitrary units) of the PSF.
+        The factor by which the stored image is oversampled.  I.e., an input
+        to this model is multipled by this factor to yield the index into the
+        stored image.
+        """
+        return self._oversampling
+
+    def _set_oversampling(self, value):
+        try:
+            value = float(value)
+        except ValueError:
+            raise ValueError('Oversampling factor must be a scalar')
+        if value <= 0:
+            raise ValueError('Oversampling factor must be greater than 0')
+
+        self._oversampling = value
+
+    @property
+    def data(self):
+        """ Get ePSF oversampled grid values. """
+        return self._data
+
+    @property
+    def normalized_data(self):
+        """ Get normalized and/or intensity-corrected image data. """
+        return (self._normalization_constant * self._data)
+
+    @property
+    def normalization_constant(self):
+        """ Get normalization constant. """
+        return self._normalization_constant
+
+    @property
+    def normalization_status(self):
+        """
+        Get normalization status. Possible status values are:
+
+        - 0: **Performed**. Model has been successfuly normalized at
+          user's request.
+        - 1: **Failed**. Attempt to normalize has failed.
+        - 2: **NotRequested**. User did not request model to be normalized.
+
+        """
+        return self._normalization_status
+
+    @property
+    def normalization_correction(self):
+        """
+        Set/Get flux correction factor.
+
+        .. note::
+            When setting correction factor, model's flux will be adjusted
+            accordingly such that if this model was a good fit to some target
+            image before, then it will remain a good fit after correction
+            factor change.
+
+        """
+        return self._normalization_correction
+
+    @normalization_correction.setter
+    def normalization_correction(self, normalization_correction):
+        old_cf = self._normalization_correction
+        self._normalization_correction = normalization_correction
+        self._compute_normalization(normalize=self._normalization_status != 2)
+
+        # adjust model's flux so that if this model was a good fit to some
+        # target image, then it will remain a good fit after correction factor
+        # change:
+        self.flux *= normalization_correction / old_cf
+
+    @property
+    def shape(self):
+        """A tuple of dimensions of the data array in numpy style (ny, nx)."""
+        return self._shape
+
+    @property
+    def nx(self):
+        """Number of columns in the data array."""
+        return self._nx
+
+    @property
+    def ny(self):
+        """Number of rows in the data array."""
+        return self._ny
+
+    @property
+    def origin(self):
+        """
+        A tuple of ``x`` and ``y`` coordinates of the origin of the coordinate
+        system in terms of pixels of model's image.
+
+        When setting the coordinate system origin, a tuple of two `int` or
+        `float` may be used. If origin is set to `None`, the origin of the
+        coordinate system will be set to the middle of the data array
+        (``(npix-1)/2.0``).
+
+        .. warning::
+            Modifying `origin` will not adjust (modify) model's parameters
+            `x_0` and `y_0`.
+        """
+        return (self._x_origin, self._y_origin)
+
+    @origin.setter
+    def origin(self, origin):
+        if origin is None:
+            self._x_origin = (self._nx - 1) / 2.0
+            self._y_origin = (self._ny - 1) / 2.0
+        elif hasattr(origin, '__iter__') and len(origin) == 2:
+            self._x_origin, self._y_origin = origin
+        else:
+            raise TypeError("Parameter 'origin' must be either None or an "
+                            "iterable with two elements.")
+
+    @property
+    def x_origin(self):
+        """X-coordinate of the origin of the coordinate system."""
+        return self._x_origin
+
+    @property
+    def y_origin(self):
+        """Y-coordinate of the origin of the coordinate system."""
+        return self._y_origin
+
+    @property
+    def fill_value(self):
+        """Fill value to be returned for coordinates outside of the domain of
+        definition of the interpolator. If ``fill_value`` is `None`, then
+        values outside of the domain of definition are the ones returned
+        by the interpolator.
+
+        """
+        return self._fill_value
+
+    @fill_value.setter
+    def fill_value(self, fill_value):
+        self._fill_value = fill_value
+
+    def _store_interpolator_kwargs(self, ikwargs):
+        """
+        This function should be called in a subclass whenever model's
+        interpolator is (re-)computed.
+        """
+        self._interpolator_kwargs = copy.deepcopy(ikwargs)
+
+    @property
+    def interpolator_kwargs(self):
+        """
+        Get current interpolator's arguments used when interpolator was
+        created.
+        """
+        return self._interpolator_kwargs
+
+    def compute_interpolator(self, ikwargs={}):
+        """
+        Compute/define the interpolating spline. This function can be overriden
+        in a subclass to define custom interpolators.
+
+        Parameters
+        ----------
+        ikwargs : dict, optional
+
+            Additional optional keyword arguments. Possible values are:
+
+            - **degree** : int, tuple, optional
+                Degree of the interpolating spline. A tuple can be used to
+                provide different degrees for the X- and Y-axes.
+                Default value is degree=3.
+
+            - **s** : float, optional
+                Non-negative smoothing factor. Default value s=0 corresponds to
+                interpolation.
+                See :py:class:`~scipy.interpolate.RectBivariateSpline` for more
+                details.
+
+        Notes
+        -----
+            * When subclassing :py:class:`FittableImageModel` for the
+              purpose of overriding :py:func:`compute_interpolator`,
+              the :py:func:`evaluate` may need to overriden as well depending
+              on the behavior of the new interpolator. In addition, for
+              improved future compatibility, make sure
+              that the overriding method stores keyword arguments ``ikwargs``
+              by calling ``_store_interpolator_kwargs`` method.
+
+            * Use caution when modifying interpolator's degree or smoothness in
+              a computationally intensive part of the code as it may decrease
+              code performance due to the need to recompute interpolator.
+
+        """
+        from scipy.interpolate import RectBivariateSpline
+
+        if 'degree' in ikwargs:
+            degree = ikwargs['degree']
+            if hasattr(degree, '__iter__') and len(degree) == 2:
+                degx = int(degree[0])
+                degy = int(degree[1])
+            else:
+                degx = int(degree)
+                degy = int(degree)
+            if degx < 0 or degy < 0:
+                raise ValueError("Interpolator degree must be a non-negative "
+                                 "integer")
+        else:
+            degx = 3
+            degy = 3
+
+        if 's' in ikwargs:
+            smoothness = ikwargs['s']
+        else:
+            smoothness = 0
+
+        x = np.arange(self._nx, dtype=np.float)
+        y = np.arange(self._ny, dtype=np.float)
+        self.interpolator = RectBivariateSpline(
+            x, y, self._data.T, kx=degx, ky=degy, s=smoothness
+        )
+
+        self._store_interpolator_kwargs(ikwargs)
+
+    def evaluate(self, x, y, flux, x_0, y_0, use_oversampling=True):
+        """
+        Evaluate the model on some input variables and provided model
+        parameters.
+
+        Parameters
+        ----------
+        use_oversampling : bool, optional
+            Whether to use the oversampling factor to calculate the
+            model pixel indices.  The default is `True`, which means the
+            input indices will be multipled by this factor.
         """
 
-        return self._pixel_scale
+        if use_oversampling:
+            xi = self._oversampling * (np.asarray(x) - x_0)
+            yi = self._oversampling * (np.asarray(y) - y_0)
+        else:
+            xi = np.asarray(x) - x_0
+            yi = np.asarray(y) - y_0
 
-    @pixel_scale.setter
-    def pixel_scale(self, pixel_scale):
-        if pixel_scale is not None:
-            pixel_scale = np.atleast_1d(pixel_scale)
-            if len(pixel_scale) == 1:
-                pixel_scale = np.repeat(pixel_scale, 2).astype(float)
-            elif len(pixel_scale) > 2:
-                raise ValueError('pixel_scale must be a scalar or tuple '
-                                 'of two floats.')
+        xi += self._x_origin
+        yi += self._y_origin
 
-        self._pixel_scale = pixel_scale
+        f = flux * self._normalization_constant
+        evaluated_model = f * self.interpolator.ev(xi, yi)
+
+        if self._fill_value is not None:
+            # find indices of pixels that are outside the input pixel grid and
+            # set these pixels to the 'fill_value':
+            invalid = (((xi < 0) | (xi > self._nx - 1)) |
+                       ((yi < 0) | (yi > self._ny - 1)))
+            evaluated_model[invalid] = self._fill_value
+
+        return evaluated_model
+
 
 
 class IntegratedGaussianPRF(Fittable2DModel):
