cd69105227e57d66d664cb673f179cfdb9b77ee9
Larry Bradley <larry.bradley@gmail.com>
2018-12-07 11:18:50 -0500

    Rework epsf_stars tests

--- photutils/psf/tests/test_epsf_stars.py
+++ photutils/psf/tests/test_epsf_stars.py
@@ -3,12 +3,11 @@
 import numpy as np
 import pytest
 
+from astropy.modeling.models import Moffat2D
 from astropy.nddata import NDData
 from astropy.table import Table
 
 from ..epsf_stars import extract_stars, EPSFStar, EPSFStars
-from ...datasets import load_simulated_hst_star_image
-from ...detection import find_peaks
 
 try:
     import scipy    # noqa
@@ -17,37 +16,42 @@ except ImportError:
     HAS_SCIPY = False
 
 
-hdu = load_simulated_hst_star_image()
-NDDATA = NDData(data=hdu.data)
-
-peaks_tbl = find_peaks(hdu.data, threshold=500)
-STARS_TBL = Table()
-STARS_TBL['x'] = peaks_tbl['x_peak']
-STARS_TBL['y'] = peaks_tbl['y_peak']
-
-
 @pytest.mark.skipif('not HAS_SCIPY')
-def test_extract_stars():
-    stars = extract_stars(NDDATA, STARS_TBL, size=25)
-    assert len(stars) == 403
-    assert isinstance(stars, EPSFStars)
-    assert isinstance(stars[0], EPSFStar)
-    assert stars[0].data.shape == (25, 25)
-    assert stars.n_stars == stars.n_all_stars
-    assert stars.n_stars == stars.n_good_stars
-    assert stars.center.shape == (len(stars), 2)
+class TestExtractStars:
+    def setup_class(self):
+        stars_tbl = Table()
+        stars_tbl['x'] = [15, 15, 35, 35]
+        stars_tbl['y'] = [15, 35, 40, 10]
+        self.stars_tbl = stars_tbl
 
+        yy, xx = np.mgrid[0:51, 0:55]
+        self.data = np.zeros(xx.shape)
+        for (xi, yi) in zip(stars_tbl['x'], stars_tbl['y']):
+            m = Moffat2D(100, xi, yi, 3, 3)
+            self.data += m(xx, yy)
 
-@pytest.mark.skipif('not HAS_SCIPY')
-def test_extract_stars_inputs():
-    with pytest.raises(ValueError):
-        extract_stars(np.ones(3), STARS_TBL)
+        self.nddata = NDData(data=self.data)
 
-    with pytest.raises(ValueError):
-        extract_stars(NDDATA, [(1, 1), (2, 2), (3, 3)])
+    def test_extract_stars(self):
+        size = 11
+        stars = extract_stars(self.nddata, self.stars_tbl, size=size)
+        assert len(stars) == 4
+        assert isinstance(stars, EPSFStars)
+        assert isinstance(stars[0], EPSFStar)
+        assert stars[0].data.shape == (size, size)
+        assert stars.n_stars == stars.n_all_stars
+        assert stars.n_stars == stars.n_good_stars
+        assert stars.center.shape == (len(stars), 2)
 
-    with pytest.raises(ValueError):
-        extract_stars(NDDATA, [STARS_TBL, STARS_TBL])
+    def test_extract_stars_inputs(self):
+        with pytest.raises(ValueError):
+            extract_stars(np.ones(3), self.stars_tbl)
 
-    with pytest.raises(ValueError):
-        extract_stars([NDDATA, NDDATA], STARS_TBL)
+        with pytest.raises(ValueError):
+            extract_stars(self.nddata, [(1, 1), (2, 2), (3, 3)])
+
+        with pytest.raises(ValueError):
+            extract_stars(self.nddata, [self.stars_tbl, self.stars_tbl])
+
+        with pytest.raises(ValueError):
+            extract_stars([self.nddata, self.nddata], self.stars_tbl)
