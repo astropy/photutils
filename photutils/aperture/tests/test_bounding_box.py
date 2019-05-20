dbdc464cf080466c449d4c06ec5eac304d5853fa
Larry Bradley <larry.bradley@gmail.com>
2019-05-20 14:57:17 -0400

    Add more BoundingBox tests

--- photutils/aperture/tests/test_bounding_box.py
+++ photutils/aperture/tests/test_bounding_box.py
@@ -4,6 +4,7 @@ from numpy.testing import assert_allclose
 import pytest
 
 from ..bounding_box import BoundingBox
+from ..rectangle import RectangularAperture
 
 try:
     import matplotlib    # noqa
@@ -14,7 +15,6 @@ except ImportError:
 
 def test_bounding_box_init():
     bbox = BoundingBox(1, 10, 2, 20)
-
     assert bbox.ixmin == 1
     assert bbox.ixmax == 10
     assert bbox.iymin == 2
@@ -56,50 +56,63 @@ def test_bounding_box_from_float():
 
 def test_bounding_box_eq():
     bbox = BoundingBox(1, 10, 2, 20)
-    assert bbox == bbox
-
+    assert bbox == BoundingBox(1, 10, 2, 20)
     assert bbox != BoundingBox(9, 10, 2, 20)
     assert bbox != BoundingBox(1, 99, 2, 20)
     assert bbox != BoundingBox(1, 10, 9, 20)
     assert bbox != BoundingBox(1, 10, 2, 99)
 
     with pytest.raises(TypeError):
-        bbox == (1, 10, 2, 20)
+        assert bbox == (1, 10, 2, 20)
 
 
 def test_bounding_box_repr():
     bbox = BoundingBox(1, 10, 2, 20)
-
     assert repr(bbox) == 'BoundingBox(ixmin=1, ixmax=10, iymin=2, iymax=20)'
-    assert eval(repr(bbox)) == bbox
 
 
 def test_bounding_box_shape():
     bbox = BoundingBox(1, 10, 2, 20)
-
     assert bbox.shape == (18, 9)
 
 
 def test_bounding_box_slices():
     bbox = BoundingBox(1, 10, 2, 20)
-
     assert bbox.slices == (slice(2, 20), slice(1, 10))
 
 
 def test_bounding_box_extent():
     bbox = BoundingBox(1, 10, 2, 20)
-
     assert_allclose(bbox.extent, (0.5, 9.5, 1.5, 19.5))
 
 
 @pytest.mark.skipif('not HAS_MATPLOTLIB')
 def test_bounding_box_as_artist():
     bbox = BoundingBox(1, 10, 2, 20)
-
     patch = bbox.as_artist()
+
     assert_allclose(patch.get_xy(), (0.5, 1.5))
     assert_allclose(patch.get_width(), 9)
     assert_allclose(patch.get_height(), 18)
+
+
+@pytest.mark.skipif('not HAS_MATPLOTLIB')
+def test_bounding_box_to_aperture():
+    bbox = BoundingBox(1, 10, 2, 20)
+    aper = RectangularAperture((5, 10.5), w=9., h=18., theta=0.)
+    bbox_aper = bbox.to_aperture()
+    assert_allclose(bbox_aper.positions, aper.positions)
+
+    assert bbox_aper.w == aper.w
+    assert bbox_aper.h == aper.h
+    assert bbox_aper.theta == aper.theta
+
+
+@pytest.mark.skipif('not HAS_MATPLOTLIB')
+def test_bounding_box_plot():
+    # TODO: check the content of the plot
+    bbox = BoundingBox(1, 10, 2, 20)
+    bbox.plot()
 
 
 def test_bounding_box_union():
@@ -114,6 +127,7 @@ def test_bounding_box_union():
 
     with pytest.raises(TypeError):
         bbox1.union((5, 21, 7, 32))
+
 
 def test_bounding_box_intersect():
     bbox1 = BoundingBox(1, 10, 2, 20)
