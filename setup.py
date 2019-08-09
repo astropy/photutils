a7796f5aa9280b3f8ae98a9e1d958fda54713b42
Larry Bradley <larry.bradley@gmail.com>
2019-08-09 13:43:39 -0400

    Include cython in setup_requires only if in dev mode

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 import builtins
+import os
 
 # Ensure that astropy-helpers is available
 import ah_bootstrap  # noqa
@@ -10,6 +10,7 @@ import ah_bootstrap  # noqa
 from setuptools import setup
 from setuptools.config import read_configuration
 
+from astropy_helpers.distutils_helpers import is_distutils_display_option
 from astropy_helpers.setup_helpers import register_commands, get_package_info
 from astropy_helpers.version_helpers import generate_version_py
 
@@ -30,4 +31,16 @@ version = generate_version_py()
 # details.
 package_info = get_package_info()
 
-setup(version=version, cmdclass=cmdclassd, **package_info)
+if is_distutils_display_option():
+    # Avoid installing setup_requires dependencies if the user just
+    # queries for information
+    setup_requires = []
+else:
+    setup_requires = read_configuration('setup.cfg')['options']['setup_requires']
+    # Make sure we have the packages needed for building photuils, but do not
+    # require them when installing from an sdist as the c files are included.
+    if not os.path.exists(os.path.join(os.path.dirname(__file__), 'PKG-INFO')):
+        setup_requires.extend(['cython>=0.28'])
+
+setup(setup_requires=setup_requires, version=version, cmdclass=cmdclassd,
+      **package_info)
