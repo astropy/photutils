fa0f4b3700991c5e5d1ab8edf2d3d208ffd10b3d
Tom J Wilson <towilson@stsci.edu>
2019-08-15 14:16:12 -0400

    Added catch for BasicPSFPhotometry allowing for init_guesses to be passed but needing to catch for no flux_0 column before falling back on default radius

--- photutils/psf/photometry.py
+++ photutils/psf/photometry.py
@@ -258,14 +258,18 @@ class BasicPSFPhotometry:
                                         gaussian_sigma_to_fwhm)
             # If PSF model doesn't have FWHM or sigma value -- as it
             # is not a Gaussian; most likely because it's an ePSF --
-            # then we fall back on fitting a circle of the average
-            # size of the fitting box. As ``fitshape`` is the width
-            # of the box, we need (width-1)/2 as the radius.
-            else:
+            # and if fluxes were not supplied in ``init_guesses`` then we fall
+            # back on fitting a circle of the average size of the fitting box.
+            # As ``fitshape`` is the width of the box, we need (width-1)/2 as
+            # the radius.
+            elif (init_guesses is not None and
+                  'flux_0' not in init_guesses.colnames):
                 self.aperture_radius = float(np.amin((np.asanyarray(
                                              self.fitshape) - 1) / 2))
-                warnings.warn('aperture_radius is None and could not '
-                              'be determined by psf_model. Setting '
+                warnings.warn('init_guesses were input, but the flux_0 column '
+                              'was not provided. Initial fluxes cannot be '
+                              'calculated as aperture_radius is None and '
+                              'could not be determined by psf_model. Setting '
                               'radius to the smallest fitshape size. '
                               'If fitshape is significantly larger than '
                               'the psf_model core lengthscale, consider '
@@ -736,7 +740,10 @@ class IterativelySubtractedPSFPhotometry(BasicPSFPhotometry):
                 # is not a Gaussian; most likely because it's an ePSF --
                 # then we fall back on fitting a circle of the average
                 # size of the fitting box. As ``fitshape`` is the width
-                # of the box, we need (width-1)/2 as the radius.
+                # of the box, we need (width-1)/2 as the radius. As opposed to
+                # ``BasicPSFPhotometry``, we have to fallback on this even if
+                # fluxes were given in ``init_guesses``, as this will be used
+                # in subsequent iterations.
                 else:
                     self.aperture_radius = float(np.amin((np.asanyarray(
                                                  self.fitshape) - 1) / 2))
