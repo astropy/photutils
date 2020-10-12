9c720121047f0ffb66e178b5f333c25db08f8904
Larry Bradley <larry.bradley@gmail.com>
2020-10-12 12:44:13 -0400

    Clean up conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,6 @@
 # else. See astropy.sphinx.conf for which values are set there.
 
 from configparser import ConfigParser
-import datetime
 import os
 import sys
 
@@ -30,6 +29,7 @@ conf = ConfigParser()
 conf.read([os.path.join(os.path.dirname(__file__), '..', 'setup.cfg')])
 setup_cfg = dict(conf.items('metadata'))
 
+
 # -- General configuration ----------------------------------------------------
 # By default, highlight as Python 3.
 highlight_language = 'python3'
@@ -38,24 +38,22 @@ highlight_language = 'python3'
 needs_sphinx = '1.7'
 
 # Extend astropy intersphinx_mapping with packages we use here
-intersphinx_mapping['skimage'] = ('https://scikit-image.org/docs/stable/',
-                                  None)
-intersphinx_mapping['gwcs'] = ('https://gwcs.readthedocs.io/en/latest/', None)
+intersphinx_mapping['skimage'] = ('https://scikit-image.org/docs/stable/', None)  # noqa
+intersphinx_mapping['gwcs'] = ('https://gwcs.readthedocs.io/en/latest/', None)  # noqa
 
 # Exclude astropy intersphinx_mapping for unused packages
-del intersphinx_mapping['h5py']
+del intersphinx_mapping['h5py']  # noqa
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns.append('_templates')
+exclude_patterns.append('_templates')  # noqa
 # Exclude template PSF block specification documentation
-exclude_patterns.append('psf_spec/*')
+exclude_patterns.append('psf_spec/*')  # noqa
 
 plot_formats = ['png', 'hires.png', 'pdf', 'svg']
 
 # This is added to the end of RST files - a good place to put
 # substitutions to be used globally.
-#import photutils
 rst_epilog = """
 .. _Astropy: https://www.astropy.org/
 """
@@ -147,16 +145,17 @@ man_pages = [('index', project.lower(), project + u' Documentation',
 
 
 # -- Resolving issue number to links in changelog -----------------------------
-github_issues_url = 'https://github.com/{0}/issues/'.format(setup_cfg['github_project'])
+github_project = setup_cfg['github_project']
+github_issues_url = f'https://github.com/{github_project}/issues/'
 
 
 # -- Turn on nitpicky mode for sphinx (to warn about references not found) ----
 nitpicky = True
 nitpick_ignore = []
 
-# Some warnings are impossible to suppress, and you can list specific references
-# that should be ignored in a nitpick-exceptions file which should be inside
-# the docs/ directory. The format of the file should be:
+# Some warnings are impossible to suppress, and you can list specific
+# references that should be ignored in a nitpick-exceptions file which
+# should be inside the docs/ directory. The format of the file should be:
 #
 # <type> <class>
 #
@@ -167,16 +166,17 @@ nitpick_ignore = []
 # py:class astropy.io.votable.tree.SimpleElementWithContent
 #
 # Uncomment the following lines to enable the exceptions:
-#
-for line in open('nitpick-exceptions'):
-    if line.strip() == "" or line.startswith("#"):
-        continue
-    dtype, target = line.split(None, 1)
-    target = target.strip()
-    nitpick_ignore.append((dtype, target))
+nitpick_filename = 'nitpick-exceptions.txt'
+if os.path.isfile(nitpick_filename):
+    for line in open(nitpick_filename):
+        if line.strip() == "" or line.startswith("#"):
+            continue
+        dtype, target = line.split(None, 1)
+        target = target.strip()
+        nitpick_ignore.append((dtype, target))
 
 
-# -- Options for linkcheck output -------------------------------------------
+# -- Options for linkcheck output ---------------------------------------------
 linkcheck_retry = 5
 linkcheck_ignore = ['http://data.astropy.org',
                     r'https://github\.com/astropy/photutils/(?:issues|pull)/\d+']
