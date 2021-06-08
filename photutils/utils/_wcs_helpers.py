a8a91d1e653ad95ddd6c120b3e6b8dc40ef6272c
Larry Bradley <larry.bradley@gmail.com>
2021-06-08 14:10:20 -0400

    Update wcs helpers

--- photutils/utils/_wcs_helpers.py
+++ photutils/utils/_wcs_helpers.py
@@ -1,93 +1,17 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+# (taken from photutils: should probably migrate into astropy.wcs)
 """
 This module provides WCS helper tools.
 """
 
-from astropy import units as u
-from astropy.coordinates import UnitSphericalRepresentation
-from astropy.wcs import WCS
-from astropy.wcs.utils import pixel_to_skycoord, skycoord_to_pixel
+import astropy.units as u
 import numpy as np
 
 
-def _pixel_to_world(xpos, ypos, wcs):
+def _pixel_scale_angle_at_skycoord(skycoord, wcs, offset=1 * u.arcsec):
     """
-    Calculate the sky coordinates at the input pixel positions.
-
-    Parameters
-    ----------
-    xpos, ypos : float or array-like
-        The x and y pixel position(s).
-
-    wcs : WCS object or `None`
-        A world coordinate system (WCS) transformation that
-        supports the `astropy shared interface for WCS
-        <https://docs.astropy.org/en/stable/wcs/wcsapi.html>`_ (e.g.,
-        `astropy.wcs.WCS`, `gwcs.wcs.WCS`).
-
-    Returns
-    -------
-    skycoord : `~astropy.coordinates.SkyCoord`
-        The sky coordinate(s) at the input position(s).
-    """
-
-    if wcs is None:
-        return None
-
-    # NOTE: unitless gwcs objects fail with Quantity inputs
-    if isinstance(xpos, u.Quantity) or isinstance(ypos, u.Quantity):
-        raise TypeError('xpos and ypos must not be Quantity objects')
-
-    try:
-        return wcs.pixel_to_world(xpos, ypos)
-    except AttributeError:
-        if isinstance(wcs, WCS):
-            # for Astropy < 3.1 WCS support
-            return pixel_to_skycoord(xpos, ypos, wcs, origin=0)
-        else:
-            raise ValueError('Input wcs does not support the shared WCS '
-                             'interface.')
-
-
-def _world_to_pixel(skycoord, wcs):
-    """
-    Calculate the sky coordinates at the input pixel positions.
-
-    Parameters
-    ----------
-    skycoord : `~astropy.coordinates.SkyCoord`
-        The sky coordinate(s).
-
-    wcs : WCS object or `None`
-        A world coordinate system (WCS) transformation that
-        supports the `astropy shared interface for WCS
-        <https://docs.astropy.org/en/stable/wcs/wcsapi.html>`_ (e.g.,
-        `astropy.wcs.WCS`, `gwcs.wcs.WCS`).
-
-    Returns
-    -------
-    xpos, ypos : float or array-like
-        The x and y pixel position(s) at the input sky coordinate(s).
-    """
-
-    if wcs is None:
-        return None
-
-    try:
-        return wcs.world_to_pixel(skycoord)
-    except AttributeError:
-        if isinstance(wcs, WCS):
-            # for Astropy < 3.1 WCS support
-            return skycoord_to_pixel(skycoord, wcs, origin=0)
-        else:
-            raise ValueError('Input wcs does not support the shared WCS '
-                             'interface.')
-
-
-def _pixel_scale_angle_at_skycoord(skycoord, wcs, offset=1.0*u.arcsec):
-    """
-    Calculate the pixel scale and WCS rotation angle at the position of
-    a SkyCoord coordinate.
+    Calculate the pixel coordinate and scale and WCS rotation angle at
+    the position of a SkyCoord coordinate.
 
     Parameters
     ----------
@@ -106,6 +30,9 @@ def _pixel_scale_angle_at_skycoord(skycoord, wcs, offset=1.0*u.arcsec):
 
     Returns
     -------
+    xypos : tuple of float
+        The (x, y) pixel coordinate.
+
     scale : `~astropy.units.Quantity`
         The pixel scale in arcsec/pixel.
 
@@ -120,27 +47,19 @@ def _pixel_scale_angle_at_skycoord(skycoord, wcs, offset=1.0*u.arcsec):
     likely differ.  This function computes a single pixel scale along
     the North/South axis.
     """
+    # Convert to pixel coordinates
+    xpos, ypos = wcs.world_to_pixel(skycoord)
 
-    try:
-        x, y = wcs.world_to_pixel(skycoord)
+    # We take a point directly North (i.e., latitude offset) the
+    # input sky coordinate and convert it to pixel coordinates,
+    # then we use the pixel deltas between the input and offset sky
+    # coordinate to calculate the pixel scale and angle.
+    skycoord_offset = skycoord.directional_offset_by(0.0, offset)
+    x_offset, y_offset = wcs.world_to_pixel(skycoord_offset)
 
-        # We take a point directly North (i.e., latitude offset) the
-        # input sky coordinate and convert it to pixel coordinates,
-        # then we use the pixel deltas between the input and offset sky
-        # coordinate to calculate the pixel scale and angle.
-        skycoord_offset = skycoord.directional_offset_by(0.0, offset)
-        x_offset, y_offset = wcs.world_to_pixel(skycoord_offset)
-    except AttributeError:
-        # for Astropy < 3.1 WCS support
-        x, y = skycoord_to_pixel(skycoord, wcs)
-        coord = skycoord.represent_as('unitspherical')
-        coord_new = UnitSphericalRepresentation(coord.lon, coord.lat + offset)
-        coord_offset = skycoord.realize_frame(coord_new)
-        x_offset, y_offset = skycoord_to_pixel(coord_offset, wcs)
-
-    dx = x_offset - x
-    dy = y_offset - y
+    dx = x_offset - xpos
+    dy = y_offset - ypos
     scale = offset.to(u.arcsec) / (np.hypot(dx, dy) * u.pixel)
     angle = (np.arctan2(dy, dx) * u.radian).to(u.deg)
 
-    return scale, angle
+    return (xpos, ypos), scale, angle
