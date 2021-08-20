88bfcb37227b1e02abfc44fa931a940cce354e03
Larry Bradley <larry.bradley@gmail.com>
2021-08-20 14:27:27 -0400

    Add module for optional dependency checking

---
+++ photutils/utils/_optional_deps.py
@@ -0,0 +1,22 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""Checks for optional dependencies using lazy import from
+`PEP 562 <https://www.python.org/dev/peps/pep-0562/>`_.
+"""
+import importlib
+
+# This list is a duplicate of the dependencies in setup.cfg "all".
+optional_deps = ['scipy', 'matplotlib', 'scikit-image', 'scikit-learn',
+                 'gwcs']
+deps = {key.upper(): key for key in optional_deps}
+__all__ = [f'HAS_{pkg}' for pkg in deps]
+
+
+def __getattr__(name):
+    if name in __all__:
+        try:
+            importlib.import_module(deps[name[4:]])
+        except (ImportError, ModuleNotFoundError):
+            return False
+        return True
+
+    raise AttributeError(f'Module {__name__!r} has no attribute {name!r}.')
