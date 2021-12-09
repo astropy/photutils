5ea57d7b727afdbe622cb19bc3df0d337af27e80
Larry Bradley <larry.bradley@gmail.com>
2021-12-09 18:29:03 -0500

    Allow extra kwargs in centroid_sources to be passed to centroid_func

--- photutils/centroids/core.py
+++ photutils/centroids/core.py
@@ -273,8 +273,8 @@ def _process_boxsize(box_size, data_shape):
     return box_size
 
 
-def centroid_sources(data, xpos, ypos, box_size=11, footprint=None,
-                     error=None, mask=None, centroid_func=centroid_com):
+def centroid_sources(data, xpos, ypos, box_size=11, footprint=None, mask=None,
+                     centroid_func=centroid_com, **kwargs):
     """
     Calculate the centroid of sources at the defined positions.
 
@@ -329,6 +329,10 @@ def centroid_sources(data, xpos, ypos, box_size=11, footprint=None,
         a tuple of two 1D `~numpy.ndarray`, representing the x and y
         centroids. The default is `~photutils.centroids.centroid_com`.
 
+    **kwargs : `dict`
+        Any additional keyword arguments accepted by the
+        ``centroid_func``.
+
     Returns
     -------
     xcentroid, ycentroid : `~numpy.ndarray`
@@ -367,13 +371,16 @@ def centroid_sources(data, xpos, ypos, box_size=11, footprint=None,
         if footprint.ndim != 2:
             raise ValueError('footprint must be a 2D array.')
 
-    use_error = False
     spec = inspect.getfullargspec(centroid_func)
     if 'mask' not in spec.args:
         raise ValueError('The input "centroid_func" must have a "mask" '
                          'keyword.')
-    if 'error' in spec.args:
-        use_error = True
+
+    # drop any **kwargs not supported by the centroid_func
+    centroid_kwargs = {}
+    for key, val in kwargs.items():
+        if key in spec.args:
+            centroid_kwargs[key] = val
 
     xcentroids = []
     ycentroids = []
@@ -386,7 +393,7 @@ def centroid_sources(data, xpos, ypos, box_size=11, footprint=None,
         if mask is not None:
             mask_cutout = mask[slices_large]
 
-        footprint_mask = ~footprint
+        footprint_mask = np.logical_not(footprint)
         # trim footprint mask if it has only partial overlap on the data
         footprint_mask = footprint_mask[slices_small]
 
@@ -396,12 +403,16 @@ def centroid_sources(data, xpos, ypos, box_size=11, footprint=None,
             # combine the input mask and footprint mask
             mask_cutout = np.logical_or(mask_cutout, footprint_mask)
 
-        kwargs = {'mask': mask_cutout}
-        if error is not None and use_error:
-            kwargs['error'] = error[slices_large]
+        if 'error' in centroid_kwargs:
+            error_cutout = centroid_kwargs['error'][slices_large]
+            centroid_kwargs['error'] = error_cutout
+
+        if 'xpeak' in centroid_kwargs and 'ypeak' in centroid_kwargs:
+            centroid_kwargs['xpeak'] -= slices_large[1].start
+            centroid_kwargs['ypeak'] -= slices_large[0].start
 
         try:
-            xcen, ycen = centroid_func(data_cutout, **kwargs)
+            xcen, ycen = centroid_func(data_cutout, **centroid_kwargs)
         except (ValueError, TypeError):
             xcen, ycen = np.nan, np.nan
 
