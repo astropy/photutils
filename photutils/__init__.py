654357fc2bc92982463ba1edb954db093e8b17c0
Larry Bradley <larry.bradley@gmail.com>
2021-12-23 13:17:48 -0500

    Importing centroids and morphology tools now require including the subpackage name

--- photutils/__init__.py
+++ photutils/__init__.py
@@ -6,8 +6,6 @@ has tools for background estimation, ePSF building, PSF matching,
 centroiding, and morphological measurements.
 """
 
-import warnings
-
 # Affiliated packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
@@ -19,37 +17,6 @@ from .background import *  # noqa
 from .detection import *  # noqa
 from .psf import *  # noqa
 from .segmentation import *  # noqa
-
-# deprecations
-from . import centroids
-from . import morphology
-
-__depr__ = {}
-__depr__[centroids] = ('centroid_com', 'centroid_quadratic',
-                       'centroid_sources', 'centroid_epsf',
-                       'centroid_1dg', 'gaussian1d_moments', 'centroid_2dg')
-__depr__[morphology] = ('data_properties', 'gini')
-
-__depr_mesg__ = ('`photutils.{attr}` is a deprecated alias for '
-                 '`{module}.{attr}`. Instead, please use `from {module} '
-                 'import {attr}` to silence this warning.')
-
-__depr_attrs__ = {}
-for k, vals in __depr__.items():
-    for val in vals:
-        __depr_attrs__[val] = (getattr(k, val),
-                               __depr_mesg__.format(module=k.__name__,
-                                                    attr=val))
-del k, val, vals
-
-
-def __getattr__(attr):
-    if attr in __depr_attrs__:
-        obj, message = __depr_attrs__[attr]
-        warnings.warn(message, DeprecationWarning, stacklevel=2)
-        return obj
-    raise AttributeError('module {!r} has no attribute {!r}'
-                         .format(__name__, attr))
 
 
 # Set the bibtex entry to the article referenced in CITATION.rst.
@@ -67,4 +34,4 @@ def _get_bibtex():
 
 __citation__ = __bibtex__ = _get_bibtex()
 
-del _astropy_init, _get_bibtex  # noqa
+del _get_bibtex  # noqa
