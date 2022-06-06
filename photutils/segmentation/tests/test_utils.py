d05a888abb8fba970694d6a1d84a8f1496caec80
Larry Bradley <larry.bradley@gmail.com>
2022-06-06 15:34:03 -0400

    Move mask_to_mirrored_num to utils module

--- photutils/segmentation/tests/test_utils.py
+++ photutils/segmentation/tests/test_utils.py
@@ -6,10 +6,10 @@ Tests for the _utils module.
 import numpy as np
 from numpy.testing import assert_allclose
 
-from .._utils import mask_to_mirrored_value
+from ..utils import _mask_to_mirrored_value
 
 
-def testmask_to_mirrored_value():
+def test_mask_to_mirrored_value():
     center = (2.0, 2.0)
     data = np.arange(25).reshape(5, 5)
     mask = np.zeros(data.shape, dtype=bool)
@@ -18,11 +18,11 @@ def testmask_to_mirrored_value():
     data_ref = data.copy()
     data_ref[0, 0] = data[4, 4]
     data_ref[1, 1] = data[3, 3]
-    mirror_data = mask_to_mirrored_value(data, mask, center)
+    mirror_data = _mask_to_mirrored_value(data, mask, center)
     assert_allclose(mirror_data, data_ref, rtol=0, atol=1.e-6)
 
 
-def testmask_to_mirrored_value_range():
+def test_mask_to_mirrored_value_range():
     """
     Test mask_to_mirrored_value when mirrored pixels are outside of the
     image.
@@ -37,11 +37,11 @@ def testmask_to_mirrored_value_range():
     data_ref[0, 0] = 0.
     data_ref[1, 1] = 0.
     data_ref[2, 2] = data[4, 4]
-    mirror_data = mask_to_mirrored_value(data, mask, center)
+    mirror_data = _mask_to_mirrored_value(data, mask, center)
     assert_allclose(mirror_data, data_ref, rtol=0, atol=1.e-6)
 
 
-def testmask_to_mirrored_value_masked():
+def test_mask_to_mirrored_value_masked():
     """
     Test mask_to_mirrored_value when mirrored pixels are also in the
     replace_mask.
@@ -58,12 +58,12 @@ def testmask_to_mirrored_value_masked():
     data_ref[1, 1] = 0.
     data_ref[3, 3] = 0.
     data_ref[4, 4] = 0.
-    mirror_data = mask_to_mirrored_value(data, mask, center)
-    mirror_data = mask_to_mirrored_value(data, mask, center)
+    mirror_data = _mask_to_mirrored_value(data, mask, center)
+    mirror_data = _mask_to_mirrored_value(data, mask, center)
     assert_allclose(mirror_data, data_ref, rtol=0, atol=1.e-6)
 
 
-def testmask_to_mirrored_value_mask_keyword():
+def test_mask_to_mirrored_value_mask_keyword():
     """
     Test mask_to_mirrored_value when mirrored pixels are masked (via the
     mask keyword).
@@ -75,5 +75,5 @@ def testmask_to_mirrored_value_mask_keyword():
     replace_mask[0, 2] = True
     data[4, 2] = np.nan
     mask[4, 2] = True
-    result = mask_to_mirrored_value(data, replace_mask, center, mask=mask)
+    result = _mask_to_mirrored_value(data, replace_mask, center, mask=mask)
     assert result[0, 2] == 0
--- photutils/segmentation/utils.py
+++ photutils/segmentation/utils.py
@@ -101,3 +101,73 @@ def _make_binary_structure(ndim, connectivity):
         selem = generate_binary_structure(ndim, 1)
 
     return selem
+
+
+def _mask_to_mirrored_value(data, replace_mask, xycenter, mask=None):
+    """
+    Replace masked pixels with the value of the pixel mirrored across a
+    given center position.
+
+    If the mirror pixel is unavailable (i.e., it is outside of the image
+    or masked), then the masked pixel value is set to zero.
+
+    Parameters
+    ----------
+    data : 2D `~numpy.ndarray`
+        A 2D array.
+
+    replace_mask : 2D bool `~numpy.ndarray`
+        A boolean mask where `True` values indicate the pixels that
+        should be replaced, if possible, by mirrored pixel values. It
+        must have the same shape as ``data``.
+
+    xycenter : tuple of two int
+        The (x, y) center coordinates around which masked pixels will be
+        mirrored.
+
+    mask : 2D bool `~numpy.ndarray`
+        A boolean mask where `True` values indicate ``replace_mask``
+        *mirrored* pixels that should never be used to fix
+        ``replace_mask`` pixels. In other words, if a pixel in
+        ``replace_mask`` has a mirror pixel in this ``mask``, then the
+        mirrored value is set to zero. Using this keyword prevents
+        potential spreading of known non-finite or bad pixel values.
+
+    Returns
+    -------
+    result : 2D `~numpy.ndarray`
+        A 2D array with replaced masked pixels.
+    """
+    outdata = np.copy(data)
+
+    ymasked, xmasked = np.nonzero(replace_mask)
+    xmirror = 2 * int(xycenter[0] + 0.5) - xmasked
+    ymirror = 2 * int(xycenter[1] + 0.5) - ymasked
+
+    # Find mirrored pixels that are outside of the image
+    badmask = ((xmirror < 0) | (ymirror < 0) | (xmirror >= data.shape[1])
+               | (ymirror >= data.shape[0]))
+
+    # remove them from the set of replace_mask pixels and set them to
+    # zero
+    if np.any(badmask):
+        outdata[ymasked[badmask], xmasked[badmask]] = 0.
+        # remove the badmask pixels from pixels to be replaced
+        goodmask = ~badmask
+        ymasked = ymasked[goodmask]
+        xmasked = xmasked[goodmask]
+        xmirror = xmirror[goodmask]
+        ymirror = ymirror[goodmask]
+
+    outdata[ymasked, xmasked] = outdata[ymirror, xmirror]
+
+    # Find mirrored pixels that are masked and replace_mask pixels that are
+    # mirrored to other replace_mask pixels. Set them both to zero.
+    mirror_mask = replace_mask[ymirror, xmirror]
+    if mask is not None:
+        mirror_mask |= mask[ymirror, xmirror]
+    xbad = xmasked[mirror_mask]
+    ybad = ymasked[mirror_mask]
+    outdata[ybad, xbad] = 0.0
+
+    return outdata
