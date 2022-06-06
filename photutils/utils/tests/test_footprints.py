5afbfb347bd0cfef35f4557ee02e3af9d65b88b2
Larry Bradley <larry.bradley@gmail.com>
2022-06-06 13:06:25 -0400

    Add circular_footprint tests

---
+++ photutils/utils/tests/test_footprints.py
@@ -0,0 +1,41 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+Tests for the footprints module.
+"""
+
+import numpy as np
+from numpy.testing import assert_equal
+import pytest
+
+from ..footprints import circular_footprint
+
+
+def test_footprints():
+    footprint = circular_footprint(1)
+    result = np.array([[0, 1, 0],
+                       [1, 1, 1],
+                       [0, 1, 0]])
+    assert_equal(footprint, result)
+
+    footprint = circular_footprint(2)
+    result = np.array([[0, 0, 1, 0, 0],
+                       [0, 1, 1, 1, 0],
+                       [1, 1, 1, 1, 1],
+                       [0, 1, 1, 1, 0],
+                       [0, 0, 1, 0, 0]])
+    assert_equal(footprint, result)
+
+    with pytest.raises(ValueError):
+        circular_footprint(5.1)
+
+    with pytest.raises(ValueError):
+        circular_footprint(0)
+
+    with pytest.raises(ValueError):
+        circular_footprint(-1)
+
+    with pytest.raises(ValueError):
+        circular_footprint(np.inf)
+
+    with pytest.raises(ValueError):
+        circular_footprint(np.nan)
