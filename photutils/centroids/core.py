7741912948396b03185e289222f7f088cdb21efd
Larry Bradley <larry.bradley@gmail.com>
2022-07-01 11:52:25 -0400

    Deprecate oversampling keyword in centroid_com

--- photutils/centroids/core.py
+++ photutils/centroids/core.py
@@ -7,6 +7,7 @@ import inspect
 import warnings
 
 from astropy.nddata.utils import overlap_slices
+from astropy.utils.decorators import deprecated_renamed_argument
 from astropy.utils.exceptions import AstropyUserWarning
 import numpy as np
 
@@ -16,6 +17,7 @@ from ..utils._parameters import as_pair
 __all__ = ['centroid_com', 'centroid_quadratic', 'centroid_sources']
 
 
+@deprecated_renamed_argument('oversampling', None, '1.5')
 def centroid_com(data, mask=None, oversampling=1):
     """
     Calculate the centroid of an n-dimensional array as its "center of
@@ -34,6 +36,7 @@ def centroid_com(data, mask=None, oversampling=1):
         value indicates the corresponding element of ``data`` is masked.
 
     oversampling : int or array_like (int)
+        Deprecated.
         The integer oversampling factor(s). If ``oversampling`` is a
         scalar then it will be used for both axes. If ``oversampling``
         has two elements, they must be in ``(y, x)`` order.
