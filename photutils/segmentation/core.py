eb12f01fc6aa332f7ac2f76c7814532b1d9419ad
Larry Bradley <larry.bradley@gmail.com>
2023-04-03 16:33:07 -0400

    Add plot_patches example

--- photutils/segmentation/core.py
+++ photutils/segmentation/core.py
@@ -1252,6 +1252,24 @@ class SegmentationImage:
             A list of matplotlib polygon patches for the plotted
             polygons. The patches can be used, for example, when adding
             a plot legend.
+
+        Examples
+        --------
+        .. plot::
+            :include-source:
+
+            import numpy as np
+            from photutils.segmentation import SegmentationImage
+
+            data = np.array([[1, 1, 0, 0, 4, 4],
+                             [0, 0, 0, 0, 0, 4],
+                             [0, 0, 3, 3, 0, 0],
+                             [7, 0, 0, 0, 0, 5],
+                             [7, 7, 0, 5, 5, 5],
+                             [7, 7, 0, 0, 5, 5]])
+            segm = SegmentationImage(data)
+            segm.imshow(figsize=(5, 5))
+            segm.plot_patches(edgecolor='white', lw=2)
         """
         import matplotlib.pyplot as plt
 
