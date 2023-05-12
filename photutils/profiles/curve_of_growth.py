88e22c99182ff3391496cb7a62c8bf6f2a695d90
Larry Bradley <larry.bradley@gmail.com>
2023-05-12 13:59:41 -0400

    Change API for CurveOfGrowth

--- photutils/profiles/curve_of_growth.py
+++ photutils/profiles/curve_of_growth.py
@@ -2,7 +2,7 @@
 """
 This module provides tools for generating curves of growth.
 """
-
+import numpy as np
 from astropy.utils import lazyproperty
 
 from photutils.profiles.core import ProfileBase
@@ -28,15 +28,11 @@ class CurveOfGrowth(ProfileBase):
     xycen : tuple of 2 floats
         The ``(x, y)`` pixel coordinate of the source center.
 
-    min_radius : float
-        The minimum radius for the profile. Must be greater than or
-        equal to zero.
-
-    max_radius : float
-        The maximum radius for the profile.
-
-    radius_step : float
-        The radial step size in pixels.
+    radii : 1D float `numpy.ndarray`
+        An array of the circular radii. ``radii`` must be strictly
+        increasing with a minimum value greater than zero, and contain
+        at least 2 values. The radial spacing does not need to be
+        constant.
 
     error : 2D `numpy.ndarray`, optional
         The 1-sigma errors of the input ``data``. ``error`` is assumed
@@ -101,31 +97,26 @@ class CurveOfGrowth(ProfileBase):
     Create the curve of growth.
 
     >>> xycen = centroid_quadratic(data, xpeak=48, ypeak=52)
-    >>> min_radius = 0.0
-    >>> max_radius = 25.0
-    >>> radius_step = 1.0
-    >>> cog = CurveOfGrowth(data, xycen, min_radius, max_radius, radius_step,
-    ...                     error=error, mask=None)
+    >>> radii = np.arange(1, 26)
+    >>> cog = CurveOfGrowth(data, xycen, radii, error=error, mask=None)
 
     >>> print(cog.radius)  # doctest: +FLOAT_CMP
-    [ 0.  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12. 13. 14. 15. 16. 17.
-     18. 19. 20. 21. 22. 23. 24. 25.]
+    [ 1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+     25]
 
     >>> print(cog.profile)  # doctest: +FLOAT_CMP
-    [   0.          130.57472018  501.34744442 1066.59182074 1760.50163608
-     2502.13955554 3218.50667597 3892.81448231 4455.36403436 4869.66609313
-     5201.99745378 5429.02043984 5567.28370644 5659.24831854 5695.06577065
-     5783.46217755 5824.01080702 5825.59003768 5818.22316662 5866.52307412
-     5896.96917375 5948.92254787 5968.30540534 5931.15611704 5941.94457249
-     5942.06535486]
+    [ 130.57472018  501.34744442 1066.59182074 1760.50163608 2502.13955554
+     3218.50667597 3892.81448231 4455.36403436 4869.66609313 5201.99745378
+     5429.02043984 5567.28370644 5659.24831854 5695.06577065 5783.46217755
+     5824.01080702 5825.59003768 5818.22316662 5866.52307412 5896.96917375
+     5948.92254787 5968.30540534 5931.15611704 5941.94457249 5942.06535486]
 
     >>> print(cog.profile_error)  # doctest: +FLOAT_CMP
-    [  0.           5.32777186   9.37111012  13.41750992  16.62928904
-      21.7350922   25.39862532  30.3867526   34.11478867  39.28263973
-      43.96047829  48.11931395  52.00967328  55.7471834   60.48824739
-      64.81392778  68.71042311  72.71899201  76.54959872  81.33806741
-      85.98568713  91.34841248  95.5173253   99.22190499 102.51980185
-     106.83601366]
+    [  5.32777186   9.37111012  13.41750992  16.62928904  21.7350922
+      25.39862532  30.3867526   34.11478867  39.28263973  43.96047829
+      48.11931395  52.00967328  55.7471834   60.48824739  64.81392778
+      68.71042311  72.71899201  76.54959872  81.33806741  85.98568713
+      91.34841248  95.5173253   99.22190499 102.51980185 106.83601366]
 
     Plot the curve of growth.
 
@@ -151,11 +142,8 @@ class CurveOfGrowth(ProfileBase):
         xycen = centroid_quadratic(data, xpeak=48, ypeak=52)
 
         # create the curve of growth
-        min_radius = 0.0
-        max_radius = 25.0
-        radius_step = 1.0
-        cog = CurveOfGrowth(data, xycen, min_radius, max_radius, radius_step,
-                            error=error, mask=None)
+        radii = np.arange(1, 26)
+        cog = CurveOfGrowth(data, xycen, radii, error=error, mask=None)
 
         # plot the curve of growth
         cog.plot()
@@ -185,11 +173,8 @@ class CurveOfGrowth(ProfileBase):
         xycen = centroid_quadratic(data, xpeak=48, ypeak=52)
 
         # create the curve of growth
-        min_radius = 0.0
-        max_radius = 25.0
-        radius_step = 1.0
-        cog = CurveOfGrowth(data, xycen, min_radius, max_radius, radius_step,
-                            error=error, mask=None)
+        radii = np.arange(1, 26)
+        cog = CurveOfGrowth(data, xycen, radii, error=error, mask=None)
 
         # plot the curve of growth
         cog.normalize()
@@ -220,11 +205,8 @@ class CurveOfGrowth(ProfileBase):
         xycen = centroid_quadratic(data, xpeak=48, ypeak=52)
 
         # create the curve of growth
-        min_radius = 0.0
-        max_radius = 25.0
-        radius_step = 1.0
-        cog = CurveOfGrowth(data, xycen, min_radius, max_radius, radius_step,
-                            error=error, mask=None)
+        radii = np.arange(1, 26)
+        cog = CurveOfGrowth(data, xycen, radii, error=error, mask=None)
 
         norm = simple_norm(data, 'sqrt')
         plt.figure(figsize=(5, 5))
@@ -233,9 +215,15 @@ class CurveOfGrowth(ProfileBase):
         cog.apertures[10].plot(color='C1', lw=2)
     """
 
-    @lazyproperty
-    def _circular_radii(self):
-        return self.radius
+    def __init__(self, data, xycen, radii, *, error=None, mask=None,
+                 method='exact', subpixels=5):
+
+        radii = np.array(radii)
+        if radii.min() <= 0:
+            raise ValueError('radii must be > 0')
+
+        super().__init__(data, xycen, radii, error=error, mask=mask,
+                         method=method, subpixels=subpixels)
 
     @lazyproperty
     def apertures(self):
