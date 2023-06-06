2e1a7eb9e3baaaaf598b123569f772bf0c3c790d
Larry Bradley <larry.bradley@gmail.com>
2023-06-06 13:40:56 -0400

    Put fit_info derived attributes in source order

--- photutils/psf/photometry.py
+++ photutils/psf/photometry.py
@@ -47,9 +47,12 @@ class PSFPhotometry:
 
         self.finder_results = []
         self.fit_error_indices = []
-        self.fit_info = []
-        self._fitted_group_models = None
-        self._fitted_models = None
+        self._fit_group_models = []
+        self.fit_models = []
+        self.fit_infos = []
+        self._fit_param_err = []
+        self._fit_nsources = []
+        self._ungroup_indices = []
 
     @staticmethod
     def _validate_model(psf_model):
@@ -91,6 +94,22 @@ class PSFPhotometry:
         return array
 
     @staticmethod
+    def _validate_params(init_params):
+        if init_params is None:
+            return init_params
+
+        if not isinstance(init_params, Table):
+            raise TypeError('init_params must be an astropy Table')
+
+        columns = ('x_init', 'y_init')
+        for column in columns:
+            if column not in init_params.columns:
+                raise ValueError(f'{column!r} must be a column in '
+                                 'init_params')
+
+        return init_params.copy()
+
+    @staticmethod
     def _make_mask(image, mask):
         def warn_nonfinite():
             warnings.warn('Input data contains unmasked non-finite values '
@@ -112,21 +131,17 @@ class PSFPhotometry:
 
         return mask
 
-    @staticmethod
-    def _validate_params(init_params):
-        if init_params is None:
-            return init_params
+    def _add_progress_bar(self, iterable, desc=None):
+        if self.progress_bar and HAS_TQDM:
+            try:
+                from ipywidgets import FloatProgress  # noqa: F401
+                from tqdm.auto import tqdm
+            except ImportError:
+                from tqdm import tqdm
 
-        if not isinstance(init_params, Table):
-            raise TypeError('init_params must be an astropy Table')
+            iterable = tqdm(iterable, desc=desc)  # pragma: no cover
 
-        columns = ('x_init', 'y_init')
-        for column in columns:
-            if column not in init_params.columns:
-                raise ValueError(f'{column!r} must be a column in '
-                                 'init_params')
-
-        return init_params.copy()
+        return iterable
 
     def _get_aper_fluxes(self, data, mask, init_params):
         # TODO: flexible input column names
@@ -152,18 +167,6 @@ class PSFPhotometry:
 
         return init_params
 
-    def _add_progress_bar(self, iterable, desc=None):
-        if self.progress_bar and HAS_TQDM:
-            try:
-                from ipywidgets import FloatProgress  # noqa: F401
-                from tqdm.auto import tqdm
-            except ImportError:
-                from tqdm import tqdm
-
-            iterable = tqdm(iterable, desc=desc)  # pragma: no cover
-
-        return iterable
-
     def _param_map(self):
         # TODO: generalize this mapping based of self.psf_model
         param_map = {}
@@ -183,13 +186,13 @@ class PSFPhotometry:
         """
         param_map = self._param_map()[0]
 
-        for i, source in enumerate(sources):
+        for index, source in enumerate(sources):
             model = self.psf_model.copy()
             for param, model_param in param_map.items():
                 setattr(model, model_param, source[param])
                 model.name = source['id']
 
-            if i == 0:
+            if index == 0:
                 psf_model = model
             else:
                 psf_model += model
@@ -228,44 +231,49 @@ class PSFPhotometry:
 
         return yi, xi
 
-    def _set_fit_info_attrs(self, fit_info, nsources):
-        if nsources == 1:  # number of sources in the group
-            self.fit_info.append(fit_info)
-        else:
-            # fit_info list has an item for each source
-            fit_info = [fit_info] * nsources
-            self.fit_info.extend(fit_info)
-
-        for idx, finfo in enumerate(self.fit_info):
-            ierr = finfo.get('ierr', None)
-            if ierr not in (1, 2, 3, 4):  # all good flags defined by scipy
-                self.fit_error_indices.append(idx)
-
-        # TODO: set param_errs attribute
-
     @staticmethod
     def _split_compound_model(model, chunk_size):
         for i in range(0, model.n_submodels, chunk_size):
             yield model[i: i + chunk_size]
 
-    def _split_grouped_models(self, models):
+    def _split_groups(self, models, infos):
         psf_nsub = self.psf_model.n_submodels
 
-        ungrouped_models = []
-        for model in models:
+        fit_models = []
+        fit_infos = []
+        for model, info in zip(models, infos):
             model_nsub = model.n_submodels
 
+            # model is for a single source (which may be compound)
             if model_nsub == psf_nsub:
-                # model for a single star (which may be compound)
-                ungrouped_models.append(model)
+                fit_models.append(model)
+                fit_infos.append(info)
+                self._fit_nsources.append(1)
                 continue
 
-            # model is a grouped model for multiple stars
-            ungrouped_models.extend(self._split_compound_model(model,
-                                                               psf_nsub))
+            # model is a grouped model for multiple sources
+            fit_models.extend(self._split_compound_model(model, psf_nsub))
+            nsources = model_nsub // psf_nsub
+            self._fit_nsources.extend([nsources] * nsources)
+            fit_infos.extend([info] * nsources)  # views
 
-        # sorted back into original source order
-        return sorted(ungrouped_models, key=lambda model: model.name)
+        if len(fit_models) != len(fit_infos):
+            raise ValueError('fit_models and fit_infos have different lengths')
+
+        self._fit_nsources = np.array(self._fit_nsources, dtype=int)
+        self._fit_nsources = self._fit_nsources[self._ungroup_indices]
+        z1 = [fit_models[i] for i in self._ungroup_indices]
+        z2 = [fit_infos[i] for i in self._ungroup_indices]
+
+        # now change the sorting from group_id to source id order
+        # the model.name attribute stores the source index
+        fit_models, fit_infos = zip(*sorted(zip(fit_models, fit_infos),
+                                            key=lambda pair: pair[0].name))
+
+        assert z1 == list(fit_models)
+        assert z2 == list(fit_infos)
+
+        return list(fit_models), list(fit_infos)
 
     def _fit_sources(self, data, init_params, *, error=None, mask=None):
         if self.maxiters is not None:
@@ -273,36 +281,68 @@ class PSFPhotometry:
         else:
             kwargs = {}
 
-        sources = init_params.group_by('group_id').groups
-        sources = self._add_progress_bar(sources, desc='Fit star/group')
+        sources = init_params.group_by('group_id')
+        self._ungroup_indices = np.argsort(sources['id'].value)
+        sources = sources.groups
+        sources = self._add_progress_bar(sources, desc='Fit source/group')
 
-        fitted_models = []
-        for sources_ in sources:
+        fit_models = []
+        fit_infos = []
+        for sources_ in sources:  # fit in group_id order
             psf_model = self._make_psf_model(sources_)
             yi, xi = self._define_fit_coords(sources_, data.shape, mask)
             cutout = data[yi, xi]
 
             if cutout.shape == (0,):
-                result = psf_model  # no fit was performed (initial model)
+                fit_model = psf_model  # no fit was performed (initial model)
                 fit_info = {'message': 'Source was not fit because it was '
                             'completely masked'}
             else:
                 if error is not None:
-                    weights = 1 / error[yi, xi]
+                    weights = 1.0 / error[yi, xi]
                 else:
                     weights = None
 
                 with warnings.catch_warnings():
                     warnings.simplefilter('ignore', AstropyUserWarning)
-                    result = self.fitter(psf_model, xi, yi, cutout,
-                                         weights=weights, **kwargs)
+                    fit_model = self.fitter(psf_model, xi, yi, cutout,
+                                            weights=weights, **kwargs)
                     fit_info = self.fitter.fit_info.copy()
 
-            fitted_models.append(result)
+            fit_models.append(fit_model)
+            fit_infos.append(fit_info)
 
-            self._set_fit_info_attrs(fit_info, len(sources_))
+        # split the groups and return objects in source-id order
+        self._fit_group_models = fit_models
+        fit_models, fit_infos = self._split_groups(fit_models, fit_infos)
+        self.fit_models = fit_models
+        self.fit_infos = fit_infos
 
-        return fitted_models
+        return fit_models
+
+    def _set_fit_info_attrs(self):
+        for index, fit_info in enumerate(self.fit_infos):
+            ierr = fit_info.get('ierr', None)
+            if ierr not in (1, 2, 3, 4):  # all good flags defined by scipy
+                self.fit_error_indices.append(index)
+
+            param_cov = fit_info.get('param_cov', None)
+            nparam = 3
+            if param_cov is None:
+                param_err = np.array([np.nan] * nparam)
+            else:
+                param_err = np.sqrt(np.diag(param_cov))
+
+            if self._fit_nsources[index] == 1:
+                self._fit_param_err.append(param_err)
+            else:
+                param_errs = []
+                for i in range(0, len(param_err),  nparam):
+                    param_errs.append(param_err[i:i + nparam])
+                self._fit_param_err.extend(param_errs)
+
+        self._fit_param_err = np.array(self._fit_param_err)
+        self.fit_error_indices = np.array(self.fit_error_indices, dtype=int)
 
     def _model_params_to_table(self, models):
         param_map = self._param_map()[1]
@@ -317,65 +357,17 @@ class PSFPhotometry:
 
         colnames = param_map.values()
         table = QTable()
-        for i, colname in enumerate(colnames):
-            table[colname] = vals[i]
+        for index, colname in enumerate(colnames):
+            table[colname] = vals[index]
 
         return table
 
-    def _get_psf_param_names(self):
-        """
-        Get the names of the PSF model parameters corresponding to x, y,
-        and flux.
-
-        The PSF model must either define 'xname', 'yname', and
-        'fluxname' attributes or have parameters called 'x_0', 'y_0',
-        and 'flux'. Otherwise, a `ValueError` is raised.
-        """
-        keys = [('xname', 'x_0'), ('yname', 'y_0'), ('fluxname', 'flux')]
-        names = []
-        for key in keys:
-            try:
-                name = getattr(self.psf_model, key[0])
-            except AttributeError:
-                if key[1] in self.psf_model.param_names:
-                    name = key[1]
-                else:
-                    raise ValueError('Could not find PSF parameter names')
-
-            names.append(name)
-
-        return tuple(names)
-
-    def make_model_image(self, shape, psf_shape):
-        fit_models = self._fitted_models
-
-        data = np.zeros(shape)
-        xname, yname, _ = self._get_psf_param_names()
-
-        desc = 'Model image'
-        fit_models = self._add_progress_bar(fit_models, desc=desc)
-
-        # fit_models must be a list of individual, not grouped, PSF
-        # models, i.e., there should be one PSF model (which may be
-        # compound) for each source
-        for fit_model in fit_models:
-            x0 = getattr(fit_model, xname).value
-            y0 = getattr(fit_model, yname).value
-            slc_lg, _ = overlap_slices(shape, psf_shape, (y0, x0), mode='trim')
-            yy, xx = np.mgrid[slc_lg]
-            data[slc_lg] += fit_model(xx, yy)
-
-        return data
-
-    def make_residual_image(self, data, psf_shape):
-        return data - self.make_model_image(data.shape, psf_shape)
-
     def _define_flags(self):
-        flags = np.zeros(len(self.fit_info), dtype=int)
+        flags = np.zeros(len(self.fit_infos), dtype=int)
         flags[self.fit_error_indices] = 1
 
         idx = []
-        for index, fit_info in enumerate(self.fit_info):
+        for index, fit_info in enumerate(self.fit_infos):
             if 'completely masked' in fit_info['message']:
                 idx.append(index)
         flags[idx] = 2
@@ -430,6 +422,7 @@ class PSFPhotometry:
             # init_params['group_id'] = self.grouper(init_params)
             init_params = self.grouper(init_params)
 
+        # no grouping
         if 'group_id' not in init_params.colnames:
             init_params['group_id'] = init_params['id']
 
@@ -437,25 +430,73 @@ class PSFPhotometry:
         colnames = ('id', 'group_id', 'x_init', 'y_init', 'flux_init')
         init_params = init_params[colnames]
 
-        fitted_models = self._fit_sources(data, init_params, error=error,
-                                          mask=mask)
-        self._fitted_group_models = fitted_models
-        ungrouped_models = self._split_grouped_models(fitted_models)
-        self._fitted_models = ungrouped_models
+        fit_models = self._fit_sources(data, init_params, error=error,
+                                       mask=mask)
+
+        self._set_fit_info_attrs()
 
         # create output table
-        sources_fit = self._model_params_to_table(ungrouped_models)
-        if len(init_params) != len(sources_fit):
-            raise ValueError('init_params and fit sources tables have '
+        fit_sources = self._model_params_to_table(fit_models)  # ungrouped
+        if len(init_params) != len(fit_sources):
+            raise ValueError('init_params and fit_sources tables have '
                              'different lengths')
-        source_tbl = hstack((init_params, sources_fit))
+        source_tbl = hstack((init_params, fit_sources))
 
         source_tbl['flags'] = self._define_flags()
 
         if len(self.fit_error_indices) > 0:
             warnings.warn('One or more fit(s) may not have converged. Please '
                           'check the "flags" column in the output table, and '
-                          'the "fit_error_indices" and "fit_info" attributes '
+                          'the "fit_error_indices" and "fit_infos" attributes '
                           'for more information.', AstropyUserWarning)
 
         return source_tbl
+
+    def _get_psf_param_names(self):
+        """
+        Get the names of the PSF model parameters corresponding to x, y,
+        and flux.
+
+        The PSF model must either define 'xname', 'yname', and
+        'fluxname' attributes or have parameters called 'x_0', 'y_0',
+        and 'flux'. Otherwise, a `ValueError` is raised.
+        """
+        keys = [('xname', 'x_0'), ('yname', 'y_0'), ('fluxname', 'flux')]
+        names = []
+        for key in keys:
+            try:
+                name = getattr(self.psf_model, key[0])
+            except AttributeError as exc:
+                if key[1] in self.psf_model.param_names:
+                    name = key[1]
+                else:
+                    msg = 'Could not find PSF parameter names'
+                    raise ValueError(msg) from exc
+
+            names.append(name)
+
+        return tuple(names)
+
+    def make_model_image(self, shape, psf_shape):
+        fit_models = self.fit_models
+
+        data = np.zeros(shape)
+        xname, yname, _ = self._get_psf_param_names()
+
+        desc = 'Model image'
+        fit_models = self._add_progress_bar(fit_models, desc=desc)
+
+        # fit_models must be a list of individual, not grouped, PSF
+        # models, i.e., there should be one PSF model (which may be
+        # compound) for each source
+        for fit_model in fit_models:
+            x0 = getattr(fit_model, xname).value
+            y0 = getattr(fit_model, yname).value
+            slc_lg, _ = overlap_slices(shape, psf_shape, (y0, x0), mode='trim')
+            yy, xx = np.mgrid[slc_lg]
+            data[slc_lg] += fit_model(xx, yy)
+
+        return data
+
+    def make_residual_image(self, data, psf_shape):
+        return data - self.make_model_image(data.shape, psf_shape)
