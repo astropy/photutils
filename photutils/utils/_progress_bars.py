0d25ac663ce0ca9be20a0c21e6e0964947178e42
Larry Bradley <larry.bradley@gmail.com>
2023-06-28 13:18:10 -0400

    Add progress_bar function

---
+++ photutils/utils/_progress_bars.py
@@ -0,0 +1,36 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+This module provides tools for progress bars.
+"""
+
+# pylint: disable-next=E0611
+from photutils.utils._optional_deps import HAS_TQDM
+
+
+def add_progress_bar(iterable, desc=None):
+    """
+    Add a progress bar for an iterable.
+
+    Parameters
+    ----------
+    iterable : iterable
+        The iterable for which to add a progress bar.
+
+    Returns
+    -------
+    result : tqdm iterable
+        A tqdm progress bar. If in a notebook and ipywidgets is
+        installed, it will return a ipywidgets-based progress bar.
+        Otherwise it will return a text-based progress bar.
+    """
+    if HAS_TQDM:
+        try:  # pragma: no cover
+            # pylint: disable-next=W0611
+            from ipywidgets import FloatProgress  # noqa: F401
+            from tqdm.auto import tqdm
+        except ImportError:  # pragma: no cover
+            from tqdm import tqdm
+
+        iterable = tqdm(iterable, desc=desc)  # pragma: no cover
+
+    return iterable
