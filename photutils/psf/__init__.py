0ae156e445c2f215ba7165052760e2587363630b
Larry Bradley <larry.bradley@gmail.com>
2023-07-18 19:20:54 -0400

    Move GriddedPSFModel to new module

--- photutils/psf/__init__.py
+++ photutils/psf/__init__.py
@@ -4,10 +4,11 @@ This subpackage contains tools to perform point-spread-function (PSF)
 photometry.
 """
 
-from . import (epsf, epsf_stars, groupers, groupstars, models, photometry,
-               photometry_depr, utils)
+from . import (epsf, epsf_stars, griddedpsfmodel, groupers, groupstars,
+               models, photometry, photometry_depr, utils)
 from .epsf import *  # noqa: F401, F403
 from .epsf_stars import *  # noqa: F401, F403
+from .griddedpsfmodel import *  # noqa: F401, F403
 from .groupers import *  # noqa: F401, F403
 from .groupstars import *  # noqa: F401, F403
 from .matching import *  # noqa: F401, F403
@@ -20,6 +21,7 @@ from .utils import *  # noqa: F401, F403
 __all__ = []
 __all__.extend(epsf.__all__)
 __all__.extend(epsf_stars.__all__)
+__all__.extend(griddedpsfmodel.__all__)
 __all__.extend(groupers.__all__)
 __all__.extend(groupstars.__all__)
 __all__.extend(models.__all__)
--- photutils/psf/tests/test_models.py
+++ photutils/psf/tests/test_models.py
@@ -11,8 +11,8 @@ from astropy.modeling.models import Gaussian2D, Moffat2D
 from astropy.nddata import NDData
 from numpy.testing import assert_allclose
 
-from photutils.psf.models import (FittableImageModel, GriddedPSFModel,
-                                  IntegratedGaussianPRF, PRFAdapter)
+from photutils.psf import (FittableImageModel, GriddedPSFModel,
+                           IntegratedGaussianPRF, PRFAdapter)
 from photutils.segmentation import SourceCatalog, detect_sources
 from photutils.utils._optional_deps import HAS_SCIPY
 
