054572b7ae342f8d71d494157f19309fa4bf799c
Larry Bradley <larry.bradley@gmail.com>
2023-07-31 18:03:34 -0400

    Disable ignoring linkcheck anchors

--- docs/conf.py
+++ docs/conf.py
@@ -185,4 +185,3 @@ linkcheck_ignore = ['http://data.astropy.org',
                     r'https://iraf.net/*',
                     r'https://github\.com/astropy/photutils/(?:issues|pull)/\d+']
 linkcheck_timeout = 180
-linkcheck_anchors = False
