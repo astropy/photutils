054cec54ce8ecda6f971896b6d5b7b296b677a91
Larry Bradley <larry.bradley@gmail.com>
2023-11-03 21:46:38 -0400

    pre-commit autoupdate

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-from setuptools import setup  # noqa: E402
+from setuptools import setup
 
-from extension_helpers import get_extensions  # noqa: E402
+from extension_helpers import get_extensions
 
 setup(ext_modules=get_extensions())
