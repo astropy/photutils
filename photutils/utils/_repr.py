da457cc0b4a4a002a59703ba331426ffe4f4b846
Larry Bradley <larry.bradley@gmail.com>
2024-02-13 18:44:00 -0500

    Add make_repr utility function

---
+++ photutils/utils/_repr.py
@@ -0,0 +1,33 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+This module provides tools for class __repr__ and __str__ strings.
+"""
+
+
+def make_repr(self, params, ellipsis=(), long=False):
+    cls_name = f'{self.__class__.__name__}'
+    if long:
+        cls_name = f'{self.__class__.__module__}.{cls_name}'
+
+    cls_info = []
+    for param in params:
+        if param in ellipsis:
+            value = '...'
+        else:
+            value = getattr(self, param)
+        cls_info.append((param, value))
+
+    if long:
+        delim = ': '
+        join_str = '\n'
+    else:
+        delim = '='
+        join_str = ', '
+
+    fmt = [f'{key}{delim}{val!r}' for key, val in cls_info]
+    fmt = f'{join_str}'.join(fmt)
+
+    if long:
+        return f'<{cls_name}>\n{fmt}'
+    else:
+        return f'{cls_name}({fmt})'
