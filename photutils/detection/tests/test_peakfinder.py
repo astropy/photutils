9b84b584a96f63c4f395910ebc2e17cf9c2f8c42
Larry Bradley <larry.bradley@gmail.com>
2024-05-21 14:49:43 -0400

    Update find_peak tests to use fixture data

--- photutils/detection/tests/test_peakfinder.py
+++ photutils/detection/tests/test_peakfinder.py
@@ -10,31 +10,30 @@ from astropy.tests.helper import assert_quantity_allclose
 from numpy.testing import assert_array_equal, assert_equal
 
 from photutils.centroids import centroid_com
-from photutils.datasets import make_4gaussians_image, make_gwcs, make_wcs
-from photutils.detection.peakfinder import find_peaks
+from photutils.datasets import make_gwcs, make_wcs
+from photutils.detection import find_peaks
 from photutils.utils._optional_deps import HAS_GWCS, HAS_SCIPY
 from photutils.utils.exceptions import NoDetectionsWarning
-
-PEAKDATA = np.array([[1, 0, 0], [0, 0, 0], [0, 0, 1]]).astype(float)
-PEAKREF1 = np.array([[0, 0], [2, 2]])
-
-IMAGE = make_4gaussians_image()
-FITSWCS = make_wcs(IMAGE.shape)
 
 
 @pytest.mark.skipif(not HAS_SCIPY, reason='scipy is required')
 class TestFindPeaks:
-    def test_box_size(self):
+    def test_box_size(self, data):
         """Test with box_size."""
-        tbl = find_peaks(PEAKDATA, 0.1, box_size=3)
+        tbl = find_peaks(data, 0.1, box_size=3)
         assert tbl['id'][0] == 1
-        assert_equal(tbl['x_peak'], PEAKREF1[:, 1])
-        assert_equal(tbl['y_peak'], PEAKREF1[:, 0])
-        assert_equal(tbl['peak_value'], [1.0, 1.0])
+        assert len(tbl) == 25
+        columns = ['id', 'x_peak', 'y_peak', 'peak_value']
+        assert all(column in tbl.colnames for column in columns)
+        assert np.min(tbl['x_peak']) > 0
+        assert np.max(tbl['x_peak']) < 101
+        assert np.min(tbl['y_peak']) > 0
+        assert np.max(tbl['y_peak']) < 101
+        assert np.max(tbl['peak_value']) < 13.1
 
         # test with units
         unit = u.Jy
-        tbl2 = find_peaks(PEAKDATA << unit, 0.1 << unit, box_size=3)
+        tbl2 = find_peaks(data << unit, 0.1 << unit, box_size=3)
         columns = ['id', 'x_peak', 'y_peak']
         for column in columns:
             assert_equal(tbl[column], tbl2[column])
@@ -42,62 +41,58 @@ class TestFindPeaks:
         assert tbl2[col].unit == unit
         assert_equal(tbl[col], tbl2[col].value)
 
-    def test_footprint(self):
+    def test_footprint(self, data):
         """Test with footprint."""
-        tbl = find_peaks(PEAKDATA, 0.1, footprint=np.ones((3, 3)))
-        assert_equal(tbl['x_peak'], PEAKREF1[:, 1])
-        assert_equal(tbl['y_peak'], PEAKREF1[:, 0])
-        assert_equal(tbl['peak_value'], [1.0, 1.0])
+        tbl0 = find_peaks(data, 0.1, box_size=3)
+        tbl1 = find_peaks(data, 0.1, footprint=np.ones((3, 3)))
+        assert_array_equal(tbl0, tbl1)
 
-    def test_mask(self):
+    def test_mask(self, data):
         """Test with mask."""
-        mask = np.zeros(PEAKDATA.shape, dtype=bool)
-        mask[0, 0] = True
-        tbl = find_peaks(PEAKDATA, 0.1, box_size=3, mask=mask)
-        assert len(tbl) == 1
-        assert_equal(tbl['x_peak'], PEAKREF1[1, 0])
-        assert_equal(tbl['y_peak'], PEAKREF1[1, 1])
-        assert_equal(tbl['peak_value'], 1.0)
+        mask = np.zeros(data.shape, dtype=bool)
+        mask[0:50, :] = True
+        tbl0 = find_peaks(data, 0.1, box_size=3)
+        tbl1 = find_peaks(data, 0.1, box_size=3, mask=mask)
+        assert len(tbl1) < len(tbl0)
 
-    def test_maskshape(self):
+    def test_maskshape(self, data):
         """Test if make shape doesn't match data shape."""
         with pytest.raises(ValueError):
-            find_peaks(PEAKDATA, 0.1, mask=np.ones((5, 5)))
+            find_peaks(data, 0.1, mask=np.ones((5, 5)))
 
-    def test_thresholdshape(self):
+    def test_thresholdshape(self, data):
         """Test if threshold shape doesn't match data shape."""
         with pytest.raises(ValueError):
-            find_peaks(PEAKDATA, np.ones((2, 2)))
+            find_peaks(data, np.ones((2, 2)))
 
-    def test_npeaks(self):
+    def test_npeaks(self, data):
         """Test npeaks."""
-        tbl = find_peaks(PEAKDATA, 0.1, box_size=3, npeaks=1)
-        assert_equal(tbl['x_peak'], PEAKREF1[1, 1])
-        assert_equal(tbl['y_peak'], PEAKREF1[1, 0])
+        tbl = find_peaks(data, 0.1, box_size=3, npeaks=1)
+        assert len(tbl) == 1
 
-    def test_border_width(self):
+    def test_border_width(self, data):
         """Test border exclusion."""
-        with pytest.warns(NoDetectionsWarning,
-                          match='No local peaks were found'):
-            tbl = find_peaks(PEAKDATA, 0.1, box_size=3, border_width=3)
-            assert tbl is None
+        tbl0 = find_peaks(data, 0.1, box_size=3)
+        tbl1 = find_peaks(data, 0.1, box_size=3, border_width=25)
+        assert len(tbl1) < len(tbl0)
 
-    def test_box_size_int(self):
+    def test_box_size_int(self, data):
         """Test non-integer box_size."""
-        tbl1 = find_peaks(PEAKDATA, 0.1, box_size=5.0)
-        tbl2 = find_peaks(PEAKDATA, 0.1, box_size=5.5)
+        tbl1 = find_peaks(data, 0.1, box_size=5.0)
+        tbl2 = find_peaks(data, 0.1, box_size=5.5)
         assert_array_equal(tbl1, tbl2)
 
-    def test_centroid_func_callable(self):
+    def test_centroid_func_callable(self, data):
         """Test that centroid_func is callable."""
         with pytest.raises(TypeError):
-            find_peaks(PEAKDATA, 0.1, box_size=2, centroid_func=True)
+            find_peaks(data, 0.1, box_size=2, centroid_func=True)
 
-    def test_wcs(self):
+    def test_wcs(self, data):
         """Test with astropy WCS."""
         columns = ['skycoord_peak', 'skycoord_centroid']
 
-        tbl = find_peaks(IMAGE, 100, wcs=FITSWCS, centroid_func=centroid_com)
+        fits_wcs = make_wcs(data.shape)
+        tbl = find_peaks(data, 1, wcs=fits_wcs, centroid_func=centroid_com)
         for column in columns:
             assert column in tbl.colnames
         assert tbl.colnames == ['id', 'x_peak', 'y_peak', 'skycoord_peak',
@@ -105,21 +100,21 @@ class TestFindPeaks:
                                 'skycoord_centroid']
 
     @pytest.mark.skipif(not HAS_GWCS, reason='gwcs is required')
-    def test_gwcs(self):
+    def test_gwcs(self, data):
         """Test with gwcs."""
         columns = ['skycoord_peak', 'skycoord_centroid']
 
-        gwcs_obj = make_gwcs(IMAGE.shape)
-        tbl = find_peaks(IMAGE, 100, wcs=gwcs_obj, centroid_func=centroid_com)
+        gwcs_obj = make_gwcs(data.shape)
+        tbl = find_peaks(data, 1, wcs=gwcs_obj, centroid_func=centroid_com)
         for column in columns:
             assert column in tbl.colnames
 
     @pytest.mark.skipif(not HAS_GWCS, reason='gwcs is required')
-    def test_wcs_values(self):
-        gwcs_obj = make_gwcs(IMAGE.shape)
-        tbl1 = find_peaks(IMAGE, 100, wcs=FITSWCS, centroid_func=centroid_com)
-        tbl2 = find_peaks(IMAGE, 100, wcs=gwcs_obj,
-                          centroid_func=centroid_com)
+    def test_wcs_values(self, data):
+        fits_wcs = make_wcs(data.shape)
+        gwcs_obj = make_gwcs(data.shape)
+        tbl1 = find_peaks(data, 1, wcs=fits_wcs, centroid_func=centroid_com)
+        tbl2 = find_peaks(data, 1, wcs=gwcs_obj, centroid_func=centroid_com)
         columns = ['skycoord_peak', 'skycoord_centroid']
         for column in columns:
             assert_quantity_allclose(tbl1[column].ra, tbl2[column].ra)
@@ -128,34 +123,37 @@ class TestFindPeaks:
     def test_constant_array(self):
         """Test for empty output table when data is constant."""
         data = np.ones((10, 10))
-        with pytest.warns(NoDetectionsWarning, match='Input data is constant'):
+        match = 'Input data is constant'
+        with pytest.warns(NoDetectionsWarning, match=match):
             tbl = find_peaks(data, 0.0)
             assert tbl is None
 
-    def test_no_peaks(self):
+    def test_no_peaks(self, data):
         """
         Tests for when no peaks are found.
         """
-        with pytest.warns(NoDetectionsWarning,
-                          match='No local peaks were found'):
-            tbl = find_peaks(IMAGE, 10000)
+        fits_wcs = make_wcs(data.shape)
+
+        match = 'No local peaks were found'
+        with pytest.warns(NoDetectionsWarning, match=match):
+            tbl = find_peaks(data, 10000)
             assert tbl is None
-        with pytest.warns(NoDetectionsWarning,
-                          match='No local peaks were found'):
-            tbl = find_peaks(IMAGE, 100000, centroid_func=centroid_com)
+
+        with pytest.warns(NoDetectionsWarning, match=match):
+            tbl = find_peaks(data, 100000, centroid_func=centroid_com)
             assert tbl is None
-        with pytest.warns(NoDetectionsWarning,
-                          match='No local peaks were found'):
-            tbl = find_peaks(IMAGE, 100000, wcs=FITSWCS)
+
+        with pytest.warns(NoDetectionsWarning, match=match):
+            tbl = find_peaks(data, 100000, wcs=fits_wcs)
             assert tbl is None
-        with pytest.warns(NoDetectionsWarning,
-                          match='No local peaks were found'):
-            tbl = find_peaks(IMAGE, 100000, wcs=FITSWCS,
+
+        with pytest.warns(NoDetectionsWarning, match=match):
+            tbl = find_peaks(data, 100000, wcs=fits_wcs,
                              centroid_func=centroid_com)
             assert tbl is None
 
-    def test_data_nans(self):
+    def test_data_nans(self, data):
         """Test that data with NaNs does not issue Runtime warning."""
-        data = np.copy(PEAKDATA)
-        data[1, 1] = np.nan
-        find_peaks(data, 0.0)
+        data = np.copy(data)
+        data[50:, :] = np.nan
+        find_peaks(data, 0.1)
