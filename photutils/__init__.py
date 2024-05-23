272b86d8632c6e38c953e97e17805c8c29ec699c
Larry Bradley <larry.bradley@gmail.com>
2024-05-22 20:42:29 -0400

    Update package docstring

--- photutils/__init__.py
+++ photutils/__init__.py
@@ -1,9 +1,10 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
 Photutils is an Astropy affiliated package to provide tools for
-detecting and performing photometry of astronomical sources.  It also
-has tools for background estimation, ePSF building, PSF matching,
-centroiding, and morphological measurements.
+detecting and performing photometry of astronomical sources. It also has
+tools for background estimation, ePSF building, PSF matching, radial
+profiles, centroiding, and morphological measurements.
+
 """
 
 import warnings
