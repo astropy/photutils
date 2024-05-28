c1142ec7053a1f0f045ef7f18344f349888a431a
Larry Bradley <larry.bradley@gmail.com>
2024-05-28 17:34:33 -0400

    Update test images

--- photutils/datasets/images.py
+++ photutils/datasets/images.py
@@ -725,16 +725,19 @@ def make_4gaussians_image(noise=True):
         image = make_4gaussians_image()
         plt.imshow(image, origin='lower', interpolation='nearest')
     """
-    table = QTable()
-    table['amplitude'] = [50, 70, 150, 210]
-    table['x_mean'] = [160, 25, 150, 90]
-    table['y_mean'] = [70, 40, 25, 60]
-    table['x_stddev'] = [15.2, 5.1, 3.0, 8.1]
-    table['y_stddev'] = [2.6, 2.5, 3.0, 4.7]
-    table['theta'] = np.radians(np.array([145.0, 20.0, 0.0, 60.0]))
+    params = QTable()
+    params['amplitude'] = [50, 70, 150, 210]
+    params['x_mean'] = [160, 25, 150, 90]
+    params['y_mean'] = [70, 40, 25, 60]
+    params['x_stddev'] = [15.2, 5.1, 3.0, 8.1]
+    params['y_stddev'] = [2.6, 2.5, 3.0, 4.7]
+    params['theta'] = np.radians(np.array([145.0, 20.0, 0.0, 60.0]))
 
     shape = (100, 200)
-    data = make_gaussian_sources_image(shape, table) + 5.0
+    model = models.Gaussian2D()
+    data = make_model_image(shape, model, params, xname='x_mean', yname='y_mean')
+
+    data += 5.0  # background
 
     if noise:
         rng = np.random.RandomState(12345)
@@ -804,7 +807,10 @@ def make_100gaussians_image(noise=True):
     sources['amplitude'] = sources['flux'] / (2.0 * np.pi * xstd * ystd)
 
     shape = (300, 500)
-    data = make_gaussian_sources_image(shape, sources) + 5.0
+    model = models.Gaussian2D()
+    data = make_model_image(shape, model, sources, bbox_factor=6.0, xname='x_mean',
+                            yname='y_mean')
+    data += 5.0  # background
 
     if noise:
         rng = np.random.RandomState(12345)
