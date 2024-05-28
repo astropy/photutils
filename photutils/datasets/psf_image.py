ad3508731acad2c7058fbe2d9910aaf92bf45170
Larry Bradley <larry.bradley@gmail.com>
2024-05-28 12:39:23 -0400

    Add docstrings

--- photutils/datasets/psf_image.py
+++ photutils/datasets/psf_image.py
@@ -16,7 +16,127 @@ __all__ = ['make_model_image']
 def make_model_image(shape, model, params_table, *, model_shape=None,
                      xname='x_0', yname='y_0', discretize_method='center',
                      discretize_oversample=10, progress_bar=False):
+    """
+    Make a 2D image containing sources generated from a user-specified
+    astropy 2D model.
 
+    The model parameters for each source are taken from the input
+    ``params_table`` table.
+
+    Parameters
+    ----------
+    shape : 2-tuple of int
+        The shape of the output image.
+
+    model : 2D `astropy.modeling.Model`
+        The 2D model to be used to render the sources. The model must
+        be two-dimensional where it accepts 2 inputs (i.e., (x, y)) and
+        has 1 output. The model must have parameters for the x and y
+        positions of the sources. Typically, these parameters are named
+        'x_0' and 'y_0', but the parameter names can be specified using
+        the ``xname`` and ``yname`` keywords.
+
+    params_table : `~astropy.table.Table`
+        A table containing the model parameters for each source. Each
+        row of the table corresponds to a source whose model parameters
+        are defined by the column names, which must match the model
+        parameter names. The table must contain columns for the x and
+        y positions of the sources. The column names for the x and y
+        positions can be specified using the ``xname`` and ``yname``
+        keywords. Model parameters not defined in the table will be set
+        to the ``model`` default value. Any units in the table will be
+        ignored.
+
+        If the table contains a column named 'model_shape', then
+        the values in that column will be used to override the
+        ``model_shape`` keyword and model ``bounding_box`` for each
+        source. This can be used to render each source with a different
+        shape.
+
+        If the table contains a column named 'local_bkg', then the
+        per-pixel local background values in that column will be used
+        to added to each model source over the region defined by its
+        ``model_shape``. This option should be used with care,
+        especially in crowded fields where the ``model_shape`` of
+        sources overlap (see Notes below).
+
+        Except for ``model_shape`` and ``local_bkg`` column names,
+        column names that do not match model parameters will be ignored.
+
+    model_shape : 2-tuple of int, int, or `None`, optional
+        The shape around the (x, y) center of each source that will
+        used to evaluate the ``model``. If ``model_shape`` is a scalar
+        integer, then a square shape of size ``model_shape`` will
+        be used. If `None`, then the bounding box of the model will
+        be used. This keyword must be specified if the model does
+        not have a ``bounding_box`` attribute. If specified, this
+        keyword overrides the model ``bounding_box`` attribute. To
+        use a different shape for each source, include a column named
+        ``'model_shape'`` in the ``params_table``. For that case, this
+        keyword is ignored.
+
+    xname : str, optional
+        The name of the ``model`` parameter that corresponds to the x
+        position of the sources. This parameter must also be a column
+        name in ``params_table``.
+
+    yname : str, optional
+        The name of the ``model`` parameter that corresponds to the y
+        position of the sources. This parameter must also be a column
+        name in ``params_table``.
+
+    discretize_method : {'center', 'interp', 'oversample', 'integrate'}, optional
+        One of the following methods for discretizing the model on the
+        pixel grid:
+
+            * ``'center'`` (default)
+                Discretize model by taking the value at the center of
+                the pixel bins. This method should be used for ePSF/PRF
+                single or gridded models.
+
+            * ``'interp'``
+                Discretize model by bilinearly interpolating between the
+                values at the corners of the pixel bins.
+
+            * ``'oversample'``
+                Discretize model by taking the average of model values
+                in the pixel bins on an oversampled grid. Use the
+                ``discretize_oversample`` keyword to set the integer
+                oversampling factor.
+
+            * ``'integrate'``
+                Discretize model by integrating the model over the pixel
+                bins using `scipy.integrate.quad`. This mode conserves
+                the model integral on a subpixel scale, but it is
+                *extremely* slow.
+
+    discretize_oversample : int, optional
+        The integer oversampling factor used when
+        ``descretize_method='oversample'``. This keyword is ignored
+        otherwise.
+
+    progress_bar : bool, optional
+        Whether to display a progress bar while adding the sources
+        to the image. The progress bar requires that the `tqdm
+        <https://tqdm.github.io/>`_ optional dependency be installed.
+        Note that the progress bar does not currently work in the
+        Jupyter console due to limitations in ``tqdm``.
+
+    Returns
+    -------
+    array : 2D `~numpy.ndarray`
+        The rendered image containing the model sources.
+
+    Notes
+    -----
+    The local background value around each source is optionally included
+    using the ``local_bkg`` column in the input ``params_table``. This
+    local background added to each source over its ``model_shape``
+    region. In regions where the ``model_shape`` of source overlap, the
+    local background will be added multiple times. This is not an issue
+    if the sources are well-separated, but for crowded fields, this
+    option should be used with care.
+    """
     if not isinstance(shape, tuple) or len(shape) != 2:
         raise ValueError('shape must be a 2-tuple')
 
