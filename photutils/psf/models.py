05d27b723b4a524877e43e58c61407363f630d46
Larry Bradley <larry.bradley@gmail.com>
2024-08-13 14:52:11 -0400

    Add GaussianPSF example

--- photutils/psf/models.py
+++ photutils/psf/models.py
@@ -97,6 +97,20 @@ class GaussianPSF(Fittable2DModel):
     References
     ----------
     .. [1] https://en.wikipedia.org/wiki/Gaussian_function
+
+    Examples
+    --------
+    .. plot::
+        :include-source:
+
+        import matplotlib.pyplot as plt
+        import numpy as np
+        from photutils.psf import GaussianPSF
+        model = GaussianPSF(flux=71.4, x_0=24.3, y_0=25.2, x_fwhm=10.1,
+                            y_fwhm=5.82, theta=21.7)
+        yy, xx = np.mgrid[0:51, 0:51]
+        data = model(xx, yy)
+        plt.imshow(data, origin='lower', interpolation='nearest')
     """
 
     flux = Parameter(
