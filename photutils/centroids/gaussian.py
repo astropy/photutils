9bf67ef1232e23ad6761f3eeaa907a54af6517e3
Larry Bradley <larry.bradley@gmail.com>
2024-08-26 14:22:05 -0400

    Add centroid_1dg example

--- photutils/centroids/gaussian.py
+++ photutils/centroids/gaussian.py
@@ -38,6 +38,34 @@ def centroid_1dg(data, error=None, mask=None):
     -------
     centroid : `~numpy.ndarray`
         The ``x, y`` coordinates of the centroid.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from photutils.datasets import make_4gaussians_image
+    >>> from photutils.centroids import centroid_1dg
+    >>> data = make_4gaussians_image()
+    >>> data -= np.median(data[0:30, 0:125])
+    >>> data = data[40:80, 70:110]
+    >>> x1, y1 = centroid_1dg(data)
+    >>> print(np.array((x1, y1)))
+    [19.96524237 20.04921073]
+
+    .. plot::
+
+        import matplotlib.pyplot as plt
+        import numpy as np
+        from photutils.centroids import centroid_1dg
+        from photutils.datasets import make_4gaussians_image
+
+        data = make_4gaussians_image()
+        data -= np.median(data[0:30, 0:125])
+        data = data[40:80, 70:110]
+        xycen = centroid_1dg(data)
+        fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+        ax.imshow(data, origin='lower', interpolation='nearest')
+        ax.scatter(*xycen, color='red', marker='+', s=100, label='Centroid')
+        ax.legend(loc='lower right')
     """
     data = np.ma.asanyarray(data)
 
