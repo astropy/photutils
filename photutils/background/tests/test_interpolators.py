ab9c02f9b27ea4d8add425177e38a4fba92e03f3
Larry Bradley <larry.bradley@gmail.com>
2024-09-06 14:47:15 -0400

    Update tests for background interpolators

--- photutils/background/tests/test_interpolators.py
+++ photutils/background/tests/test_interpolators.py
@@ -20,11 +20,32 @@ def test_zoom_interp():
                      [0.03, 0.03, 12.9]])
 
     interp = BkgZoomInterpolator(clip=False)
-    zoom = interp(mesh, bkg)
+    zoom = interp(mesh, **bkg._interp_kwargs)
     assert zoom.shape == (300, 300)
 
     cls_repr = repr(interp)
     assert cls_repr.startswith(f'{interp.__class__.__name__}')
+
+
+@pytest.mark.skipif(not HAS_SCIPY, reason='scipy is required')
+def test_zoom_interp_clip():
+    bkg = Background2D(np.ones((300, 300)), 100)
+    mesh = np.array([[0.01, 0.01, 0.02],
+                     [0.01, 0.02, 0.03],
+                     [0.03, 0.03, 12.9]])
+
+    interp1 = BkgZoomInterpolator(clip=False)
+    zoom1 = interp1(mesh, **bkg._interp_kwargs)
+
+    interp2 = BkgZoomInterpolator(clip=True)
+    zoom2 = interp2(mesh, **bkg._interp_kwargs)
+
+    minval = np.min(mesh)
+    maxval = np.max(mesh)
+    assert np.min(zoom1) < minval
+    assert np.max(zoom1) > maxval
+    assert np.min(zoom2) == minval
+    assert np.max(zoom2) == maxval
 
 
 @pytest.mark.skipif(not HAS_SCIPY, reason='scipy is required')
@@ -35,7 +56,7 @@ def test_idw_interp():
                      [0.03, 0.03, 12.9]])
 
     interp = BkgIDWInterpolator()
-    zoom = interp(mesh, bkg)
+    zoom = interp(mesh, **bkg._interp_kwargs)
     assert zoom.shape == (300, 300)
 
     cls_repr = repr(interp)
