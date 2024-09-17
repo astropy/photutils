47370ba3ba5b7f6dde2fa08927d9608f8bc951bc
Larry Bradley <larry.bradley@gmail.com>
2024-09-17 10:51:16 -0400

    Remove deprecations in datasets package

--- photutils/datasets/tests/test_sources.py
+++ photutils/datasets/tests/test_sources.py
@@ -6,11 +6,9 @@ Tests for the sources module.
 import numpy as np
 import pytest
 from astropy.table import Table
-from astropy.utils.exceptions import (AstropyDeprecationWarning,
-                                      AstropyUserWarning)
+from astropy.utils.exceptions import AstropyUserWarning
 
-from photutils.datasets import (make_model_params, make_random_gaussians_table,
-                                make_random_models_table)
+from photutils.datasets import make_model_params, make_random_models_table
 
 
 def test_make_model_params():
@@ -88,39 +86,3 @@ def test_make_random_models_table():
         assert col in source_table.colnames
         assert np.min(source_table[col]) >= param_ranges[col][0]
         assert np.max(source_table[col]) <= param_ranges[col][1]
-
-
-def test_make_random_gaussians_table():
-    with pytest.warns(AstropyDeprecationWarning):
-        n_sources = 5
-        param_ranges = {'amplitude': [500, 1000], 'x_mean': [0, 500],
-                        'y_mean': [0, 300], 'x_stddev': [1, 5],
-                        'y_stddev': [1, 5], 'theta': [0, np.pi]}
-
-        table = make_random_gaussians_table(n_sources, param_ranges, seed=0)
-        assert 'flux' in table.colnames
-        assert len(table) == n_sources
-
-
-def test_make_random_gaussians_table_no_stddev():
-    with pytest.warns(AstropyDeprecationWarning):
-        n_sources = 5
-        param_ranges = {'amplitude': [500, 1000], 'x_mean': [0, 500],
-                        'y_mean': [0, 300]}
-
-        table = make_random_gaussians_table(n_sources, param_ranges, seed=0)
-        assert 'flux' in table.colnames
-        assert len(table) == n_sources
-        assert 'x_stddev' not in table.colnames
-        assert 'y_stddev' not in table.colnames
-
-
-def test_make_random_gaussians_table_flux():
-    with pytest.warns(AstropyDeprecationWarning):
-        n_sources = 5
-        param_ranges = {'flux': [500, 1000], 'x_mean': [0, 500],
-                        'y_mean': [0, 300], 'x_stddev': [1, 5],
-                        'y_stddev': [1, 5], 'theta': [0, np.pi]}
-        table = make_random_gaussians_table(n_sources, param_ranges, seed=0)
-        assert 'amplitude' in table.colnames
-        assert len(table) == n_sources
