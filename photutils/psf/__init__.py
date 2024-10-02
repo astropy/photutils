5ae9e4c92df8b93f33ee7bbc669ad50b9782e355
Larry Bradley <larry.bradley@gmail.com>
2024-10-02 17:19:16 -0400

    Require imports from psf.matching

--- photutils/psf/__init__.py
+++ photutils/psf/__init__.py
@@ -4,34 +4,15 @@ This subpackage contains tools to perform point-spread-function (PSF)
 photometry.
 """
 
-from . import (epsf, epsf_stars, functional_models, gridded_models, groupers,
-               image_models, photometry, model_helpers, model_io,
-               model_plotting, simulation, utils)
 from .epsf import *  # noqa: F401, F403
 from .epsf_stars import *  # noqa: F401, F403
 from .functional_models import *  # noqa: F401, F403
 from .gridded_models import *  # noqa: F401, F403
 from .groupers import *  # noqa: F401, F403
 from .image_models import *  # noqa: F401, F403
-from .matching import *  # noqa: F401, F403
-from .photometry import *  # noqa: F401, F403
 from .model_helpers import *  # noqa: F401, F403
 from .model_io import *  # noqa: F401, F403
 from .model_plotting import *  # noqa: F401, F403
+from .photometry import *  # noqa: F401, F403
 from .simulation import *  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
-
-# exclude matching from this list to avoid sphinx warnings
-__all__ = []
-__all__ += epsf.__all__
-__all__ += epsf_stars.__all__
-__all__ += functional_models.__all__
-__all__ += gridded_models.__all__
-__all__ += groupers.__all__
-__all__ += image_models.__all__
-__all__ += photometry.__all__
-__all__ += model_helpers.__all__
-__all__ += model_io.__all__
-__all__ += model_plotting.__all__
-__all__ += simulation.__all__
-__all__ += utils.__all__
