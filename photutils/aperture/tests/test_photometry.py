657118426611262cc5cf8fd6cb1c1d4bbcf26d0f
Larry Bradley <larry.bradley@gmail.com>
2024-11-27 15:01:16 -0500

    Add aperture_photometry tests for sky_center column

--- photutils/aperture/tests/test_photometry.py
+++ photutils/aperture/tests/test_photometry.py
@@ -263,6 +263,25 @@ class TestInputNDData(BaseTestDifferentData):
         self.fluxunit = u.adu
 
 
+def test_input_wcs():
+    data = make_4gaussians_image()
+    wcs = make_wcs(data.shape)
+
+    # hard wired positions in make_4gaussian_image
+    xypos = np.transpose(([160.0, 25.0, 150.0, 90.0],
+                          [70.0, 40.0, 25.0, 60.0]))
+    aper = CircularAperture(xypos, 3.0)
+    aper3 = [CircularAperture((160.0, 70.0), r) for r in (3, 4, 5)]
+
+    phot1 = aperture_photometry(data, aper[0], wcs=wcs)
+    phot2 = aperture_photometry(data, aper, wcs=wcs)
+    phot3 = aperture_photometry(data, aper3, wcs=wcs)
+
+    assert 'sky_center' in phot1.colnames
+    assert 'sky_center' in phot2.colnames
+    assert 'sky_center' in phot3.colnames
+
+
 @pytest.mark.remote_data
 def test_wcs_based_photometry_to_catalog():
     pathcat = get_path('spitzer_example_catalog.xml', location='remote')
@@ -830,16 +849,6 @@ def test_nddata_input():
         assert_allclose(tbl1[column], tbl2[column])
 
 
-def test_invalid_subpixels():
-    data = np.ones((11, 11))
-    aper = CircularAperture((5, 5), r=3)
-    match = 'subpixels must be a strictly positive integer'
-    with pytest.raises(ValueError, match=match):
-        aperture_photometry(data, aper, method='subpixel', subpixels=0)
-    with pytest.raises(ValueError, match=match):
-        aperture_photometry(data, aper, method='subpixel', subpixels=-1)
-
-
 @pytest.mark.skipif(not HAS_REGIONS, reason='regions is required')
 class BaseTestRegionPhotometry:
     def test_region_matches_aperture(self):
@@ -875,6 +884,36 @@ class BaseTestRegionPhotometry:
                                            strict=True):
                 assert_allclose(reg_table['aperture_sum_err'],
                                 ap_table['aperture_sum_err'])
+
+
+def test_invalid_inputs():
+    data = np.ones((11, 11))
+    aper = CircularAperture((5, 5), r=3)
+    wcs = make_wcs(data.shape)
+    sky_aper = aper.to_sky(wcs=wcs)
+
+    match = 'A WCS transform must be defined'
+    with pytest.raises(ValueError, match=match):
+        aperture_photometry(data, sky_aper)
+
+    aper2 = CircularAperture((7, 7), r=3)
+    sky_aper2 = aper2.to_sky(wcs=wcs)
+    apers = [aper, aper2]
+    sky_apers = [sky_aper, sky_aper2]
+
+    match = 'Input apertures must all have identical positions'
+    with pytest.raises(ValueError, match=match):
+        aperture_photometry(data, apers)
+    with pytest.raises(ValueError, match=match):
+        aperture_photometry(data, sky_apers, wcs=wcs)
+
+    data = np.ones((11, 11))
+    aper = CircularAperture((5, 5), r=3)
+    match = 'subpixels must be a strictly positive integer'
+    with pytest.raises(ValueError, match=match):
+        aperture_photometry(data, aper, method='subpixel', subpixels=0)
+    with pytest.raises(ValueError, match=match):
+        aperture_photometry(data, aper, method='subpixel', subpixels=-1)
 
 
 @pytest.mark.skipif(not HAS_REGIONS, reason='regions is required')
