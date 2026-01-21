a0d1ba7f6a48213b24aed5db75849fe095a9fbde
Erik Tollerud <erik.tollerud@gmail.com>
2026-01-21 09:04:59 -0700

    Fix pointed out by @larrybradley
    Co-authored-by: Larry Bradley <larry.bradley@gmail.com>

--- photutils/psf/utils.py
+++ photutils/psf/utils.py
@@ -629,7 +629,7 @@ def _create_call_docstring(iterative=False):
         <https://www.stsci.edu/files/live/sites/www/files/home/hst/instr
         umentation/acs/documentation/instrument-science-reports-isrs/_do
         cuments/isr2202.pdf>`_ software.  They are also similar to the
-        ``chi`` (``qfit``) and ``sharp`` (``cfit```) metrics used by
+        ``chi`` (``qfit``) and ``sharp`` (``cfit``) metrics used by
         `DAOPHOT
         <https://ui.adsabs.harvard.edu/abs/1987PASP...99..191S/abstract>`_.
         """
