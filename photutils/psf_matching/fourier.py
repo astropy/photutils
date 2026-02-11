ec58abd55046b9641d30e59c3beddfd2db86dc6f
Larry Bradley <larry.bradley@gmail.com>
2026-02-11 15:07:25 -0500

    Add psf validation helper function

--- photutils/psf_matching/fourier.py
+++ photutils/psf_matching/fourier.py
@@ -3,16 +3,55 @@
 Tools for matching PSFs using Fourier methods.
 """
 
+import warnings
+
 import numpy as np
+from astropy.utils.exceptions import AstropyUserWarning
 from numpy.fft import fft2, fftshift, ifft2, ifftshift
 from scipy.ndimage import zoom
 
 __all__ = ['create_matching_kernel', 'resize_psf']
 
 
+def _validate_psf(psf, name):
+    """
+    Validate that a PSF is 2D with odd dimensions and centered.
+
+    Parameters
+    ----------
+    psf : `~numpy.ndarray`
+        The PSF array to validate.
+
+    name : str
+        The parameter name used in error messages.
+
+    Raises
+    ------
+    ValueError
+        If the PSF is not 2D or has even dimensions.
+    """
+    if psf.ndim != 2:
+        msg = f'{name} must be a 2D array.'
+        raise ValueError(msg)
+
+    if psf.shape[0] % 2 == 0 or psf.shape[1] % 2 == 0:
+        msg = (f'{name} must have odd dimensions, got '
+               f'shape {psf.shape}.')
+        raise ValueError(msg)
+
+    center = ((psf.shape[0] - 1) / 2, (psf.shape[1] - 1) / 2)
+    peak = np.unravel_index(np.argmax(psf), psf.shape)
+    if peak != center:
+        msg = (f'The peak of {name} is not centered. Expected peak at '
+               f'{center}, but found peak at {peak}.')
+        warnings.warn(msg, AstropyUserWarning)
+
+
 def resize_psf(psf, input_pixel_scale, output_pixel_scale, *, order=3):
     """
     Resize a PSF using spline interpolation of the requested order.
+
+    The total flux of the PSF is conserved during the resizing.
 
     Parameters
     ----------
@@ -27,15 +66,32 @@ def resize_psf(psf, input_pixel_scale, output_pixel_scale, *, order=3):
         The pixel scale of the output ``psf``. The units must match
         ``input_pixel_scale``.
 
-    order : float, optional
+    order : int, optional
         The order of the spline interpolation (0-5). The default is 3.
 
     Returns
     -------
     result : 2D `~numpy.ndarray`
         The resampled/interpolated 2D data array.
+
+    Raises
+    ------
+    ValueError
+        If ``psf`` is not a 2D array, has even dimensions, is not
+        centered, or if the pixel scales are not positive.
     """
+    psf = np.asarray(psf, dtype=float)
+
+    if input_pixel_scale <= 0 or output_pixel_scale <= 0:
+        msg = ('input_pixel_scale and output_pixel_scale must be '
+               'positive.')
+        raise ValueError(msg)
+
+    _validate_psf(psf, 'psf')
+
     ratio = input_pixel_scale / output_pixel_scale
+
+    # Scale by ratio**2 to conserve total flux
     return zoom(psf, ratio, order=order) / ratio**2
 
 
