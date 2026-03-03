9eb87997304aef36d8fefcc5d50e93c2ba2c2a0a
Larry Bradley <larry.bradley@gmail.com>
2026-03-03 11:28:00 -0500

    Add DAOStarFinder tests for 2D threshold

--- photutils/detection/tests/test_daofinder.py
+++ photutils/detection/tests/test_daofinder.py
@@ -38,10 +38,6 @@ class TestDAOStarFinder:
         """
         Test that invalid inputs raise appropriate errors.
         """
-        match = 'threshold must be a scalar value'
-        with pytest.raises(TypeError, match=match):
-            DAOStarFinder(threshold=np.ones((2, 2)), fwhm=3.0)
-
         match = 'fwhm must be a scalar value'
         with pytest.raises(TypeError, match=match):
             DAOStarFinder(threshold=3.0, fwhm=np.ones((2, 2)))
@@ -281,22 +277,22 @@ class TestDAOStarFinder:
         Test the __repr__ of DAOStarFinder.
         """
         finder = DAOStarFinder(threshold=5.0, fwhm=3.0)
-        r = repr(finder)
-        assert 'DAOStarFinder(' in r
-        assert 'threshold=5.0' in r
-        assert 'fwhm=3.0' in r
-        assert 'ratio=1.0' in r
-        assert 'xycoords=None' in r
+        repr_ = repr(finder)
+        assert 'DAOStarFinder(' in repr_
+        assert 'threshold=5.0' in repr_
+        assert 'fwhm=3.0' in repr_
+        assert 'ratio=1.0' in repr_
+        assert 'xycoords=None' in repr_
 
     def test_str(self):
         """
         Test the __str__ of DAOStarFinder.
         """
         finder = DAOStarFinder(threshold=5.0, fwhm=3.0)
-        s = str(finder)
-        assert 'DAOStarFinder' in s
-        assert 'threshold: 5.0' in s
-        assert 'fwhm: 3.0' in s
+        str_ = str(finder)
+        assert 'DAOStarFinder' in str_
+        assert 'threshold: 5.0' in str_
+        assert 'fwhm: 3.0' in str_
 
     def test_repr_with_xycoords(self):
         """
@@ -305,5 +301,58 @@ class TestDAOStarFinder:
         xycoords = np.array([[5, 5], [10, 10]])
         finder = DAOStarFinder(threshold=5.0, fwhm=3.0,
                                xycoords=xycoords)
-        r = repr(finder)
-        assert '<array; shape=(2, 2)>' in r
+        assert '<array; shape=(2, 2)>' in repr(finder)
+
+    def test_threshold_2d_uniform(self, data):
+        """
+        Test that a uniform 2D threshold gives the same results
+        as a scalar threshold.
+        """
+        threshold = 5.0
+        fwhm = 1.0
+        finder_scalar = DAOStarFinder(threshold, fwhm)
+        finder_2d = DAOStarFinder(np.full(data.shape, threshold), fwhm)
+        tbl_scalar = finder_scalar(data)
+        tbl_2d = finder_2d(data)
+        assert_array_equal(tbl_scalar, tbl_2d)
+
+    def test_threshold_2d_varying(self, data):
+        """
+        Test that a varying 2D threshold detects fewer sources in
+        regions with a higher threshold.
+        """
+        fwhm = 1.0
+        threshold_low = 1.0
+        threshold_high = 100.0
+        threshold_2d = np.full(data.shape, threshold_low)
+        threshold_2d[0:50, :] = threshold_high
+
+        finder_low = DAOStarFinder(threshold_low, fwhm)
+        finder_2d = DAOStarFinder(threshold_2d, fwhm)
+
+        tbl_low = finder_low(data)
+        tbl_2d = finder_2d(data)
+        assert len(tbl_low) > len(tbl_2d)
+        # all 2D sources should be in the lower half
+        assert all(tbl_2d['ycentroid'] >= 50)
+
+    def test_threshold_2d_repr(self):
+        """
+        Test repr with a 2D threshold array.
+        """
+        threshold = np.ones((10, 10))
+        finder = DAOStarFinder(threshold=threshold, fwhm=3.0)
+        assert '<array; shape=(10, 10)>' in repr(finder)
+        assert '<array; shape=(10, 10)>' in str(finder)
+
+    def test_threshold_2d_with_units(self, data):
+        """
+        Test that a 2D threshold with units works correctly.
+        """
+        units = u.Jy
+        threshold = 5.0
+        fwhm = 1.0
+        threshold_2d = np.full(data.shape, threshold) * units
+        finder = DAOStarFinder(threshold_2d, fwhm)
+        tbl = finder(data << units)
+        assert len(tbl) > 0
