c7ce27f845cdbd8d86c760753847f06aeb71d8dd
Nadia Dencheva <nadia.dencheva@gmail.com>
2026-04-21 13:58:46 +0300

    parametrize tests

--- photutils/converters/tests/test_apertures.py
+++ photutils/converters/tests/test_apertures.py
@@ -18,17 +18,17 @@ apertures = [
 
 @pytest.mark.skipif(not ASDF_ASTROPY_INSTALLED,
                     reason='asdf-astropy is not installed')
-def test_aperture_converters(tmp_path):
+@pytest.mark.parametrize('aperture', apertures)
+def test_aperture_converters(tmp_path, aperture):
     """
     Test that the aperture converters can round-trip an aperture object.
     """
-    for aperture in apertures:
-        with asdf.AsdfFile() as af:
-            af['aperture'] = aperture
-            af.write_to(tmp_path / 'aperture.asdf')
+    with asdf.AsdfFile() as af:
+        af['aperture'] = aperture
+        af.write_to(tmp_path / 'aperture.asdf')
 
-        with asdf.open(tmp_path / 'aperture.asdf') as af:
-            aperture2 = af['aperture']
+    with asdf.open(tmp_path / 'aperture.asdf') as af:
+        aperture2 = af['aperture']
 
-            assert np.all(aperture.positions == aperture2.positions)
-            assert aperture.r == aperture2.r
+        assert np.all(aperture.positions == aperture2.positions)
+        assert aperture.r == aperture2.r
