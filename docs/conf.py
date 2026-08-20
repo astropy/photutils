f9d9f3999168a52732dfa4511a4852a330968307
Larry Bradley <larry.bradley@gmail.com>
2026-08-20 14:17:31 -0400

    Ignore ADS in linkcheck due to HTTP 405 Client Errors

--- docs/conf.py
+++ docs/conf.py
@@ -202,6 +202,7 @@ linkcheck_retry = 5
 linkcheck_ignore = [
     'http://data.astropy.org',
     r'https://github\.com/astropy/photutils/(?:issues|pull)/\d+',
+    r'https://ui\.adsabs\.harvard\.edu/.*',  # 405 Client Error
     # Zenodo/doi: 403 Client Error: Forbidden for url
     r'https://zenodo.org/records/*',
     r'https://doi.org/*',
