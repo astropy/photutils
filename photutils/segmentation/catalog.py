d344a8d55ffaaccd1c814278b6d080dc734aa222
Larry Bradley <larry.bradley@gmail.com>
2026-09-15 20:07:30 -0400

    Add a sky_orientation property to SourceCatalog

--- photutils/segmentation/catalog.py
+++ photutils/segmentation/catalog.py
@@ -4110,6 +4110,39 @@ class SourceCatalog:
 
     @cached_property
     @use_detcat
+    def sky_orientation(self):
+        """
+        The position angle on the sky of the major axis of the 2D
+        Gaussian function that has the same second-order moments as the
+        source.
+
+        The pixel `covariance` matrix is transported to the local
+        tangent plane with the WCS Jacobian evaluated at each source
+        `centroid`, which accounts for the WCS rotation, parity, and
+        any local distortion. The position angle is measured from
+        North toward East (i.e., counter-clockwise on the sky) in the
+        celestial frame of the input ``wcs`` and is in the range (-90,
+        90] degrees. This is the same convention as SourceExtractor's
+        ``THETA_J2000`` parameter for an equatorial ``wcs``.
+
+        `None` if ``wcs`` is not input.
+        """
+        if self.wcs is None:
+            return self._null_objects
+        sky_cov = self._sky_cov_from_pixel_cov(self._covariance,
+                                               self._array('centroid'))
+
+        # The tangent-plane axes are ordered (East, North). Measuring
+        # the angle from North toward East makes North play the role
+        # of the x axis and East the role of the y axis in the pixel
+        # `orientation` formula.
+        orient_radians = 0.5 * np.arctan2(2.0 * sky_cov[:, 0, 1],
+                                          (sky_cov[:, 1, 1]
+                                           - sky_cov[:, 0, 0]))
+        return np.rad2deg(orient_radians) * u.deg
+
+    @cached_property
+    @use_detcat
     def eccentricity(self):
         r"""
         The eccentricity of the 2D Gaussian function that has the same
